6ebb95ed4b328a5090e815d01ea82dd90a7f45e6
Bill Thies <bthies@gmail.com>
2001-09-25 13:24:21 +0000

    Made the constructor protected to be consistent with all other KOPI types.  You should use CStdType.Integer to get at an integer type when building the IR.
    (Email notification does discourage small check-ins, doesn't it?  hmm. sorry.)

--- streams/src/at/dms/kjc/CIntType.java
+++ streams/src/at/dms/kjc/CIntType.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: CIntType.java,v 1.1 2001-08-30 16:32:50 thies Exp $
+ * $Id: CIntType.java,v 1.2 2001-09-25 13:24:21 thies Exp $
  */
 
 package at.dms.kjc;
@@ -37,7 +37,7 @@ public class CIntType extends CNumericType {
   /**
    * Constructs a new instance.
    */
-  public CIntType() {
+  protected CIntType() {
     super(TID_INT);
   }
 

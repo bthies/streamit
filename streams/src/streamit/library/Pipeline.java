eba18d6d489517381596e6100fab71149c008052
Michal Karczmarek <karczma@cag.csail.mit.edu>
2001-09-26 17:39:47 +0000

    Added Pipeline.java - should have done it before Pipeline implements a pipeline - basically what a Stream used to represent Because all the functionality is already implemented in Stream, I only need to inherit from Stream and don't need to add any code

---
+++ streams/src/streamit/library/Pipeline.java
@@ -0,0 +1,7 @@
+package streamit;
+
+// implements a pipeline - stream already has all the functionality,
+// so there's no need to put it in Pipeline - just inherit Stream :)
+public class Pipeline extends Stream
+{
+}

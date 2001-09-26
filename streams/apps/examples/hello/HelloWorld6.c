83fec1de92d75af369c6eececb2b73602044c590
David Maze <dmaze@cag.csail.mit.edu>
2001-09-26 14:04:18 +0000

    Work functions now take the type of the data object as their first parameter, and get cast to void (*)(void *, tape *, tape *) (which is work_fn) if they need to be passed around.

--- streams/apps/examples/hello/HelloWorld6.c
+++ streams/apps/examples/hello/HelloWorld6.c
@@ -1,6 +1,6 @@
 /*
  * HelloWorld6.c: translated "hello, world" StreaMIT example
- * $Id: HelloWorld6.c,v 1.4 2001-09-25 22:47:08 dmaze Exp $
+ * $Id: HelloWorld6.c,v 1.5 2001-09-26 14:04:18 dmaze Exp $
  */
 
 #include "streamit.h"
@@ -16,21 +16,20 @@ typedef struct HelloWorld6_1_data
 } HelloWorld6_1_data;
 
 void HelloWorld6_1_init(HelloWorld6_1_data *d, void *p);
-void HelloWorld6_1_work(void *dv, tape *in_tape, tape *out_tape);
+void HelloWorld6_1_work(HelloWorld6_1_data *d, tape *in_tape, tape *out_tape);
 
 /* dzm: We need to tell the library the type/size of the channel. */
 void HelloWorld6_1_init(HelloWorld6_1_data *d, void *p)
 {
   set_stream_type(d->c, FILTER);
   set_push(d->c, 1);
-  set_work(d->c, HelloWorld6_1_work);
+  set_work(d->c, (work_fn)HelloWorld6_1_work);
   d->x = 0;
 }
 
 /* dzm: subtlety involving the ++ operator and macros.  Hmm. */
-void HelloWorld6_1_work(void *dv, tape *in_tape, tape *out_tape)
+void HelloWorld6_1_work(HelloWorld6_1_data *d, tape *in_tape, tape *out_tape)
 {
-  HelloWorld6_1_data *d = dv;
   d->x++;
   PUSH(out_tape, int, d->x);
 }
@@ -41,17 +40,17 @@ typedef struct HelloWorld6_2_data
 } HelloWorld6_2_data;
 
 void HelloWorld6_2_init(HelloWorld6_2_data *d, void *p);
-void HelloWorld6_2_work(void *dv, tape *in_tape, tape *out_tape);
+void HelloWorld6_2_work(HelloWorld6_2_data *d, tape *in_tape, tape *out_tape);
 
 void HelloWorld6_2_init(HelloWorld6_2_data *d, void *p)
 {
   set_stream_type(d->c, FILTER);
   set_pop(d->c, 1);
-  set_work(d->c, HelloWorld6_2_work);
+  set_work(d->c, (work_fn)HelloWorld6_2_work);
 }
 
 /* dzm: Way magical. */
-void HelloWorld6_2_work(void *dv, tape *in_tape, tape *out_tape)
+void HelloWorld6_2_work(HelloWorld6_2_data *d, tape *in_tape, tape *out_tape)
 {
   printf("%d\n", POP(in_tape, int));
 }
@@ -64,12 +63,12 @@ typedef struct HelloWorld6_data
 } HelloWorld6_data;
 
 void HelloWorld6_init(HelloWorld6_data *d, void *p);
-void HelloWorld6_work(void *dv, tape *in_tape, tape *out_tape);
+void HelloWorld6_work(HelloWorld6_data *d, tape *in_tape, tape *out_tape);
 
 void HelloWorld6_init(HelloWorld6_data *d, void *p)
 {
   set_stream_type(d->c, PIPELINE);
-  set_work(d->c, HelloWorld6_work);
+  set_work(d->c, (work_fn)HelloWorld6_work);
   
   d->child1 = malloc(sizeof(HelloWorld6_1_data));
   d->child1->c = create_context(d->child1);
@@ -82,10 +81,9 @@ void HelloWorld6_init(HelloWorld6_data *d, void *p)
   HelloWorld6_2_init(d->child2, d);
 }
 
-void HelloWorld6_work(void *dv, tape *in_tape, tape *out_tape)
+void HelloWorld6_work(HelloWorld6_data *d, tape *in_tape, tape *out_tape)
 {
   int itape[1];
-  HelloWorld6_data *d = dv;
   d->child1->x++;
   itape[0] = d->child1->x;
   printf("%d\n", itape[0]);

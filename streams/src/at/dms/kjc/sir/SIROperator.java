4e8be68c66052aad018bbb98e967ae9c0ab1b152
Bill Thies <bthies@gmail.com>
2001-09-27 18:47:59 +0000

    added naming support

--- streams/src/at/dms/kjc/sir/SIROperator.java
+++ streams/src/at/dms/kjc/sir/SIROperator.java
@@ -1,27 +1,41 @@
 package at.dms.kjc.sir;
 
 import at.dms.kjc.*;
+import at.dms.kjc.sir.lowering.Namer;
 
 /**
  * This represents an operator in the stream graph.
  */
 public abstract class SIROperator extends at.dms.util.Utils {
-  /**
-   * The stream structure containing this, or NULL if this is the
-   * toplevel stream.
-   */
-  protected SIRStream parent;
+    /**
+     * The stream structure containing this, or NULL if this is the
+     * toplevel stream.
+     */
+    protected SIRStream parent;
 
-  /**
-   * Constructs and operator with parent <parent>.
-   */
-  protected SIROperator(SIRStream parent) {
-    this.parent = parent;
-  }
+    /**
+     * Constructs and operator with parent <parent>.
+     */
+    protected SIROperator(SIRStream parent) {
+	this.parent = parent;
+    }
 
+    /**
+     * Accepts <v> at this node.
+     */
+    public abstract void accept(SIRVisitor v);
 
-  /**
-   * Accepts <v> at this node.
-   */
-  public abstract void accept(SIRVisitor v);
+    /**
+     * Returns the name of this, or null if a name has not yet been assigned.
+     */
+    public String getName() {
+	return Namer.getName(this);
+    }
+
+    /**
+     * Returns the parent of this.
+     */
+    public SIRStream getParent() {
+	return parent;
+    }
 }

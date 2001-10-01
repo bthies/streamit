217be84db85361d2aeac6b3d56b70f7c0e71d6ab
David Maze <dmaze@cag.csail.mit.edu>
2001-10-01 17:45:49 +0000

    Don't explicitly tag interface functions as public, since jikes complains.

--- streams/src/at/dms/kjc/lir/LIRVisitor.java
+++ streams/src/at/dms/kjc/lir/LIRVisitor.java
@@ -1,6 +1,6 @@
 /*
  * LIRVisitor.java: visit StreaMIT Low IR nodes
- * $Id: LIRVisitor.java,v 1.1 2001-10-01 17:39:46 dmaze Exp $
+ * $Id: LIRVisitor.java,v 1.2 2001-10-01 17:45:49 dmaze Exp $
  */
 
 package at.dms.kjc.lir;
@@ -12,68 +12,68 @@ public interface LIRVisitor
     /**
      * Visits a function pointer.
      */
-    public void visitFunctionPointer(LIRFunctionPointer self,
-                                     String name);
+    void visitFunctionPointer(LIRFunctionPointer self,
+                              String name);
     
     /**
      * Visits an LIR node.
      */
-    public void visitNode(LIRNode self);
+    void visitNode(LIRNode self);
 
     /**
      * Visits a child registration node.
      */
-    public void visitSetChild(LIRSetChild self,
-                              JExpression streamContext,
-                              JExpression childContext);
+    void visitSetChild(LIRSetChild self,
+                       JExpression streamContext,
+                       JExpression childContext);
     
     /**
      * Visits a decoder registration node.
      */
-    public void visitSetDecode(LIRSetDecode self,
-                               JExpression streamContext,
-                               LIRFunctionPointer fp);
+    void visitSetDecode(LIRSetDecode self,
+                        JExpression streamContext,
+                        LIRFunctionPointer fp);
     
     /**
      * Visits an encoder registration node.
      */
-    public void visitSetEncode(LIRSetEncode self,
-                               JExpression streamContext,
-                               LIRFunctionPointer fp);
+    void visitSetEncode(LIRSetEncode self,
+                        JExpression streamContext,
+                        LIRFunctionPointer fp);
     
     /**
      * Visits a peek-rate-setting node.
      */
-    public void visitSetPeek(LIRSetPeek self,
-                             JExpression streamContext,
-                             int peek);
+    void visitSetPeek(LIRSetPeek self,
+                      JExpression streamContext,
+                      int peek);
     
     /**
      * Visits a pop-rate-setting node.
      */
-    public void visitSetPop(LIRSetPop self,
-                            JExpression streamContext,
-                            int pop);
+    void visitSetPop(LIRSetPop self,
+                     JExpression streamContext,
+                     int pop);
     
     /**
      * Visits a push-rate-setting node.
      */
-    public void visitSetPush(LIRSetPush self,
-                             JExpression streamContext,
-                             int push);
+    void visitSetPush(LIRSetPush self,
+                      JExpression streamContext,
+                      int push);
 
     /**
      * Visits a stream-type-setting node.
      */
-    public void visitSetStreamType(LIRSetStreamType self,
-                                   JExpression streamContext,
-                                   LIRStreamType streamType);
+    void visitSetStreamType(LIRSetStreamType self,
+                            JExpression streamContext,
+                            LIRStreamType streamType);
     
     /**
      * Visits a work-function-setting node.
      */
-    public void visitSetWork(LIRSetWork self,
-                             JExpression streamContext,
-                             LIRFunctionPointer fn);
+    void visitSetWork(LIRSetWork self,
+                      JExpression streamContext,
+                      LIRFunctionPointer fn);
 }
 

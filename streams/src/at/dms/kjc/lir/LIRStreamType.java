6cf791f19d8116562157b3c7e6d9802d94bfbb6a
Bill Thies <bthies@gmail.com>
2001-10-02 20:35:43 +0000

    Made IRPrinter into an SLIRVisitor that can print the SIR and LIR.

--- streams/src/at/dms/kjc/lir/LIRStreamType.java
+++ streams/src/at/dms/kjc/lir/LIRStreamType.java
@@ -8,10 +8,22 @@ import at.dms.compiler.*;
  */
 public class LIRStreamType {
 
-    public static final LIRStreamType LIR_PIPELINE = new LIRStreamType();
-    public static final LIRStreamType LIR_SPLIT_JOIN = new LIRStreamType();
-    public static final LIRStreamType LIR_FEEDBACK_LOOP = new LIRStreamType();
-    public static final LIRStreamType LIR_FILTER = new LIRStreamType();
+    public static final LIRStreamType LIR_PIPELINE = 
+	new LIRStreamType("PIPELINE");
+    public static final LIRStreamType LIR_SPLIT_JOIN = 
+	new LIRStreamType("SPLIT-JOIN");
+    public static final LIRStreamType LIR_FEEDBACK_LOOP = 
+	new LIRStreamType("FEEDBACK LOOP");
+    public static final LIRStreamType LIR_FILTER = 
+	new LIRStreamType("FILTER");
 
-    private LIRStreamType() {}
+    private final String name;
+
+    private LIRStreamType(String name) {
+	this.name = name;
+    }
+
+    public String toString() {
+	return name;
+    }
 }

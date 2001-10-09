d06c72d774f95b5f78d59702045d7966574fcfa3
David Maze <dmaze@cag.csail.mit.edu>
2001-10-09 19:34:35 +0000

    Added create_tape_internal(), which creates an unbound tape with a particular length and size.

--- streams/library/c/streamit_internal.h
+++ streams/library/c/streamit_internal.h
@@ -4,5 +4,6 @@
 #include "streamit.h"
 
 void connect_tapes (stream_context *c);
+tape *create_tape_internal(int data_size, int tape_length);
 
 #endif // __STREAMIT_INTERNAL_H__
--- streams/library/c/streamit_tape.c
+++ streams/library/c/streamit_tape.c
@@ -11,6 +11,17 @@ void create_tape (stream_context *src, stream_context *dst, int data_size, int t
     assert (data_size > 0);
     assert (tape_length > 0);
 
+    new_tape = create_tape_internal(data_size, tape_length);
+    
+    // and tell the source and sink that they have a new tape
+    src->output_tape = new_tape;
+    dst->input_tape = new_tape;
+}
+
+tape *create_tape_internal(int data_size, int tape_length)
+{
+    tape *new_tape;
+  
     new_tape = (tape*) malloc (sizeof (tape));
     assert (new_tape);
 
@@ -21,8 +32,7 @@ void create_tape (stream_context *src, stream_context *dst, int data_size, int t
     new_tape->data_size = data_size;
     new_tape->data = malloc (data_size * tape_length);
 
-    // and tell the source and sink that they have a new tape
-    src->output_tape = new_tape;
-    dst->input_tape = new_tape;
+    return new_tape;
 }
 
+  

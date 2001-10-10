a0376ded57134a8c875953409bc22635e4b04d1b
David Maze <dmaze@cag.csail.mit.edu>
2001-10-10 19:58:55 +0000

    Rewrote to have fewer nested anonymous classes and make it easier for the existing compiler to generate most of the code for me.  Ideally, the comments around the IntFly class can be removed to get a SplitJoin.  For now, keep this code since it has a Pipeline inside a Pipeline, which breaks the existing scheduler.

--- streams/apps/tests/flybit/Flybit.java
+++ streams/apps/tests/flybit/Flybit.java
@@ -1,10 +1,64 @@
 /*
  * Flybit.java: an interesting piece of the Butterfly example
  * (to demonstrate split/joins)
- * $Id: Flybit.java,v 1.2 2001-10-10 19:02:26 dmaze Exp $
+ * $Id: Flybit.java,v 1.3 2001-10-10 19:58:55 dmaze Exp $
  */
 
 import streamit.*;
+
+class IntSource extends Filter
+{
+    int x;
+    Channel output = new Channel(Integer.TYPE, 1);
+    public void init()
+    {
+        this.x = 0;
+    }
+    public void work()
+    {
+        output.pushInt(x++);
+    }
+}
+
+class IntSub extends Filter
+{
+    Channel input = new Channel(Integer.TYPE, 2);
+    Channel output = new Channel(Integer.TYPE, 1);
+    public void work()
+    {
+        output.pushInt(input.popInt() - input.popInt());
+    }
+}
+
+class IntAdd extends Filter
+{
+    Channel input = new Channel(Integer.TYPE, 2);
+    Channel output = new Channel(Integer.TYPE, 1);
+    public void work()
+    {
+        output.pushInt(input.popInt() + input.popInt());
+    }
+}
+
+class IntFly extends Pipeline // SplitJoin
+{
+    public void init() 
+    {
+        // setSplitter(DUPLICATE());
+        add(new IntSub());
+        add(new IntAdd());
+        // setJoiner(WEIGHTED_ROUND_ROBIN(2, 2));
+    }
+}
+
+class IntPrinter extends Filter
+{
+    Channel input = new Channel(Integer.TYPE, 1);
+    public void work ()
+    {
+        System.out.println (input.popInt ());
+    }
+}
 
 public class Flybit extends Pipeline
 {
@@ -16,59 +70,9 @@ public class Flybit extends Pipeline
     
     public void init()
     {
-        add(new Filter()
-            {
-                int x;
-                Channel output = new Channel(Integer.TYPE, 1);
-                public void init()
-                {
-                    this.x = 0;
-                }
-                public void work()
-                {
-                    output.pushInt(x++);
-                }
-            });
-        add(new SplitJoin()
-            {
-                Channel input = new Channel(Integer.TYPE, 2);
-                Channel output = new Channel(Integer.TYPE, 2);
-                public void init() 
-                {
-                    setSplitter(DUPLICATE());
-                    add(new Filter()
-                        {
-                            Channel input = new Channel(Integer.TYPE, 2);
-                            Channel output = new Channel(Integer.TYPE, 2);
-                            public void work()
-                            {
-                                output.pushInt(input.popInt()-input.popInt());
-                            }
-                        });
-                    add(new Filter()
-                        {
-                            Channel input = new Channel(Integer.TYPE, 2);
-                            Channel output = new Channel(Integer.TYPE, 2);
-                            public void work()
-                            {
-                                output.pushInt(input.popInt()+input.popInt());
-                            }
-                        });
-                    setJoiner(WEIGHTED_ROUND_ROBIN(2, 2));
-                }
-            });
-        add (new Filter()
-            {
-                Channel input = new Channel(Integer.TYPE, 1);
-                public void initIO ()
-                {
-                    streamInput = input;
-                } 
-                public void work ()
-                {
-                    System.out.println (input.popInt ());
-                }
-            });
+        add(new IntSource());
+        add(new IntFly());
+        add(new IntPrinter());
     }
 }
 

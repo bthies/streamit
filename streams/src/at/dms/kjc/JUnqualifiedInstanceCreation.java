79955a1152dc52bf994bf7e4bfa0b2c4bc65a640
Michael Gordon <michael.ian.gordon@gmail.com>
2001-10-12 05:53:07 +0000

    Needed access to the args of the creation, so I broke some abstraction barriers.

--- streams/src/at/dms/kjc/JUnqualifiedInstanceCreation.java
+++ streams/src/at/dms/kjc/JUnqualifiedInstanceCreation.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: JUnqualifiedInstanceCreation.java,v 1.2 2001-10-02 19:25:05 mgordon Exp $
+ * $Id: JUnqualifiedInstanceCreation.java,v 1.3 2001-10-12 05:53:07 mgordon Exp $
  */
 
 package at.dms.kjc;
@@ -220,6 +220,12 @@ public class JUnqualifiedInstanceCreation extends JExpression {
 
     constructor.genCode(code, true);
   }
+
+    
+
+    public JExpression[] getParams() {
+	return params;
+    }
 
   // ----------------------------------------------------------------------
   // DATA MEMBERS

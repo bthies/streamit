9b5f1281c1ad00e58df6c4e4b3acbff66e4d0a0f
David Maze <dmaze@cag.csail.mit.edu>
2001-10-12 21:12:22 +0000

    Wrote a peek-free version of fib.  This should be a good, simple feedback loop test.

---
+++ streams/apps/examples/fib/Fib2.java
@@ -0,0 +1,61 @@
+/* -*- Java -*-
+ * Fib.str: Fibonacci number example
+ * $Id: Fib2.java,v 1.1 2001-10-12 21:12:22 dmaze Exp $
+ */
+
+import streamit.*;
+
+class Fib extends Pipeline
+{
+    static public void main (String[] t)
+    {
+        Fib test = new Fib();
+        test.run();
+    }
+    
+    public void init()
+    {
+        add(new FeedbackLoop()
+            {
+                public void init()
+                {
+                    setDelay(2);
+                    setJoiner(WEIGHTED_ROUND_ROBIN(0, 1));
+                    setBody(new Filter()
+                        {
+                            Channel input = new Channel(Integer.TYPE, 1, 1);
+                            Channel output = new Channel(Integer.TYPE, 1);
+                            public void work()
+                            {
+                                int a = input.popInt();
+                                int b = input.popInt();
+                                int result = a + b;
+                                output.pushInt(b);
+                                output.pushInt(result);
+                                output.pushInt(result);
+                            }
+                        });
+                    setSplitter(WEIGHTED_ROUND_ROBIN(2, 1));
+                }
+                int initPath(int index)
+                {
+                    return 1;
+                }
+            });
+        add (new Filter ()
+            {
+                Channel input = new Channel (Integer.TYPE, 1);
+                
+                public void initIO ()
+                {
+                    streamInput = input;
+                }
+                
+                public void work ()
+                {
+                    System.out.println (input.popInt ());
+                }
+            });
+    }
+    
+}

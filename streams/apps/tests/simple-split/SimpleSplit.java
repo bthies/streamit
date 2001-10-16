24cf031c22cedec5055ed5bb7008cf51802d30b6
Bill Thies <bthies@gmail.com>
2001-10-16 18:54:51 +0000

    Modified this example to actually be a runnable stream... it should print a sequence like this:
    1
    4
    3
    2
    5
    8
    7
    6
    
    -- that is, every other two on the odd parity, and every other two in
    backwards order on the even parity.

--- streams/apps/tests/simple-split/SimpleSplit.java
+++ streams/apps/tests/simple-split/SimpleSplit.java
@@ -1,15 +1,68 @@
 import streamit.*;
 
-public class SimpleSplit extends SplitJoin {
+public class SimpleSplit extends Pipeline {
+    public static void main(String args[]) {
+	new SimpleSplit().run();
+    }
+
     public void init() {
-	setSplitter(ROUND_ROBIN());
+	// push increasing integers
 	add(new Filter() {
-		Channel input = new Channel(Integer.TYPE, 1);
 		Channel output = new Channel(Integer.TYPE, 1);
+		int count;
+		public void initIO() {
+		    this.streamOutput = output;
+		}
+		public void init() {
+		    this.count = 0;
+		}
 		public void work() {
-		    output.pushInt(input.popInt());
+		    output.pushInt(count++);
 		}
 	    });
-	setJoiner(ROUND_ROBIN());
+	// add a split-join
+	add(new SplitJoin() {
+		public void init() {
+		    setSplitter(ROUND_ROBIN());
+		    // pop two and push two
+		    this.add(new Filter() {
+			    Channel input = new Channel(Integer.TYPE, 2);
+			    Channel output = new Channel(Integer.TYPE, 2);
+			    public void initIO() {
+				this.streamInput = input;
+				this.streamOutput = output;
+			    }
+			    public void work() {
+				output.pushInt(input.popInt());
+				output.pushInt(input.popInt());
+			    }
+			});
+		    // pop two and push two in opposite order
+		    this.add(new Filter() {
+			    Channel input = new Channel(Integer.TYPE, 2);
+			    Channel output = new Channel(Integer.TYPE, 2);
+			    public void initIO() {
+				this.streamInput = input;
+				this.streamOutput = output;
+			    }
+			    public void work() {
+				int i1 = input.popInt();
+				int i2 = input.popInt();
+				output.pushInt(i2);
+				output.pushInt(i1);
+			    }
+			});
+		    setJoiner(ROUND_ROBIN());
+		}});
+	// print result
+	add(new Filter() {
+		Channel input = new Channel(Integer.TYPE, 1);
+		public void initIO() {
+		    this.streamInput = input;
+		}
+		public void work() {
+		    System.out.println(input.popInt());
+		}
+	    });
     }
 }

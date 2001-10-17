96380822007328bbc42830406de953c182b06bd9
Michal Karczmarek <karczma@cag.csail.mit.edu>
2001-10-17 01:14:12 +0000

    Added copying of peek values into initialization of SchedPipeline

--- streams/src/streamit/scheduler1/SchedPipeline.java
+++ streams/src/streamit/scheduler1/SchedPipeline.java
@@ -43,6 +43,15 @@ public class SchedPipeline extends SchedStream
             }
         }
 
+        // copy the peek of the first child into my peek value
+        // this is the correct peek value to have in a pipeline
+        {
+            SchedStream firstChild = (SchedStream) allChildren.get (0);
+            ASSERT (firstChild);
+
+            setPeekConsumption (firstChild.getPeekConsumption ());
+        }
+
         // multiply the children's num of executions
         // by the appropriate output factors
         {

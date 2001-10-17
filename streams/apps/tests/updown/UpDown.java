0e65b1e72e87c8c442b6c6f25abe61d8275bbe8d
David Maze <dmaze@cag.csail.mit.edu>
2001-10-17 13:41:51 +0000

    Hopefully a simple example of messaging.  This has three components: -- UpDownGen, which counts either up or down, and accepts a message    to change directions; -- Limiter, which sends a message to UpDownGen if the number is    outside of the range (0, 10); -- IntPrinter. I don't expect that we'll be able to compile it anytime soon, but it's a useful simple test case for building infrastructure.

---
+++ streams/apps/tests/updown/UpDown.java
@@ -0,0 +1,70 @@
+/*
+ * UpDown.java: a counter that counts between 0 and 10, and back
+ */
+
+class UpDownGen extends Filter
+{
+    channel output = new Channel(Integer.TYPE, 1);
+    boolean up;
+    int x;
+    public void init()
+    {
+        up = true;
+        x = 0;
+    }
+    public void work()
+    {
+        if (up) x++;
+        else x--;
+        output.pushInt(x);
+    }
+    public void setUp(boolean up)
+    {
+        this.up = up;
+    }
+}
+
+class Limiter extends Filter
+{
+    Channel input = new Channel(Integer.TYPE, 1);
+    Channel output = new Channel(Integer.TYPE, 1);
+    UpDownGenPortal p;
+    public void init(UpDownGenPortal p)
+    {
+        this.p = p;
+    }
+    public void work()
+    {
+        int val = input.popInt();
+        if (val <= 0)
+            p.setUp(true);
+        if (vale >= 10)
+            p.setUp(false);
+        output.pushInt(val);
+    }
+}
+
+class IntPrinter extends Filter
+{
+    Channel input = new Channel(Integer.TYPE, 1);
+    public void work()
+    {
+        System.out.println(input.popInt());
+    }
+}
+
+public class UpDown extends Pipeline
+{
+    public void init()
+    {
+        UpDownGenPortal p = new UpDownGenPortal();
+        UpDownGen g = new UpDownGen();
+        p.register(g);
+        
+        add(g);
+        add(new Limiter(p));
+        add(new IntPrinter());
+    }
+}
+
+    

1d13eebcb4a12684bee0e693ddad6ff370ba0021
Michael Gordon <michael.ian.gordon@gmail.com>
2001-10-22 22:02:48 +0000

    This class implements a deep cloner using the serializable interface.

---
+++ streams/src/at/dms/kjc/ObjectDeepCloner.java
@@ -0,0 +1,59 @@
+//===========================================================================
+//
+//   FILE: ObjectDeepCloner.java:
+//   
+//   Author: Michael Gordon
+//   Date: Wed Oct 17 14:03:33 2001
+//
+//   Function:  Deep clone using Serializable
+//
+//===========================================================================
+
+package at.dms.kjc;
+
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+
+
+/**
+ * This class implements general deep cloning using the serializable interface
+ */
+public class ObjectDeepCloner
+{
+   // so that nobody can accidentally create an ObjectCloner object
+   private ObjectDeepCloner(){}
+  
+    /**
+     * Returns the deep clone of an object
+     */ 
+   static public Object deepCopy(Object oldObj)
+   {
+      ObjectOutputStream oos = null;
+      ObjectInputStream ois = null;
+      System.out.println("Deep Copying");
+      try
+      {
+         ByteArrayOutputStream bos = 
+               new ByteArrayOutputStream();
+         oos = new ObjectOutputStream(bos);
+         // serialize and pass the object
+         oos.writeObject(oldObj);  
+         oos.flush();              
+         ByteArrayInputStream bin = 
+               new ByteArrayInputStream(bos.toByteArray()); 
+         ois = new ObjectInputStream(bin);                  
+         // return the new object
+	 oos.close();
+         ois.close();
+	 return ois.readObject(); 
+      }
+      catch(Exception e)
+      {
+         System.err.println("Exception in ObjectCloner = " + e);
+	 System.exit(-1);
+	 
+      }
+      return null;
+   }
+}

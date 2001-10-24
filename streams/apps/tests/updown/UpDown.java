c6d22763efe0bd1f58a7868380905cf07ee2a31f
Bill Thies <bthies@gmail.com>
2001-10-24 17:58:12 +0000

    Modified to conform to the new plan for message syntax.  This will compile as legal java now.  Any chance the library can deal with it, Michal?  I'll send out a brain dump of the new plan for portals in a sec.

--- streams/apps/tests/updown/UpDown.java
+++ streams/apps/tests/updown/UpDown.java
@@ -1,16 +1,22 @@
 /*
  * UpDown.java: a counter that counts between 0 and 10, and back
- * $Id: UpDown.java,v 1.5 2001-10-24 16:42:58 dmaze Exp $
+ * $Id: UpDown.java,v 1.6 2001-10-24 17:58:12 thies Exp $
  */
 
 import streamit.*;
 
 interface UpDownMsg
 {
-    public void setUp(boolean up);
+    void setUp(boolean up);
 }
 
-filter UpDownGen implements UpDownMsg
+class UpDownMsgPortal implements UpDownMsg {
+    public void regSender(Filter sender) {}
+    public void regReceiver(UpDownMsg receiver) {}
+    public void setUp(boolean up) {}
+}
+
+class UpDownGen extends Filter implements UpDownMsg
 {
     Channel output = new Channel(Integer.TYPE, 1);
     boolean up;
@@ -36,24 +42,26 @@ filter UpDownGen implements UpDownMsg
     }
 }
 
-filter Limiter
+class Limiter extends Filter
 {
     Channel input = new Channel(Integer.TYPE, 1);
     Channel output = new Channel(Integer.TYPE, 1);
-    /* UpDownMsgPortal p; */
-    public void init(/* UpDownMsgPortal p */)
+    UpDownMsgPortal p;
+
+    public Limiter(UpDownMsgPortal p) {}
+
+    public void init(UpDownMsgPortal p)
     {
-        /* this.p = p; */
+        this.p = p;
+	p.regSender(this);
     }
     public void work()
     {
         int val = input.popInt();
-        /*
         if (val <= 0)
             p.setUp(true);
-        if (vale >= 10)
+        if (val >= 10)
             p.setUp(false);
-        */
         output.pushInt(val);
     }
     public void initIO()
@@ -63,7 +71,7 @@ filter Limiter
     }
 }
 
-filter IntPrinter
+class IntPrinter extends Filter
 {
     Channel input = new Channel(Integer.TYPE, 1);
     public void work()
@@ -80,12 +88,12 @@ public class UpDown extends StreamIt
 {
     public void init()
     {
-        /* UpDownMsgPortal p = new UpDownMsgPortal(); */
+        UpDownMsgPortal p = new UpDownMsgPortal();
         UpDownGen g = new UpDownGen();
-        /* p.register(g); */
+        p.regReceiver(g);
         
         add(g);
-        add(new Limiter(/* p */));
+        add(new Limiter(p));
         add(new IntPrinter());
     }
     public static void main(String[] args)

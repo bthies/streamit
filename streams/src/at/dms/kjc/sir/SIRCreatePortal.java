f5ce34b84d23e0534ff0f4395d4918ef06b2f4a5
Michael Gordon <michael.ian.gordon@gmail.com>
2001-10-25 20:00:09 +0000

    Here is the class for the SIRCreatePortal, nothing fancy...

---
+++ streams/src/at/dms/kjc/sir/SIRCreatePortal.java
@@ -0,0 +1,47 @@
+package at.dms.kjc.sir;
+
+import at.dms.kjc.*;
+import at.dms.compiler.*;
+
+/**
+ * Create Portal Expression.
+ *
+ * This expression is a place holder for a particular portal
+ */
+public class SIRCreatePortal extends JExpression {
+    public SIRCreatePortal() {
+	super(null); 
+    }
+
+
+    
+    /**
+     * @return the type of this expression
+     */
+    public CType getType() {
+	return CStdType.Void;
+    }
+
+    
+    public void genCode(CodeSequence code, boolean discardValue) {
+	at.dms.util.Utils.fail("Codegen of SIR nodes not supported yet.");
+    }
+
+    public Object accept(AttributeVisitor p) {
+        at.dms.util.Utils.fail("Visitors to SIRE nodes not supported yet.");
+        return null;
+    }
+
+    public void accept(KjcVisitor p) {
+	if (p instanceof SLIRVisitor) {
+	    ((SLIRVisitor)p).visitCreatePortalExpression();
+	}
+    }
+
+    public JExpression analyse(CExpressionContext context) throws PositionedError {
+	at.dms.util.Utils.fail("Analysis of SIR nodes not supported yet.");
+	return this;
+    }
+
+}
+

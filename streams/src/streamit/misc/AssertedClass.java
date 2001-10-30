3352943e54529520d7557f612cfa51ddf520667d
Michal Karczmarek <karczma@cag.csail.mit.edu>
2001-10-30 18:35:53 +0000

    Changed the ASSERT to print the class that was executing when an ASSERT was triggered.  This means that ASSERT is now an instance function.  For static functions use SASSERT.

--- streams/src/streamit/misc/AssertedClass.java
+++ streams/src/streamit/misc/AssertedClass.java
@@ -11,50 +11,96 @@ public class AssertedClass
 
     // assert the condition
     // if the condition is false, print an error and exit the program
-    public static void ASSERT (boolean condition)
+    public static void ASSERT (boolean condition, Object object)
     {
         if (condition) return;
 
         // condition is not satisifed:
         // print an error and exit.
 
-        System.err.println ("An ASSERT has failed.  Exiting.\n\n");
+        System.err.println ("An ASSERT has failed in class " + (object != null ? object.getClass ().getName () : "(unknown)") + ".  Exiting.\n\n");
         new RuntimeException().printStackTrace();
         System.exit (1);
     }
 
-    public static void ASSERT (char cond)
+    public void ASSERT(boolean condition)
     {
-        ASSERT (cond != 0);
-    }
-    public static void ASSERT (short cond)
-    {
-        ASSERT (cond != 0);
-    }
-    public static void ASSERT (int cond)
-    {
-        ASSERT (cond != 0);
-    }
-    public static void ASSERT (long cond)
-    {
-        ASSERT (cond != 0);
-    }
-    public static void ASSERT (float cond)
-    {
-        ASSERT (cond != 0);
-    }
-    public static void ASSERT (double cond)
-    {
-        ASSERT (cond != 0);
-    }
-    public static void ASSERT (Object cond)
-    {
-        ASSERT (cond != null);
+        ASSERT (condition, this);
     }
 
-    public static void ERROR (String error)
+    public void ASSERT (char cond)
+    {
+        ASSERT (cond != 0, this);
+    }
+    public void ASSERT (short cond)
+    {
+        ASSERT (cond != 0, this);
+    }
+    public void ASSERT (int cond)
+    {
+        ASSERT (cond != 0, this);
+    }
+    public void ASSERT (long cond)
+    {
+        ASSERT (cond != 0, this);
+    }
+    public void ASSERT (float cond)
+    {
+        ASSERT (cond != 0, this);
+    }
+    public void ASSERT (double cond)
+    {
+        ASSERT (cond != 0, this);
+    }
+    public void ASSERT (Object cond)
+    {
+        ASSERT (cond != null, this);
+    }
+
+    public void ERROR (String error)
     {
         System.err.println (error);
-        ASSERT (false);
+        ASSERT (false, this);
+    }
+
+    // static versions of asserts:
+    public static void SASSERT(boolean condition)
+    {
+        ASSERT (condition, null);
+    }
+
+    public static void SASSERT (char cond)
+    {
+        ASSERT (cond != 0, null);
+    }
+    public static void SASSERT (short cond)
+    {
+        ASSERT (cond != 0, null);
+    }
+    public static void SASSERT (int cond)
+    {
+        ASSERT (cond != 0, null);
+    }
+    public static void SASSERT (long cond)
+    {
+        ASSERT (cond != 0, null);
+    }
+    public static void SASSERT (float cond)
+    {
+        ASSERT (cond != 0, null);
+    }
+    public static void SASSERT (double cond)
+    {
+        ASSERT (cond != 0, null);
+    }
+    public static void SASSERT (Object cond)
+    {
+        ASSERT (cond != null, null);
+    }
+
+    public static void SERROR (String error)
+    {
+        System.err.println (error);
+        ASSERT (false, null);
     }
 }

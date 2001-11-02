f10f1ee57eb08c09453958f02d00cc3651d6156f
Michal Karczmarek <karczma@cag.csail.mit.edu>
2001-11-02 22:24:10 +0000

    Wrote an implementation for FileReader and FileWriter.  Hopefully these will work (as I've never written anything like this, and I don't feel like writing some tests)
    Please update library with "cvs update -d" to get the new io directory

---
+++ streams/src/streamit/library/io/FileReader.java
@@ -0,0 +1,67 @@
+package streamit.io;
+
+import streamit.Filter;
+import streamit.Channel;
+
+import java.io.*;
+
+public class FileReader extends Filter
+{
+    Class fileType;
+    File inputFile;
+    java.io.FileInputStream fileInputStream;
+    ObjectInputStream inputStream;
+
+    public FileReader (String fileName, Class type)
+    {
+        fileType = type;
+
+        try
+        {
+            inputFile = new File(fileName);
+            fileInputStream = new java.io.FileInputStream (inputFile);
+            inputStream = new ObjectInputStream (fileInputStream);
+        }
+        catch(Throwable e)
+        {
+            ERROR ("Could not open file " + fileName + " for reading.");
+        }
+    }
+
+    public void init ()
+    {
+        output = new Channel (fileType, 1);
+    }
+
+    public void work ()
+    {
+        try
+        {
+            if (fileType == Character.TYPE)
+            {
+                output.pushChar (inputStream.readChar ());
+            } else
+            if (fileType == Float.TYPE)
+            {
+                output.pushFloat (inputStream.readFloat ());
+            } else
+            if (fileType == Integer.TYPE)
+            {
+                output.pushInt (inputStream.readInt ());
+            } else
+            if (Class.forName ("java.io.Serializable").isAssignableFrom (fileType))
+            {
+                Object newObject = inputStream.readObject ();
+                ASSERT (newObject);
+                output.push (newObject);
+            } else
+            {
+                ERROR ("You must define a reader for your type here.\nIf you're trying to read an object, it should be a serializable object\n(and then you won't have to do anything special).");
+            }
+        }
+        catch (Throwable e)
+        {
+            ERROR ("There was an error reading from a file");
+        }
+    }
+}
---
+++ streams/src/streamit/library/io/FileWriter.java
@@ -0,0 +1,65 @@
+package streamit.io;
+
+import streamit.Filter;
+import streamit.Channel;
+
+import java.io.*;
+
+public class FileWriter extends Filter
+{
+    Class fileType;
+    File outputFile;
+    java.io.FileOutputStream fileOutputStream;
+    ObjectOutputStream outputStream;
+
+    public FileWriter (String fileName, Class type)
+    {
+        fileType = type;
+
+        try
+        {
+            outputFile = new File(fileName);
+            fileOutputStream = new java.io.FileOutputStream (outputFile);
+            outputStream = new ObjectOutputStream (fileOutputStream);
+        }
+        catch(Throwable e)
+        {
+            ERROR ("Could not open file " + fileName + " for writing.");
+        }
+    }
+
+    public void init ()
+    {
+        input = new Channel (fileType, 1);
+    }
+
+    public void work ()
+    {
+        try
+        {
+            if (fileType == Character.TYPE)
+            {
+                outputStream.writeChar (input.popChar ());
+            } else
+            if (fileType == Float.TYPE)
+            {
+                outputStream.writeFloat (input.popFloat ());
+            } else
+            if (fileType == Integer.TYPE)
+            {
+                outputStream.writeInt (input.popInt ());
+            } else
+            if (Class.forName ("java.io.Serializable").isAssignableFrom (fileType))
+            {
+                outputStream.writeObject (input.pop ());
+            } else
+            {
+                ERROR ("You must define a writer for your type here.\nIf you're trying to write an object, it should be a serializable object\n(and then you won't have to do anything special).");
+            }
+        }
+        catch (Throwable e)
+        {
+            ERROR ("There was an error reading from a file");
+        }
+    }
+}

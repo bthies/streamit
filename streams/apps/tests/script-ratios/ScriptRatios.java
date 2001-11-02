2771f25bc8e67e19b2ece4bc283f89152334d254
Michal Karczmarek <karczma@cag.csail.mit.edu>
2001-11-02 18:59:22 +0000

    Fixed the pushFloat to output to OUTPUT instead of INPUT.

--- streams/apps/tests/script-ratios/ScriptRatios.java
+++ streams/apps/tests/script-ratios/ScriptRatios.java
@@ -4,40 +4,40 @@ class ScriptFilter extends Filter {
     int PEEK;
     int POP;
     int PUSH;
-    
+
     public ScriptFilter(int PEEK, int POP, int PUSH) {
-	super (PEEK, POP, PUSH);
+        super (PEEK, POP, PUSH);
     }
 
     public void init(int PEEK, int POP, int PUSH) {
-	input = new Channel(Float.TYPE, POP, PEEK);
-	output = new Channel(Float.TYPE, PUSH);
-	this.PEEK = PEEK;
-	this.POP = POP;
-	this.PUSH = PUSH;
+        input = new Channel(Float.TYPE, POP, PEEK);
+        output = new Channel(Float.TYPE, PUSH);
+        this.PEEK = PEEK;
+        this.POP = POP;
+        this.PUSH = PUSH;
     }
 
     public void work() {
-	for (int i=0; i<PEEK; i++) {
-	    input.peekFloat(i);
-	}
-	for (int i=0; i<POP; i++) {
-	    input.popFloat();
-	}
-	for (int i=0; i<PUSH; i++) {
-	    input.pushFloat(i);
-	}
+        for (int i=0; i<PEEK; i++) {
+            input.peekFloat(i);
+        }
+        for (int i=0; i<POP; i++) {
+            input.popFloat();
+        }
+        for (int i=0; i<PUSH; i++) {
+            output.pushFloat(i);
+        }
     }
 }
 
 public class ScriptRatios extends StreamIt {
     public static void main(String[] args) {
-	new ScriptRatios().run();
+        new ScriptRatios().run();
     }
 
     public void init() {
-	add(new FloatOneSource());
-	add(new ScriptFilter(10, 5, 1));
-	add(new FloatPrinter());
+        add(new FloatOneSource());
+        add(new ScriptFilter(10, 5, 1));
+        add(new FloatPrinter());
     }
 }

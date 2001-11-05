6d781cc9de1a5725540d211fd8aea8df28304648
David Maze <dmaze@cag.csail.mit.edu>
2001-11-05 19:11:46 +0000

    Set the mask in the tape field based off the tape length. You get an assertion failure now if the tape length isn't a power of 2.

--- streams/library/c/streamit_tape.c
+++ streams/library/c/streamit_tape.c
@@ -22,6 +22,7 @@ void create_tape (stream_context *src, stream_context *dst, int data_size, int t
 tape *create_tape_internal(int data_size, int tape_length)
 {
     tape *new_tape;
+    int check, nmask;
   
     new_tape = (tape*) malloc (sizeof (tape));
     assert (new_tape);
@@ -32,6 +33,22 @@ tape *create_tape_internal(int data_size, int tape_length)
     new_tape->tape_length = tape_length;
     new_tape->data_size = data_size;
     new_tape->data = malloc (data_size * tape_length);
+    new_tape->mask = 0xd00d;
+
+    check = 1;
+    nmask = ~0;
+    while (check != 0)
+    {
+      if (check == tape_length)
+      {
+        new_tape->mask = ~nmask;
+        break;
+      }
+      check <<= 1;
+      nmask <<= 1;
+    }
+    
+    assert(new_tape->mask != 0xd00d);
 
     return new_tape;
 }

69b52cbf345b545613ce2892a7f79e8fb213ca82
Bill Thies <bthies@gmail.com>
2001-11-07 23:14:32 +0000

    Modifying init function so that all local vars are declared final.  If you assign to a local variable and expect the value to be propagated, you need to make it a final local variable.  (I think arguments get away without being final for now, but that might cause bugs.)
    So now PCADemo gets through the constant propagator and on to
    scheduling... where it dies after 5 minutes of printing out the
    schedule, hehe.  Might want to think about encoding some loops in the
    representation of the schedule about now...

--- streams/apps/old/PCADemo/PCADemo.java
+++ streams/apps/old/PCADemo/PCADemo.java
@@ -17,24 +17,24 @@ public class PCADemo extends StreamIt
   {
 
     System.out.println("Running the PCA Demo...");
-    int numChannels           = 48;
-    int numSamples            = 4096;
-    int numBeams              = 16;
-    int numCoarseFilterTaps   = 16;
-    int numFineFilterTaps     = 64;
-    int coarseDecimationRatio = 1;
-    int fineDecimationRatio   = 1;
-    int numSegments           = 1;
-    int numPostDec1           = numSamples/coarseDecimationRatio;
-    int numPostDec2           = numPostDec1/fineDecimationRatio;
-    int mfSize                = numSegments*numPostDec2;
-    int pulseSize             = numPostDec2/2;
-    int predecPulseSize       = pulseSize*coarseDecimationRatio*fineDecimationRatio;
-    int targetBeam            = numBeams/4;
-    int targetSample          = numSamples/4;
-    int targetSamplePostdec   = 1 + targetSample/coarseDecimationRatio/fineDecimationRatio;
-    float dOverLambda         = 0.5f;
-    float cfarThreshold       = 0.95f*dOverLambda*numChannels*(0.5f*pulseSize);
+    final int numChannels           = 48;
+    final int numSamples            = 4096;
+    final int numBeams              = 16;
+    final int numCoarseFilterTaps   = 16;
+    final int numFineFilterTaps     = 64;
+    final int coarseDecimationRatio = 1;
+    final int fineDecimationRatio   = 1;
+    final int numSegments           = 1;
+    final int numPostDec1           = numSamples/coarseDecimationRatio;
+    final int numPostDec2           = numPostDec1/fineDecimationRatio;
+    final int mfSize                = numSegments*numPostDec2;
+    final int pulseSize             = numPostDec2/2;
+    final int predecPulseSize       = pulseSize*coarseDecimationRatio*fineDecimationRatio;
+    final int targetBeam            = numBeams/4;
+    final int targetSample          = numSamples/4;
+    final int targetSamplePostdec   = 1 + targetSample/coarseDecimationRatio/fineDecimationRatio;
+    final float dOverLambda         = 0.5f;
+    final float cfarThreshold       = 0.95f*dOverLambda*numChannels*(0.5f*pulseSize);
 
     add(new DataSource(numChannels, numSamples, numBeams,
 		       targetBeam, targetSample, predecPulseSize));

4dc460423ad867e7a4a6e29ce57192d7247fa7b8
Michael Gordon <michael.ian.gordon@gmail.com>
2001-11-16 16:15:38 +0000

    Class with static members so we can query optimization options at various stages in the compiler.  The options are set in Main.java...

---
+++ streams/src/at/dms/kjc/StreamItOptions.java
@@ -0,0 +1,8 @@
+package at.dms.kjc;
+
+public class StreamItOptions 
+{
+    public static boolean constprop = false;
+    public static boolean fusion = false;
+    public static boolean unroll = false;
+}

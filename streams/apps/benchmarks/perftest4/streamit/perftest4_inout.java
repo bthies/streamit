d51222dd11534bc5e29ca3440dbb197cc27009a0
David Maze <dmaze@cag.csail.mit.edu>
2001-11-16 16:29:31 +0000

    Call StreamIt.run() with the correct arguments.

--- streams/apps/benchmarks/perftest4/streamit/perftest4_inout.java
+++ streams/apps/benchmarks/perftest4/streamit/perftest4_inout.java
@@ -81,7 +81,7 @@ public class perftest4_inout extends StreamIt
     static public void main(String[] t)
     {
         perftest4_inout test = new perftest4_inout();
-        test.run();
+        test.run(t);
     }
     
     public void init() {

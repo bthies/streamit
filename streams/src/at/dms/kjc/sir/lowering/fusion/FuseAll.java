be89f79a2258f3760af7ab94daaffd63c6a53ed8
Bill Thies <bthies@gmail.com>
2002-02-21 21:50:03 +0000

    Forgot to add this -- does deep fusion of every fusable portion of a pipeline in a stream graph.

---
+++ streams/src/at/dms/kjc/sir/lowering/fusion/FuseAll.java
@@ -0,0 +1,36 @@
+package at.dms.kjc.sir.lowering.fusion;
+
+import at.dms.kjc.*;
+import at.dms.kjc.sir.*;
+import at.dms.kjc.sir.lowering.*;
+import at.dms.kjc.lir.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/**
+ * This class fuses everything it can in a stream graph.
+ */
+public class FuseAll extends EmptyStreamVisitor {
+
+    private FuseAll() {}
+
+    /**
+     * Fuse everything we can in <str>
+     */
+    public static void fuse(SIRStream str) {
+	str.accept(new FuseAll());
+    }
+
+    /* post-visit a pipeline */
+    public void postVisitPipeline(SIRPipeline self,
+				  SIRStream parent,
+				  JFieldDeclaration[] fields,
+				  JMethodDeclaration[] methods,
+				  JMethodDeclaration init,
+				  List elements) {
+	FusePipe.fuse(self);
+    }
+}

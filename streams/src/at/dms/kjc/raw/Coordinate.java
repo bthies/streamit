7c94dbf707d813fc51d7504a3ea83613ab5b08c7
Michael Gordon <michael.ian.gordon@gmail.com>
2002-02-22 23:11:23 +0000

    Added this stupid class to represent a tile...

---
+++ streams/src/at/dms/kjc/raw/Coordinate.java
@@ -0,0 +1,24 @@
+package at.dms.kjc.raw;
+
+public class Coordinate {
+    private int row;
+    private int column;
+
+    public Coordinate(int r, int c) 
+    {
+	row = r;
+	c = column;
+    }
+    
+    public int getColumn() 
+    {
+	return column;
+    }
+    
+    public int getRow() 
+    {
+	return row;
+    }
+}
+
+    

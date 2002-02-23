295eeb3e106192e1d254502a5ce07be7d09f29d4
Bill Thies <bthies@gmail.com>
2002-02-23 00:21:17 +0000

    I'm very sorry, forgot to add this class.  What's even sorrier is that it took me more than thirty minutes to figure out it was missing.  Sorry.

---
+++ streams/src/at/dms/kjc/sir/EmptyStreamVisitor.java
@@ -0,0 +1,100 @@
+package at.dms.kjc.sir;
+
+import at.dms.kjc.*;
+import java.util.*;
+
+/**
+ * This is a stream visitor that does nothing.
+ */
+public class EmptyStreamVisitor implements StreamVisitor {
+
+    /**
+     * PLAIN-VISITS 
+     */
+	    
+    /* visit a filter */
+    public void visitFilter(SIRFilter self,
+			    SIRStream parent,
+			    JFieldDeclaration[] fields,
+			    JMethodDeclaration[] methods,
+			    JMethodDeclaration init,
+			    JMethodDeclaration work,
+			    CType inputType, CType outputType) {
+    }
+  
+    /* visit a splitter */
+    public void visitSplitter(SIRSplitter self,
+			      SIRStream parent,
+			      SIRSplitType type,
+			      JExpression[] weights) {
+    }
+    
+    /* visit a joiner */
+    public void visitJoiner(SIRJoiner self,
+			    SIRStream parent,
+			    SIRJoinType type,
+			    JExpression[] weights) {
+    }
+
+    /**
+     * PRE-VISITS 
+     */
+	    
+    /* pre-visit a pipeline */
+    public void preVisitPipeline(SIRPipeline self,
+				 SIRStream parent,
+				 JFieldDeclaration[] fields,
+				 JMethodDeclaration[] methods,
+				 JMethodDeclaration init,
+				 List elements) {
+    }
+
+    /* pre-visit a splitjoin */
+    public void preVisitSplitJoin(SIRSplitJoin self,
+				  SIRStream parent,
+				  JFieldDeclaration[] fields,
+				  JMethodDeclaration[] methods,
+				  JMethodDeclaration init) {
+    }
+
+    /* pre-visit a feedbackloop */
+    public void preVisitFeedbackLoop(SIRFeedbackLoop self,
+				     SIRStream parent,
+				     JFieldDeclaration[] fields,
+				     JMethodDeclaration[] methods,
+				     JMethodDeclaration init,
+				     int delay,
+				     JMethodDeclaration initPath) {
+    }
+
+    /**
+     * POST-VISITS 
+     */
+	    
+    /* post-visit a pipeline */
+    public void postVisitPipeline(SIRPipeline self,
+				  SIRStream parent,
+				  JFieldDeclaration[] fields,
+				  JMethodDeclaration[] methods,
+				  JMethodDeclaration init,
+				  List elements) {
+    }
+
+    /* post-visit a splitjoin */
+    public void postVisitSplitJoin(SIRSplitJoin self,
+				   SIRStream parent,
+				   JFieldDeclaration[] fields,
+				   JMethodDeclaration[] methods,
+				   JMethodDeclaration init) {
+    }
+
+    /* post-visit a feedbackloop */
+    public void postVisitFeedbackLoop(SIRFeedbackLoop self,
+				      SIRStream parent,
+				      JFieldDeclaration[] fields,
+				      JMethodDeclaration[] methods,
+				      JMethodDeclaration init,
+				      int delay,
+				      JMethodDeclaration initPath) {
+    }
+}

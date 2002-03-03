0dd84f580e14450e7d2f5b5dac1d5e893e5c1923
Bill Thies <bthies@gmail.com>
2002-03-03 03:10:40 +0000

    Adding an example that was meant to illustrate paramterizable round-robins, but for now illustrates a bug in the compiler.  We get the wrong output with weighted-rr splitters and joiners.  It'll show up in bugzilla in a sec...

---
+++ streams/apps/tests/weighted-rr/WeightedRR.java
@@ -0,0 +1,53 @@
+import streamit.*;
+
+class Source extends Filter {
+    int i;
+    public void init() {
+	output = new Channel(Integer.TYPE, 1);
+	i=0;
+    }
+    public void work() {
+	output.pushInt(i++);
+    }
+}
+
+class Sink extends Filter {
+    public void init() {
+	input = new Channel(Integer.TYPE, 1);
+    }
+    public void work() {
+	System.out.println(input.popInt());
+    }
+}
+
+class OneToOne extends Filter {
+    public void init() {
+	input = new Channel(Integer.TYPE, 1);
+	output = new Channel(Integer.TYPE, 1);
+    }
+    public void work() {
+	output.pushInt(input.popInt());
+    }
+}
+
+public class WeightedRR extends StreamIt {
+
+    public static void main(String[] args) {
+	new WeightedRR().run(args);
+    }
+
+    public void init() {
+	add(new Source());
+	add(new SplitJoin() {
+		public void init() {
+		    setSplitter(WEIGHTED_ROUND_ROBIN(2,2,2));
+		    this.add(new OneToOne());
+		    this.add(new OneToOne());
+		    this.add(new OneToOne());
+		    setJoiner(WEIGHTED_ROUND_ROBIN(2,2,2));
+		}
+		
+	    });
+	add(new Sink());
+    }
+}

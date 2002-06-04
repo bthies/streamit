30a85e13f04f3d065c2216636459a52261e30519
Bill Thies <bthies@gmail.com>
2002-06-04 01:53:24 +0000

    Adding a delay that is implemented as a feedback loop (thanks karczma)

---
+++ streams/apps/examples/delay/Delay.java
@@ -0,0 +1,30 @@
+import streamit.*;
+
+class Delay extends FeedbackLoop {
+
+    public Delay(int N) {
+	super(N);
+    }
+
+    public void init(int N) {
+	setSplitter(ROUND_ROBIN());
+	setDelay(N);
+	setBody(new Filter() {
+                public void init() {
+		    setInput(Integer.TYPE);setOutput(Integer.TYPE);
+		    setPush(2); setPop(2);
+                }
+                public void work() {
+		    this.output.pushInt(this.input.peekInt(1));
+		    this.output.pushInt(this.input.peekInt(0));
+		    this.input.pop();
+		    this.input.pop();
+                }
+	    });
+	setJoiner(ROUND_ROBIN());
+    }
+
+    public int initPathInt(int index) {
+	return 0;
+    }
+}

ac2829b534e501af5b94e28af3f3bef223c77c73
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-06-13 21:48:15 +0000

    Example implementation for a trellis encoder/decoder. A trellis decoder is one (small) part of HDTV decoders. This code works great with the java library, but I don't have high hopes for it compiling with the actual compiler (yet).

---
+++ streams/apps/examples/toy-trellis/ToyTrellis.java
@@ -0,0 +1,44 @@
+/*
+ * Simple trellis encoder/decoder system.
+ */
+
+import streamit.*;
+
+class ToyTrellis extends StreamIt
+{
+    static public void main (String [] t)
+    {
+        ToyTrellis test = new ToyTrellis ();
+        test.run (t);
+    }
+
+    public void init ()
+    {
+	//add (new DataSource());
+	//add (new Shifter());	
+	add (new ExampleSource());
+	add (new RateDoubler());
+	add (new SplitJoin() {
+		public void init() {
+		    setSplitter(WEIGHTED_ROUND_ROBIN(1,1));
+		    this.add(new TrellisPipeline());
+		    this.add(new OneToOne());
+		    setJoiner(WEIGHTED_ROUND_ROBIN(1,1));
+		}
+	    });
+	
+	//add (new UnShifter());	
+	//add (new CharPrinter());
+	add (new DownSample());
+	add (new IntPrinter());
+    }
+}
+
+
+class TrellisPipeline extends Pipeline {
+    public void init() {
+	add(new TrellisEncoder());
+	add(new TrellisDecoder(17));
+    }
+}
+

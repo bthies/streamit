6fd9386be468fc9f2b5dbbe4b72c36f467a2a9f1
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-06-18 19:55:47 +0000

    Field Propagation test programs (FieldPropTest, FieldPropTest2) and their associated regression test output.

--- streams/apps/tests/field-prop/FieldPropTest.java
+++ streams/apps/tests/field-prop/FieldPropTest.java
@@ -1,76 +1,77 @@
 /**
  * Simple application used for regression testing of
  * field propagation.
- * $Id: FieldPropTest.java,v 1.1 2002-06-18 17:09:32 aalamb Exp $
+ * $Id: FieldPropTest.java,v 1.2 2002-06-18 19:55:47 aalamb Exp $
  **/
 import streamit.*;
 
 class FieldPropTest extends StreamIt {
   static public void main (String [] t)
-    {
-        FieldPropTest test = new FieldPropTest ();
-        test.run (t);
-    }
-
-    public void init ()
-    {
-	add (new DataSource());
-	add (new OneToOne());
-	add (new OneToTwo());
-	add (new DataSink());
-    }
+  {
+    FieldPropTest test = new FieldPropTest ();
+    test.run (t);
+  }
+  
+  public void init ()
+  {
+    add (new DataSource());
+    add (new OneToOne());
+    add (new OneToTwo());
+    add (new DataSink());
+  }
 }
 
 class DataSource extends Filter {
-    int x;
-    public void init() {
-	// pushes 1 item onto the tape each iteration
-	output = new Channel(Integer.TYPE, 1); 
-	this.x = 0;
-    }
-    public void work() {
-	output.pushInt(this.x);
-	this.x++;
-    }
+  int x;
+  public void init() {
+    // pushes 1 item onto the tape each iteration
+    output = new Channel(Integer.TYPE, 1); 
+    this.x = 0;
+  }
+  public void work() {
+    output.pushInt(this.x);
+    this.x++;
+  }
 }
 
 class DataSink extends Filter {
-    public void init() {
-	// pops 1 item from the tape each iteration
-	input = new Channel(Integer.TYPE, 1); 
-    }
-
-    public void work() {
-	System.out.println(input.popInt());
-    }
+  public void init() {
+    // pops 1 item from the tape each iteration
+    input = new Channel(Integer.TYPE, 1); 
+  }
+  
+  public void work() {
+    System.out.println(input.popInt());
+  }
 }
-
-
 
 
 
 class OneToOne extends Filter {
-    static final int F = 6;
-    public void init() {
-	input = new Channel(Integer.TYPE, 1);
-	output= new Channel(Integer.TYPE, 1);
-    }
-
-    public void work() {
-	output.pushInt(input.popInt());
-    }
+  static final int F = 5;
+  public void init() {
+    input = new Channel(Integer.TYPE, 1);
+    output= new Channel(Integer.TYPE, 1);
+  }
+  
+  public void work() {
+    output.pushInt(input.popInt());
+  }
 }
 
 
 class OneToTwo extends Filter {
+  int G;
+  // sticking in something like int G = 5; doesn't work correctly;
   public void init() {
+    G = 6;
     input = new Channel(Integer.TYPE, 1);
     output= new Channel(Integer.TYPE, 2);
   }
   
   public void work() {
     int temp_value = input.popInt();
-    output.pushInt(temp_value);
-    output.pushInt(OneToOne.F);
+    output.pushInt(temp_value + OneToOne.F);
+    output.pushInt(temp_value + G);
   }
 }
---
+++ streams/apps/tests/field-prop/FieldPropTest2.java
@@ -0,0 +1,79 @@
+/**
+ * Simple application used for regression testing of
+ * field propagation. This one uses final fields that are not static
+ * and assigns them values in the init function.
+ * $Id: FieldPropTest2.java,v 1.1 2002-06-18 19:55:47 aalamb Exp $
+ **/
+import streamit.*;
+
+class FieldPropTest2 extends StreamIt {
+  static public void main (String [] t)
+    {
+        FieldPropTest2 test = new FieldPropTest2 ();
+        test.run (t);
+    }
+
+    public void init ()
+    {
+	add (new DataSource2());
+	add (new OneToOne2());
+	add (new OneToTwo2());
+	add (new DataSink2());
+    }
+}
+
+class DataSource2 extends Filter {
+    int x;
+    public void init() {
+	// pushes 1 item onto the tape each iteration
+	output = new Channel(Integer.TYPE, 1); 
+	this.x = 0;
+    }
+    public void work() {
+	output.pushInt(this.x);
+	this.x++;
+    }
+}
+
+class DataSink2 extends Filter {
+    public void init() {
+	// pops 1 item from the tape each iteration
+	input = new Channel(Integer.TYPE, 1); 
+    }
+
+    public void work() {
+	System.out.println(input.popInt());
+    }
+}
+
+
+
+
+
+class OneToOne2 extends Filter {
+    static final int F = 7;
+    public void init() {
+	input = new Channel(Integer.TYPE, 1);
+	output= new Channel(Integer.TYPE, 1);
+    }
+
+    public void work() {
+	output.pushInt(input.popInt());
+    }
+}
+
+
+class OneToTwo2 extends Filter {
+    static final int G = 6;
+    public void init() {
+	input = new Channel(Integer.TYPE, 1);
+	output= new Channel(Integer.TYPE, 2);
+  }
+  
+  public void work() {
+    int temp_value = input.popInt();
+    output.pushInt(temp_value + OneToOne2.F);
+    output.pushInt(temp_value + G);
+  }
+}
+

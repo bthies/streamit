6de7d70d594a09870b56e8a03fcbf4d700b33e51
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-06-20 21:18:17 +0000

    field initialization test cases and associated known output

---
+++ streams/apps/tests/field-init/FieldInit.java
@@ -0,0 +1,80 @@
+/**
+ * Simple StreamIT program with automatic initialization
+ * of field members (these are moved to the init function by the compiler);
+ **/
+import streamit.*;
+
+class FieldInit extends StreamIt {
+  static public void main (String [] t)
+    {
+        FieldInit test = new FieldInit ();
+        test.run (t);
+    }
+    public void init ()
+    {
+	add (new DataSource());
+	//add (new OneToOne());
+	//add (new OneToTwo());
+	add (new DataSink());
+    }
+}
+
+class DataSource extends Filter {
+    int x;
+    int FOO = 5;
+    int BAR[] = new int[FOO];
+    
+    public void init() {
+	// pushes 1 item onto the tape each iteration
+	int i = 5;
+	BAR[3] = 2;
+	output = new Channel(Integer.TYPE, 1); 
+	this.x = 0;
+    }
+    public void work() {
+	output.pushInt(this.x + BAR[3]);
+	this.x++;
+    }
+}
+
+class DataSink extends Filter {
+    public void init() {
+	// pops 1 item from the tape each iteration
+	input = new Channel(Integer.TYPE, 1); 
+    }
+
+    public void work() {
+	System.out.println(input.popInt());
+    }
+}
+
+
+
+
+
+
+
+class OneToOne extends Filter {
+    public void init() {
+	input = new Channel(Integer.TYPE, 1);
+	output= new Channel(Integer.TYPE, 1);
+    }
+
+    public void work() {
+	output.pushInt(input.popInt());
+    }
+}
+
+
+class OneToTwo extends Filter {
+  public void init() {
+    input = new Channel(Integer.TYPE, 1);
+    output= new Channel(Integer.TYPE, 2);
+  }
+  
+  public void work() {
+    int temp_value = input.popInt();
+    output.pushInt(temp_value);
+    output.pushInt(temp_value);
+  }
+}
---
+++ streams/apps/tests/field-init/FieldInit2.java
@@ -0,0 +1,42 @@
+/** Simple StreamIT program for testing purposes **/
+import streamit.*;
+
+class FieldInit2 extends StreamIt {
+  static public void main (String [] t)
+    {
+        FieldInit2 test = new FieldInit2 ();
+        test.run (t);
+    }
+
+    public void init ()
+    {
+	add (new DataSource2());
+	add (new DataSink2());
+    }
+}
+
+class DataSource2 extends Filter {
+    int x;
+    int[] FOO = new int[5];
+    public void init() {
+	// pushes 1 item onto the tape each iteration
+	int i; i = 5;
+	output = new Channel(Integer.TYPE, 1); 
+	this.x = 0;
+    }
+    public void work() {
+	output.pushInt(this.x + FOO[0]);
+	this.x++;
+    }
+}
+
+class DataSink2 extends Filter {
+    public void init() {
+	// pops 1 item from the tape each iteration
+	input = new Channel(Integer.TYPE, 1); 
+    }
+
+    public void work() {
+	System.out.println(input.popInt());
+    }
+}

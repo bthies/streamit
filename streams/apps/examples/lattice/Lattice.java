99047d2cb5bd3518bbdd37cf06902cead3499b8e
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-06-28 21:01:44 +0000

    Output for regression testing

--- streams/apps/examples/lattice/Lattice.java
+++ streams/apps/examples/lattice/Lattice.java
@@ -113,6 +113,35 @@ public void init() {
   }
 }
 
+class Delay extends FeedbackLoop {
+
+    public Delay(int N) {
+	super(N);
+    }
+
+    public void init(int N) {
+	setSplitter(ROUND_ROBIN());
+	setDelay(N);
+	setBody(new Filter() {
+                public void init() {
+		    input = new Channel(Float.TYPE, 2);
+		    output = new Channel(Float.TYPE, 2);
+                }
+                public void work() {
+		    this.output.pushFloat(this.input.peekFloat(1));
+		    this.output.pushFloat(this.input.peekFloat(0));
+		    this.input.popFloat();
+		    this.input.popFloat();
+                }
+	    });
+	setLoop(new FloatIdentity());
+	setJoiner(ROUND_ROBIN());
+    }
+
+    public float initPathFloat(int index) {
+	return 0.0f;
+    }
+}
 
    
 

1b0eb11347ebbc7b6fd8724424f9ecd06b8c9fef
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-07-02 19:55:18 +0000

    test cases

---
+++ streams/apps/tests/field-init/FieldInit4.java
@@ -0,0 +1,59 @@
+/**
+ * Simple StreamIT program with automatic initialization
+ * of field members (these are moved to the init function by the compiler);
+ * This file tests that the field declaration and initializations
+ * in PipeLines, Splitters and Joiners, and RoundRobin work correctly
+ **/
+import streamit.*;
+
+class FieldInit4 extends StreamIt {
+  static public void main (String [] t)
+    {
+        FieldInit4 test = new FieldInit4 ();
+        test.run (t);
+    }
+    public void init ()
+    {
+	add(new SimplePipe());
+    }
+}
+
+class SimplePipe extends Pipeline {
+    int offset = 7;
+    public void init() {
+	int x = 3 + offset;
+	add (new DataSource(x));
+	add (new DataSink());
+    }
+}
+
+class DataSource extends Filter {
+    int x = 5;
+
+    public DataSource(int num) {
+	super(num);
+
+    }
+    
+    public void init(int num) {
+	// pushes 1 item onto the tape each iteration
+	output = new Channel(Integer.TYPE, 1); 
+	this.x += num;
+    }
+
+    public void work() {
+	output.pushInt(this.x);
+	this.x++;
+    }
+}
+
+class DataSink extends Filter {
+    public void init() {
+	// pops 1 item from the tape each iteration
+	input = new Channel(Integer.TYPE, 1); 
+    }
+
+    public void work() {
+	System.out.println(input.popInt());
+    }
+}

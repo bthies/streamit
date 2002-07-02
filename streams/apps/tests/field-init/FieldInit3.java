7b5fd26c6a4d2c118724b2e0563b0e51ab63b0bc
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-07-02 18:01:18 +0000

    New Field initialization test and output. Through this I found out the hard way that the compiler doesn't  allow named constructors... Instead to pass a value to a filter, you need to:
    1. call the super class's constructor with the arguments
    2. add a formal parameter to the init method
    
    
    writing code like this:
    
    DataSource(int num) {
      int k = num;
    }
    void init() {}
    
    died in ConstProp (specifically line 174) because arguments were passed to the init function that were not formal arguments.
    
    
    you need to write code like this:
    DataSource(int num) {
      super(num)
    }
    void init(int anotherNum) {}
      int k = anotherNum;
    }
    
    While everyone probably already knows this, I got stuck for a while until I asked David. I just wanted to write it down.

---
+++ streams/apps/tests/field-init/FieldInit3.java
@@ -0,0 +1,55 @@
+/**
+ * Simple StreamIT program with automatic initialization
+ * of field members (these are moved to the init function by the compiler);
+ **/
+import streamit.*;
+
+class FieldInit3 extends StreamIt {
+  static public void main (String [] t)
+    {
+        FieldInit3 test = new FieldInit3 ();
+        test.run (t);
+    }
+    public void init ()
+    {
+	add(new SimplePipe());
+    }
+}
+
+class SimplePipe extends Pipeline {
+    public void init() {
+	add (new DataSource(1));
+	add (new DataSink());
+    }
+}
+
+class DataSource extends Filter {
+    int x;
+
+    public DataSource(int num) {
+	super(num);
+
+    }
+    
+    public void init(int num) {
+	// pushes 1 item onto the tape each iteration
+	output = new Channel(Integer.TYPE, 1); 
+	this.x = num;
+    }
+
+    public void work() {
+	output.pushInt(this.x);
+	this.x++;
+    }
+}
+
+class DataSink extends Filter {
+    public void init() {
+	// pops 1 item from the tape each iteration
+	input = new Channel(Integer.TYPE, 1); 
+    }
+
+    public void work() {
+	System.out.println(input.popInt());
+    }
+}

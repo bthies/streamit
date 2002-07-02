3a17bad611f6486b9e4b10c2afdb11fe048c5e9a
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-07-02 19:10:44 +0000

    Cleaned up the initialization code mover (I am starting to get what the deal with visitors is now).
    Also fixed a bug: only field initializations for fields in filters (not pipelines, splitjoins, or feedpackloops) were being moved by the mover. I fixed that.

--- streams/src/at/dms/kjc/sir/lowering/FieldInitMover.java
+++ streams/src/at/dms/kjc/sir/lowering/FieldInitMover.java
@@ -18,80 +18,121 @@ import java.util.*;
  * int i;
  * i = 5;
  * </pre>
- * $Id: FieldInitMover.java,v 1.3 2002-06-28 06:50:59 thies Exp $
+ * $Id: FieldInitMover.java,v 1.4 2002-07-02 19:10:44 aalamb Exp $
  **/
-public class FieldInitMover extends EmptyStreamVisitor{
-
+public class FieldInitMover extends EmptyStreamVisitor {
+  
     public static void moveStreamInitialAssignments(SIRStream str) {
 	FieldInitMover mover = new FieldInitMover();
 	IterFactory.createIter(str).accept(mover);
     }
 
+
     /*
-     * Visit a filter -- move any field initialization out of the field
+     * Visit each stream construct (eg Filter, Pipeline, SplitJoin or
+     * FeedBackLoop) and move any field initialization out of the field
      * declaration into the body of the init.
      */
-    public void visitFilter(SIRFilter filter,
-			    SIRFilterIter iter) {
+    public void visitStream(SIRStream self,
+			    SIRIterator iter) {
+	System.out.println("!!visiting stream: " + self);
+	
+	moveFieldInitializations(self);
+	
+    }
 
-	// assignment statements that need to be added to the init function 
-	final Vector assignmentStatements = new Vector();
+    /**
+     * Does the actual moving of field initializations
+     * from their declarations to the init function.
+     **/
+    private void moveFieldInitializations(SIRStream filter) {
+	// get a visitor that will walk down the filter, replacing fields as it goes
+	FieldInitMoverVisitor harvester = new FieldInitMoverVisitor();
+
+	// for each field declaration, harvest any initialization expressions
 	JFieldDeclaration[] fields = filter.getFields();
 	for (int i=0; i<fields.length; i++) {
-	    	    
-	    // go and visit all of the field declarations
-	    fields[i].accept(new SLIRReplacingVisitor() {
-		    public Object visitFieldDeclaration(JFieldDeclaration self,
-							int modifiers,
-							CType type,
-							String ident,
-							JExpression expr)
-		    {
-			// if this field declaration has an initial value,
-			// make an assignment expression to stick in the
-			// init function
-			//System.out.println("Initial expression for field: " + expr);
-			if (expr != null) {
-			    // build up the this.field = initalValue expression
-			    
-			    
-			    JThisExpression thisExpr = new JThisExpression (self.getTokenReference());
-			    JFieldAccessExpression fieldExpr;
-			    fieldExpr = new JFieldAccessExpression(self.getTokenReference(),
-								   thisExpr,
-								   ident);
-			    
-			    JAssignmentExpression assignExpr;
-			    assignExpr = new JAssignmentExpression(self.getTokenReference(),
-								   fieldExpr,
-								   expr);
-			    
-			    JExpressionStatement assignStmt;
-			    assignStmt = new JExpressionStatement(self.getTokenReference(),
-								  assignExpr,
-								  null);
-			    // add the new statement to the list of statements we are going to
-			    // add to the init function
-			    assignmentStatements.add(assignStmt);
-			    
-			    // mutate the field so that it has no initializer expression
-			    self.getVariable().setExpression(null);
-			}
-		    return self;
-		    }
-		}); // end crazy anonymous class
-	} // end for loop
+	    fields[i].accept(harvester);
+	}
 	
 	// now, we have to add the initializing assignment statements
 	// to the beginning of the init function.
 	// Reverses the order of the initializations (because we see the fields
 	// in reverse order above)
+	Vector newStatements = harvester.getAssignmentStatements();
 	if(filter.getInit()!=null) {
 	    JBlock body = filter.getInit().getBody();
-	    for (int i=0; i<assignmentStatements.size(); i++) {
+	    for (int i=0; i<newStatements.size(); i++) {
 		body.addStatement(0,
-				  (JStatement)assignmentStatements.elementAt(i));
+				  (JStatement)newStatements.elementAt(i));
 	    }
 	}
     }
+
+}
+
+
+/**
+ * A visitor class that goes throught the field declarations
+ * of a filter removing any initialization statements
+ * and generating a list of equivalent statements to add
+ * to the start of the init function.
+ **/
+
+class FieldInitMoverVisitor extends SLIRReplacingVisitor {
+    /** Assignments that need to be added to the initializations statements **/
+    Vector assignmentStatements;
+
+    FieldInitMoverVisitor() {
+	super();
+	this.assignmentStatements = new Vector();
+    }
+
+    public Vector getAssignmentStatements() {
+	return this.assignmentStatements;
+    }
+
+    /**
+     * Visit a field declaration. Mutates the initialization expr if
+     * present and creates the appropriate initialization statement instead.
+     **/
+    public Object visitFieldDeclaration(JFieldDeclaration self,
+					int modifiers,
+					CType type,
+					String ident,
+					JExpression expr)
+	{
+	    System.out.println("!!Visiting field: " + self);
+	    // if this field declaration has an initial value,
+	    // make an assignment expression to stick in the
+	    // init function
+	    //System.out.println("Initial expression for field: " + expr);
+	    if (expr != null) {
+		// build up the this.field = initalValue expression
+		
+		
+		JThisExpression thisExpr = new JThisExpression (self.getTokenReference());
+		JFieldAccessExpression fieldExpr;
+		fieldExpr = new JFieldAccessExpression(self.getTokenReference(),
+						       thisExpr,
+						       ident);
+		
+		JAssignmentExpression assignExpr;
+		assignExpr = new JAssignmentExpression(self.getTokenReference(),
+						       fieldExpr,
+						       expr);
+			    
+		JExpressionStatement assignStmt;
+		assignStmt = new JExpressionStatement(self.getTokenReference(),
+						      assignExpr,
+						      null);
+		// add the new statement to the list of statements we are going to
+		// add to the init function
+		assignmentStatements.add(assignStmt);
+		
+		// mutate the field so that it has no initializer expression
+		self.getVariable().setExpression(null);
+	    }
+	    return self;
+	}
 }

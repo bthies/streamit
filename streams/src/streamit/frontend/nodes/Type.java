75e62e63c12f5496f7420af80ad6521e31b15f60
David Maze <dmaze@cag.csail.mit.edu>
2002-07-15 18:50:58 +0000

    Add isComplex() query, since this is a very common operation.

--- streams/src/streamit/frontend/nodes/Type.java
+++ streams/src/streamit/frontend/nodes/Type.java
@@ -1,7 +1,7 @@
 /*
  * Type.java: base class for variable data types
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: Type.java,v 1.1 2002-07-15 15:44:08 dmaze Exp $
+ * $Id: Type.java,v 1.2 2002-07-15 18:50:58 dmaze Exp $
  */
 
 package streamit.frontend.nodes;
@@ -11,4 +11,6 @@ package streamit.frontend.nodes;
  */
 public abstract class Type
 {
+    /** Returns true if this type is a complex type. */
+    public boolean isComplex() { return false; }
 }

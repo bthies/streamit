03ed915a077088a2f28518957000002161736739
David Maze <dmaze@cag.csail.mit.edu>
2002-07-15 18:52:22 +0000

    Add "double" and "void"; implement isComplex() to possibly return true.

--- streams/src/streamit/frontend/nodes/TypePrimitive.java
+++ streams/src/streamit/frontend/nodes/TypePrimitive.java
@@ -1,7 +1,7 @@
 /*
  * TypePrimitive.java: a primitive data type
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: TypePrimitive.java,v 1.1 2002-07-15 15:44:08 dmaze Exp $
+ * $Id: TypePrimitive.java,v 1.2 2002-07-15 18:52:22 dmaze Exp $
  */
 
 package streamit.frontend.nodes;
@@ -14,7 +14,9 @@ public class TypePrimitive extends Type
 {
     public static final int TYPE_INT = 1;
     public static final int TYPE_FLOAT = 2;
-    public static final int TYPE_COMPLEX = 3;
+    public static final int TYPE_DOUBLE = 3;
+    public static final int TYPE_COMPLEX = 4;
+    public static final int TYPE_VOID = 5;
     
     private int type;
 
@@ -26,5 +28,10 @@ public class TypePrimitive extends Type
     public int getType()
     {
         return type;
+    }
+
+    public boolean isComplex()
+    {
+        return type == TYPE_COMPLEX;
     }
 }

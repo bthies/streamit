e68d1da9539c4090ef3ca06be20cbb720e3deff8
David Maze <dmaze@cag.csail.mit.edu>
2002-07-16 15:06:11 +0000

    Use GetExprType to determine the types of variables and array references.  Possibly walk into real and imaginary parts of array members as well as fields and arrays.

--- streams/src/streamit/frontend/tojava/VarToComplex.java
+++ streams/src/streamit/frontend/tojava/VarToComplex.java
@@ -1,7 +1,7 @@
 /*
  * VarToComplex.java: split variables into separate real/complex parts
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: VarToComplex.java,v 1.3 2002-07-15 19:14:17 dmaze Exp $
+ * $Id: VarToComplex.java,v 1.4 2002-07-16 15:06:11 dmaze Exp $
  */
 
 package streamit.frontend.tojava;
@@ -15,16 +15,17 @@ import streamit.frontend.nodes.*;
 public class VarToComplex extends FEReplacer
 {
     private SymbolTable symtab;
+    private GetExprType getExprType;
     
     public VarToComplex(SymbolTable st)
     {
         symtab = st;
+        getExprType = new GetExprType(st);
     }
     
     public Object visitExprVar(ExprVar exp)
     {
-        String name = exp.getName();
-        Type type = symtab.lookup(name);
+        Type type = (Type)exp.accept(getExprType);
         if (type.isComplex())
         {
             Expression real = new ExprField(exp, "real");
@@ -49,5 +50,20 @@ public class VarToComplex extends FEReplacer
         }
         // Otherwise recurse normally.
         return super.visitExprField(exp);
+    }
+
+    public Object visitExprArray(ExprArray exp)
+    {
+        // If the type of the expression is complex, decompose it;
+        // otherwise, move on.
+        Type type = (Type)exp.accept(getExprType);
+        if (type.isComplex())
+        {
+            Expression real = new ExprField(exp, "real");
+            Expression imag = new ExprField(exp, "imag");
+            return new ExprComplex(real, imag);
+        }
+        else
+            return exp;
     }
 }

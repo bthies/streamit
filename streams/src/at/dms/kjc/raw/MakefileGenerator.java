1525080b7a2feb469f749908f5645e6a240747a0
David Maze <dmaze@cag.csail.mit.edu>
2002-07-24 21:41:06 +0000

    Always write a fileio.bc file.  Include the FLOP-counting code in it. This is the same code that's been being passed around in email, with an additional count_FLOPS(cycles) function.  So either set gFLOPS=0, run some code, and examine gFLOPS, or do count_FLOPS(n), which does the same thing and prints the mFLOPS number.

--- streams/src/at/dms/kjc/raw/MakefileGenerator.java
+++ streams/src/at/dms/kjc/raw/MakefileGenerator.java
@@ -39,10 +39,11 @@ public class MakefileGenerator
 	    fw.write("SIM-CYCLES = 500000\n\n");
 	    fw.write("include $(TOPDIR)/Makefile.include\n\n");
 	    fw.write("RGCCFLAGS += -O3\n\n");
-	    if (FileVisitor.foundReader || FileVisitor.foundWriter) {
-		fw.write("BTL-MACHINE-FILE = fileio.bc\n\n");
-		createBCFile();
-	    }
+            fw.write("BTL-MACHINE-FILE = fileio.bc\n\n");
+	    if (FileVisitor.foundReader || FileVisitor.foundWriter)
+		createBCFile(true);
+            else
+                createBCFile(false);
 	    if (StreamItOptions.rawRows > 4)
 		fw.write("TILE_PATTERN=8x8\n\n");
 	    fw.write("TILES = ");
@@ -84,71 +85,97 @@ public class MakefileGenerator
 	    }
     }
     
-    private static void createBCFile() throws Exception 
+    private static void createBCFile(boolean hasIO) throws Exception 
     {
 	FileWriter fw = new FileWriter("fileio.bc");
 	
 	fw.write("include(\"<dev/basic.bc>\");\n");
+
+        fw.write
+            ("global gFLOPS = 0;\n" +
+             "fn __clock_handler(hms)\n" +
+             "{\n" +
+             "  local i;\n" +
+             "  for (i = 0; i < gNumProc; i++)\n" +
+             "  {\n" +
+             "    gFLOPS += imem_instr_is_fpu(get_imem_instr(i, get_pc_for_proc(i)));\n" +
+             "  }\n" +
+             "}\n" +
+             "\n" +
+             "EventManager_RegisterHandler(\"clock\", \"__clock_handler\");\n" +
+             "\n" +
+             "fn count_FLOPS(steps)\n" +
+             "{\n" +
+             "  gFLOPS = 0;\n" +
+             "  step(steps);\n" +
+             "  printf(\"// **** count_FLOPS: %4d FLOPS, %4d mFLOPS\n\",\n" +
+             "         gFLOPS, (250*gFLOPS)/steps);\n" +
+             "}\n" +
+             "\n");
 	
-	// create preamble
-	fw.write("if (FindFunctionInSymbolHash(gSymbolTable, \"dev_data_transmitter_init\",3) == NULL)\n");
-	fw.write("include(\"<dev/data_transmitter.bc>\");\n\n");
+        if (hasIO)
+        {
+            // create preamble
+            fw.write("if (FindFunctionInSymbolHash(gSymbolTable, \"dev_data_transmitter_init\",3) == NULL)\n");
+            fw.write("include(\"<dev/data_transmitter.bc>\");\n\n");
 
-	// create the instrumentation function
-	fw.write("// instrumentation code\n");
-	fw.write("fn streamit_instrument(val){\n");
-	fw.write("  local a;\n"); 
-	fw.write("  local b;\n");
-	fw.write("  Proc_GetCycleCounter(Machine_GetProc(machine,0), &a, &b);\n");
-	fw.write("  //printf(\"cycleHi %X, cycleLo %X\\n\", a, b);\n");
-	// use the same format string that generating a printf causes so we can use
-	// the same results script;
-	fw.write("  printf(\"[00: %08x%08x]: %d\\n\", a, b, val);\n");
-	fw.write("}\n\n");
+            // create the instrumentation function
+            fw.write("// instrumentation code\n");
+            fw.write("fn streamit_instrument(val){\n");
+            fw.write("  local a;\n"); 
+            fw.write("  local b;\n");
+            fw.write("  Proc_GetCycleCounter(Machine_GetProc(machine,0), &a, &b);\n");
+            fw.write("  //printf(\"cycleHi %X, cycleLo %X\\n\", a, b);\n");
+            // use the same format string that generating a printf causes so we can use
+            // the same results script;
+            fw.write("  printf(\"[00: %08x%08x]: %d\\n\", a, b, val);\n");
+            fw.write("}\n\n");
 
 
-	//create the function to write the data
-	fw.write("fn dev_st_port_to_file_size(filename, size, port)\n{\n");
-	fw.write("local receive_device_descriptor = hms_new();\n");
-	fw.write("// open the file\n  ;");
-	fw.write("receive_device_descriptor.fileName = filename;\n  ");
-	fw.write("receive_device_descriptor.theFile = fopen(receive_device_descriptor.fileName,\"w\");\n");
-	fw.write("verify(receive_device_descriptor.theFile != NULL, \"### Failed to open output file\");\n");
-	fw.write("receive_device_descriptor.calc =\n");
-	fw.write("& fn(this)\n  {\n");
-	fw.write("local theFile = this.theFile;\n");
-	fw.write("while (1)\n {\n");
-	fw.write("     local value = this.receive();\n");
-	fw.write("     fwrite(&value, size, 1, theFile);\n");
-	fw.write("     streamit_instrument(value);\n");
-	fw.write("     fflush(theFile);\n");
-	fw.write("}\n");
-	fw.write("};\n");
-	fw.write("return dev_data_transmitter_init(\"st_port_to_file\", port,0,receive_device_descriptor);\n");
-	fw.write("}");
+            //create the function to write the data
+            fw.write("fn dev_st_port_to_file_size(filename, size, port)\n{\n");
+            fw.write("local receive_device_descriptor = hms_new();\n");
+            fw.write("// open the file\n  ;");
+            fw.write("receive_device_descriptor.fileName = filename;\n  ");
+            fw.write("receive_device_descriptor.theFile = fopen(receive_device_descriptor.fileName,\"w\");\n");
+            fw.write("verify(receive_device_descriptor.theFile != NULL, \"### Failed to open output file\");\n");
+            fw.write("receive_device_descriptor.calc =\n");
+            fw.write("& fn(this)\n  {\n");
+            fw.write("local theFile = this.theFile;\n");
+            fw.write("while (1)\n {\n");
+            fw.write("     local value = this.receive();\n");
+            fw.write("     fwrite(&value, size, 1, theFile);\n");
+            fw.write("     streamit_instrument(value);\n");
+            fw.write("     fflush(theFile);\n");
+            fw.write("}\n");
+            fw.write("};\n");
+            fw.write("return dev_data_transmitter_init(\"st_port_to_file\", port,0,receive_device_descriptor);\n");
+            fw.write("}");
 
-	fw.write("\n{\n");
+            fw.write("\n{\n");
 
-	//generate the code for the fileReaders
-	Iterator frs = FileVisitor.fileReaders.iterator();
-	while (frs.hasNext()) {
-	    FlatNode node = (FlatNode)frs.next();
-	    SIRFileReader fr = (SIRFileReader)node.contents;
-	    fw.write("\tdev_serial_rom_init(\"" + fr.getFileName() + "\", " + 
-		     getIOPort(Layout.getTile(node)) + 
-		     ", 1);\n");
-	}
-	//generate the code for the file writers
-	Iterator fws = FileVisitor.fileWriters.iterator();
-	while (fws.hasNext()) {
-	    FlatNode node = (FlatNode)fws.next();
-	    SIRFileWriter sfw = (SIRFileWriter)node.contents;
-	    int size = getTypeSize(((SIRFileWriter)node.contents).getInputType());
-	    fw.write("\tdev_st_port_to_file_size(\"" + sfw.getFileName() + "\", " + 
-		     size + ", " + getIOPort(Layout.getTile(node)) + 
-		     ");\n");
-	}
-	fw.write("\n}\n");
+            //generate the code for the fileReaders
+            Iterator frs = FileVisitor.fileReaders.iterator();
+            while (frs.hasNext()) {
+                FlatNode node = (FlatNode)frs.next();
+                SIRFileReader fr = (SIRFileReader)node.contents;
+                fw.write("\tdev_serial_rom_init(\"" + fr.getFileName() +
+                         "\", " + getIOPort(Layout.getTile(node)) + 
+                         ", 1);\n");
+            }
+            //generate the code for the file writers
+            Iterator fws = FileVisitor.fileWriters.iterator();
+            while (fws.hasNext()) {
+                FlatNode node = (FlatNode)fws.next();
+                SIRFileWriter sfw = (SIRFileWriter)node.contents;
+                int size = getTypeSize(((SIRFileWriter)node.contents).getInputType());
+                fw.write("\tdev_st_port_to_file_size(\"" + sfw.getFileName() +
+                         "\", " + size + ", " +
+                         getIOPort(Layout.getTile(node)) + ");\n");
+            }
+            fw.write("\n}\n");
+        }
+        
 	fw.close();
     }
     

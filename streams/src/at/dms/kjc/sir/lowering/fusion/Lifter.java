29ea83d59fd37357bcbc56418a37db7ac5628ef6
Bill Thies <bthies@gmail.com>
2002-07-26 22:40:58 +0000

    Making the lifter a visitor.

--- streams/src/at/dms/kjc/sir/lowering/fusion/Lifter.java
+++ streams/src/at/dms/kjc/sir/lowering/fusion/Lifter.java
@@ -6,6 +6,7 @@ import streamit.scheduler.simple.*;
 import at.dms.util.IRPrinter;
 import at.dms.util.Utils;
 import at.dms.kjc.*;
+import at.dms.kjc.iterator.*;
 import at.dms.kjc.sir.*;
 import at.dms.kjc.sir.lowering.*;
 import at.dms.kjc.lir.*;
@@ -17,7 +18,9 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
-public class Lifter {
+public class Lifter implements StreamVisitor {
+
+    private Lifter() {}
 
     /**
      * Given that <pipe> is a pipeline containing only a single
@@ -35,23 +38,101 @@ public class Lifter {
 	// rename the contents of <filter>
 	new RenameAll().renameFilterContents(filter);
 
-	// add a method call to filter's <init> from <pipe's> init function
-	pipe.getInit().addStatement(
+	// add a method call to filter's <init> from <pipe's> init
+	// function 
+	if (pipe.getInit()!=null) {
+	    pipe.getInit().addStatement(
 		    new JExpressionStatement(null,
 			     new JMethodCallExpression(null, 
 			     new JThisExpression(null),
 			     filter.getInit().getName(),
 			     (JExpression[])pipe.getParams(pipe.indexOf(filter)).toArray(new JExpression[0])),
 				     null));
+	    filter.setInitWithoutReplacement(pipe.getInit());
+	}
 
 	// add all the methods and fields of <pipe> to <filter>
 	filter.addFields(pipe.getFields());
 	filter.addMethods(pipe.getMethods());
-	filter.setInitWithoutReplacement(pipe.getInit());
 
-	// set <pipe>'s old init function as <filter>'s init function
-
+	SIRContainer parent = pipe.getParent();
 	// in parent, replace <pipe> with <filter>
-	pipe.getParent().replace(pipe, filter);
+	parent.replace(pipe, filter);
+
+	// set new arguments to <filter>
+	parent.setParams(parent.indexOf(filter), 
+			 pipe.getParams(pipe.indexOf(filter)));
+    }
+
+    /**
+     * Lift everything we can in <str> and its children
+     */
+    public static void lift(SIRStream str) {
+	IterFactory.createIter(str).accept(new Lifter());
+    }
+
+    /**
+     * PLAIN-VISITS 
+     */
+	    
+    /* visit a filter */
+    public void visitFilter(SIRFilter self,
+			    SIRFilterIter iter) {
+    }
+  
+    /**
+     * PRE-VISITS 
+     */
+	    
+    /* pre-visit a pipeline */
+    public void preVisitPipeline(SIRPipeline self,
+				 SIRPipelineIter iter) {
+	liftChildren(self);
+    }
+
+    /* pre-visit a splitjoin */
+    public void preVisitSplitJoin(SIRSplitJoin self,
+				  SIRSplitJoinIter iter) {
+	liftChildren(self);
+    }
+
+    /* pre-visit a feedbackloop */
+    public void preVisitFeedbackLoop(SIRFeedbackLoop self,
+				     SIRFeedbackLoopIter iter) {
+	liftChildren(self);
+    }
+
+    /**
+     * POST-VISITS 
+     */
+	    
+    /* post-visit a pipeline */
+    public void postVisitPipeline(SIRPipeline self,
+				  SIRPipelineIter iter) {
+	liftChildren(self);
+    }
+
+    /* post-visit a splitjoin */
+    public void postVisitSplitJoin(SIRSplitJoin self,
+				   SIRSplitJoinIter iter) {
+	liftChildren(self);
+    }
+
+    /* post-visit a feedbackloop */
+    public void postVisitFeedbackLoop(SIRFeedbackLoop self,
+				      SIRFeedbackLoopIter iter) {
+	liftChildren(self);
+    }
+
+    private void liftChildren(SIRContainer str) {
+	for (ListIterator it = str.getChildren().listIterator(); it.hasNext(); ) {
+	    SIROperator child = (SIROperator)it.next();
+	    if (child instanceof SIRPipeline) {
+		SIRPipeline pipe = (SIRPipeline)child;
+		if (pipe.size()==1 && pipe.get(0) instanceof SIRFilter && pipe.getParent()!=null) {
+		    eliminatePipe(pipe);
+		}
+	    }
+	}
     }
 }

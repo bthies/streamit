acd36ed47e5498d682c10aac8c19a40cc129dde0
David Maze <dmaze@cag.csail.mit.edu>
2002-07-30 01:34:22 +0000

    Okay, implemented new FIR-fine code in C.

--- streams/apps/benchmarks/fir/c/firref.c
+++ streams/apps/benchmarks/fir/c/firref.c
@@ -1,7 +1,7 @@
 /*
  * firref.c: C reference implementation of FIR
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: firref.c,v 1.1 2002-07-30 00:35:54 dmaze Exp $
+ * $Id: firref.c,v 1.2 2002-07-30 01:34:22 dmaze Exp $
  */
 
 #ifdef raw
@@ -15,6 +15,7 @@
 #include <math.h>
 
 void begin(void);
+float calc_fir(const float *a, float *b, float W, float last);
 
 /* Globals: */
 static int numiters = -1;
@@ -38,29 +39,62 @@ int main(int argc, char **argv)
 }
 #endif
 
+#define DEPTH 128
+#define LENGTH 12
+
 void begin(void)
 {
   int val = 0;
   const int limit = 10000;
-  float c, s;
   int i;
-  float W[64];
+  float W[DEPTH], last[DEPTH];
+  float f1[LENGTH], f2[LENGTH];
+  float *a, *b, *c;
   
-  for (i = 0; i < 64; i++)
+  for (i = 0; i < DEPTH; i++)
+  {
     W[i] = (float)(2*i*i) / (float)(i+1);
+    last[i] = 0;
+  }
+  a = f1;
+  b = f2;
   
   /* Main loop: */
   while (numiters == -1 || numiters-- > 0)
   {
-    c = val++;
-    if (val >= limit) val = 0;
-    s = 0.0;
-    for (i = 0; i < 64; i++)
-      s = s + c * W[i];
+    for (i = 0; i < LENGTH; i += 2)
+    {
+      a[i] = 0;
+      a[i+1] = val++;
+      if (val >= limit) val = 0;
+    }
+    for (i = 0; i < DEPTH; i++)
+    {
+      last[i] = calc_fir(a, b, W[i], last[i]);
+      c = a;
+      a = b;
+      b = c;
+    }
+      
+    for (i = 0; i < LENGTH; i += 2)
 #ifdef raw
-    print_float(s);
+      print_float(a[i]);
 #else
-    printf("%f\n", s);
+      printf("%f\n", a[i]);
 #endif
   }
+}
+
+float calc_fir(const float *a, float *b, float W, float last)
+{
+  int i;
+  
+  for (i = 0; i < LENGTH/2; i++)
+  {
+    float s = a[i*2];
+    b[i*2] = s + last * W;
+    b[i*2+1] = last;
+    last = a[i*2+1];
+  }
+  return last;
 }

3828c9677b7446ffddc668fe10ab2c653648f46f
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-08-15 20:36:13 +0000

    Modified tests for the framework of the linear analysis.

--- streams/regtest/streamittest/TestLinear.java
+++ streams/regtest/streamittest/TestLinear.java
@@ -4,12 +4,13 @@ import junit.framework.*;
 import at.dms.kjc.sir.linear.ComplexNumber;
 import at.dms.kjc.sir.linear.FilterMatrix;
 import at.dms.kjc.sir.linear.FilterVector;
+import at.dms.kjc.sir.linear.LinearForm;
 
 
 /**
  * Regression test for linear filter extraction and
  * manipulation framework.
- * $Id: TestLinear.java,v 1.3 2002-08-14 19:13:40 aalamb Exp $
+ * $Id: TestLinear.java,v 1.4 2002-08-15 20:36:13 aalamb Exp $
  **/
 
 public class TestLinear extends TestCase {
@@ -22,10 +23,14 @@ public class TestLinear extends TestCase {
 	suite.addTest(new TestLinear("testSimple"));
 	suite.addTest(new TestLinear("testComplexNumberCreationAndAccess"));
 	suite.addTest(new TestLinear("testComplexNumberEquality"));
+	suite.addTest(new TestLinear("testComplexNumberNegation"));
+	suite.addTest(new TestLinear("testComplexNumberAddition"));
 	suite.addTest(new TestLinear("testFilterMatrixConstructor"));
 	suite.addTest(new TestLinear("testFilterMatrixAccessor"));
 	suite.addTest(new TestLinear("testFilterMatrixModifier"));
 	suite.addTest(new TestLinear("testFilterVector"));
+	suite.addTest(new TestLinear("testLinearForm"));
+	suite.addTest(new TestLinear("testLinearFormAddition"));
 	
 	
 	return suite;
@@ -83,8 +88,56 @@ public class TestLinear extends TestCase {
 	cn2 = new ComplexNumber(-.32453252342142523, .78881282845324);
 	assertTrue("not always equal", !cn1.equals(cn2));
 	assertTrue("not always equal", !cn2.equals(cn1));
-
     }
+
+    /** test that negating a complex number works **/
+    public void testComplexNumberNegation() {
+	ComplexNumber cn1;
+	ComplexNumber cn2;
+
+	//make sure that nothing screwy wiht zero is happening
+	assertTrue("zero is fine", ComplexNumber.ZERO.negate().equals(ComplexNumber.ZERO));
+	
+	cn1 = new ComplexNumber(1.34879759847, 21324);
+	cn2 = cn1.negate();
+	assertTrue("negation not equal", !cn1.equals(cn2));
+
+	// we should also be able to negate a number twice and have it be equal
+	assertTrue("double negation", cn1.equals(cn1.negate().negate()));
+	assertTrue("double negation", cn2.equals(cn2.negate().negate()));
+	
+    }
+
+    /** test adding complex numbers together **/
+    public void testComplexNumberAddition() {
+	ComplexNumber cn1;
+	ComplexNumber cn2;
+	ComplexNumber cn3;
+
+	ComplexNumber zero = ComplexNumber.ZERO;
+	ComplexNumber one = ComplexNumber.ONE;
+	ComplexNumber negativeOne = one.negate();
+
+	// make sure that adding zero to itself is still zero
+	assertTrue("0+0", zero.plus(zero).equals(zero));
+	// make sure that one is fine too
+	assertTrue("1+0", one.plus(zero).equals(one));	
+	assertTrue("0+1", zero.plus(one).equals(one));
+
+	// make sure that adding the negative of a number to itself is zero
+	assertTrue("1+-1", one.plus(negativeOne).equals(zero));
+	
+	// test some random numbers
+	cn1 = new ComplexNumber(1.2, 3.2);
+	cn2 = new ComplexNumber(-1.2, -3);
+	cn3 = new ComplexNumber(.4, -.3);
+	assertTrue(cn1.plus(cn2).equals(new ComplexNumber(0, .2)));
+	assertTrue(cn2.plus(cn1).equals(new ComplexNumber(0, .2)));
+	assertTrue(cn1.plus(cn3).equals(new ComplexNumber(1.6, 2.9)));
+	assertTrue(cn3.plus(cn1).equals(new ComplexNumber(1.6, 2.9))); 
+	assertTrue(cn2.plus(cn3).equals(new ComplexNumber(-.8, -3.3)));
+	assertTrue(cn3.plus(cn2).equals(new ComplexNumber(-.8, -3.3))); 
+    }       
 
     
     /** FilterMatrix tests **/
@@ -218,12 +271,96 @@ public class TestLinear extends TestCase {
 			// make sure that this element is the same
 			assertTrue("elt in vector is same",
 				   fv.getElement(j).equals(new ComplexNumber(a,b)));
+			// make sure that the size is correct
+			assertTrue("Correct size", fv.getSize() == i);
 		    }
 		}
 	    }
 	}
     }
 
+    public void testLinearForm() {
+	LinearForm lf;
+	// make sure that we can't make negative sized linear forms
+	try {lf = new LinearForm(-1); fail("no error!");} catch(RuntimeException e){}
+	try {lf = new LinearForm(0); fail("no error!");} catch(RuntimeException e){}
 
-    
+	// now, make sure that if we make a linear form with an integer offset,
+	// isIntegerOffset works correctly
+	lf = new LinearForm(1);
+	lf.setOffset(10);
+	assertTrue("int is actually an int", lf.isIntegerOffset());
+
+	// make sure that the actual offset is the same
+	assertTrue("Offset matches", lf.getIntegerOffset() == 10);
+
+	// make sure that we can't get an integer offset of a offset
+	// that is non integer
+	lf.setOffset(3.2);
+	try{lf.getIntegerOffset(); fail("should die");} catch(RuntimeException e) {}
+
+	// now, make sure that the isOnlyOffset method works
+	lf = new LinearForm(5);
+	assertTrue("is only offset", lf.isOnlyOffset());
+	// set the offset, and try again
+	lf.setOffset(1);
+	assertTrue("is only offset", lf.isOnlyOffset());
+	lf.setOffset(-83.42);
+	assertTrue("is only offset", lf.isOnlyOffset());
+	
+	
+	// set an element and make sure that it is false
+	lf.setWeight(3, new ComplexNumber(4,-1.4322));
+	assertTrue("is not only offset", !(lf.isOnlyOffset()));	
+    }
+
+    /** test the additon and negation of linear forms **/
+    public void testLinearFormAddition() {
+	LinearForm lf1;
+	LinearForm lf2;
+
+	int MAX=10;
+	
+	lf1 = new LinearForm(MAX);
+	lf2 = new LinearForm(MAX);
+	// manually construct lf1 and lf2 to be its negative
+	for (int i=0; i<MAX; i++) {
+	    lf1.setWeight(i, new ComplexNumber(i,i));
+	    lf2.setWeight(i, new ComplexNumber(-i,-i));
+	}
+	lf1.setOffset(new ComplexNumber(-32.423,0));
+	lf2.setOffset(new ComplexNumber(32.423,0));
+
+	LinearForm negativelf1 = lf1.negate();
+
+	// check offsets
+	assertTrue("Same offsets", negativelf1.getOffset().equals(lf2.getOffset()));
+	assertTrue("Same offsets", lf2.getOffset().equals(negativelf1.getOffset()));
+	
+	// make sure that the element of negating lf1 are the same as lf2
+	for (int i=0; i<MAX; i++) {
+	    assertTrue("element " + i,
+		       negativelf1.getWeight(i).equals(lf2.getWeight(i)));
+	}
+
+	// check to see if they sum to zero
+	LinearForm lfzero = lf1.plus(lf2);
+	assertTrue("Zero Offset", lfzero.getOffset().equals(ComplexNumber.ZERO));
+	for (int i=0; i<MAX; i++) {
+	    assertTrue("element " + i + " is zero",
+		       lfzero.getWeight(i).equals(ComplexNumber.ZERO));
+	}
+	lfzero = lf2.plus(lf1);
+	assertTrue("Zero Offset", lfzero.getOffset().equals(ComplexNumber.ZERO));
+	for (int i=0; i<MAX; i++) {
+	    assertTrue("element " + i + " is zero",
+		       lfzero.getWeight(i).equals(ComplexNumber.ZERO));
+	}
+	
+	// make sure that we can't add linear forms of different sizes
+	lf1 = new LinearForm(10);
+	lf2 = new LinearForm(11);
+	try{lf1.plus(lf2); fail("diff linear form size");} catch (RuntimeException e) {}
+
+    }
 }

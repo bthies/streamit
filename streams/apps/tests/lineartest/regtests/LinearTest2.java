9753b077ed26af7693daab0bff9973f19a4dc7ff
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-08-30 20:10:46 +0000

    continued testing for Linear analysis

--- streams/apps/tests/lineartest/regtests/LinearTest2.java
+++ streams/apps/tests/lineartest/regtests/LinearTest2.java
@@ -14,6 +14,9 @@ public class LinearTest2 extends StreamIt {
 	this.add(new Source());
 	this.add(new FloatIdentity());
 	this.add(new FloatFilter());
+	this.add(new FloatFilter2());
+	this.add(new FloatFilter3());
+	this.add(new FloatFilter4());
 	this.add(new Sink());
     }
 }
@@ -75,11 +78,11 @@ class FloatFilter extends Filter {
 			 input.peekFloat(1) +
 			 input.peekFloat(2));
 
-	// should result in 2nd col: [3,2,10] + 6
+	// should result in 2nd col: [3,2,10] + 7
 	output.pushFloat(1 +
 			 2*input.peekFloat(1) +
 			 input.peekFloat(2)*3 +
-			 5 +
+			 2*3 +
 			 2*input.peekFloat(0)*5);
 
 	// should result in 1st col: [0,0,0] + -11	
@@ -91,3 +94,85 @@ class FloatFilter extends Filter {
 	input.popFloat();
     }
 }
+
+/**
+ * Use this class to make sure that peeks work correctly
+ * when interspersed with pops.
+ **/
+class FloatFilter2 extends Filter {
+    public void init() {
+	input = new Channel(Float.TYPE, 4);
+	output = new Channel(Float.TYPE, 3);
+    }
+    public void work() {
+	// ok, first make a nice, normal column with peek expressions
+	// result in col 3 being: [8, 5, 6, 12] + 3
+	output.pushFloat(input.peekFloat(0) * 2 * 1 * 3 * 1 * 2 +
+			 3 * 1 * 1 * 2 * input.peekFloat(1) +
+			 5 * input.peekFloat(2) +
+			 input.peekFloat(3) * 8 +
+			 2+4+2+1-3-3);
+
+	// pop off a result
+	input.popFloat();
+
+	// now, push using updated peek values and ensure that they are correct
+	// result in col 2 being [4 3 2 0] + 7
+	output.pushFloat(input.peekFloat(0) * 2 +
+			 input.peekFloat(1) * 3 +
+			 input.peekFloat(2) * 4 +
+			 8-1);
+
+	// pop again and figure out what is going on
+	input.popFloat();
+
+	// col 1 = [1 1 0 0] + 2
+	output.pushFloat(input.peekFloat(0) +
+			 input.peekFloat(1) +
+			 2);
+
+	// pop off the last 2 items
+	input.popFloat();
+	input.popFloat();
+    }
+}
+
+/**
+ * Use this class to test division
+ **/
+class FloatFilter3 extends Filter {
+    public void init() {
+	input = new Channel(Float.TYPE, 4);
+	output = new Channel(Float.TYPE, 1);
+    }
+    public void work() {
+	// col 1 = [1/5, 2/3, 11, 3/4] + 21/32
+	output.pushFloat(input.peekFloat(3) / 5 +
+			 (2*input.peekFloat(2)) / 3 +
+			 11 * input.peekFloat(1) +
+			 (3 * input.peekFloat(0)) / (2 * 2 * 2 / 2) +
+			 ((float)21)/((float)32));
+	input.popFloat();
+	input.popFloat();
+	input.popFloat();
+	input.popFloat();
+    }
+}
+
+
+/**
+ * Use this one to make sure that we don't screw and do bad things with division
+ **/
+class FloatFilter4 extends Filter {
+    public void init() {
+	input = new Channel(Float.TYPE, 1);
+	output = new Channel(Float.TYPE, 1);
+    }
+    public void work() {
+	// non linear
+	output.pushFloat(1/input.peekFloat(0));
+	input.popFloat();
+    }
+}
+
+

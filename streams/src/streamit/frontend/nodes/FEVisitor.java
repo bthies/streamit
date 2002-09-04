66ef67b00ecb65078721060a537dda4472fb47b5
David Maze <dmaze@cag.csail.mit.edu>
2002-09-04 19:21:16 +0000

    Backed out splitting visitors into "expressions" and "everything else". This sucks in the particular case of trying to accept generic visitors by nodes known statically to be Expressions; in this case, the Java compiler refuses to decide if it's a generic or expression visitor, even though the same code path is followed by both.

--- streams/src/streamit/frontend/nodes/FEVisitor.java
+++ streams/src/streamit/frontend/nodes/FEVisitor.java
@@ -1,24 +1,35 @@
 /*
  * FEVisitor.java: visit a tree of front-end nodes
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: FEVisitor.java,v 1.4 2002-09-04 18:42:18 dmaze Exp $
+ * $Id: FEVisitor.java,v 1.5 2002-09-04 19:21:16 dmaze Exp $
  */
 
 package streamit.frontend.nodes;
 
 /**
  * A FEVisitor implements part of the "visitor" design pattern for
- * all StreamIt front-end nodes.  The pattern basically exchanges type
+ * StreamIt front-end nodes.  The pattern basically exchanges type
  * structures for function calls, so a different function in the visitor
  * is called depending on the run-time type of the object being visited.
  * Calling visitor methods returns some value, the type of which
  * depends on the semantics of the visitor in question.  In general,
  * you will create a visitor object, and then pass it to the accept()
  * method of the object in question.
- *
- * This visitor visits all nodes created in the front-end.  It is built
- * on top of FEExprVisitor, which only visits nodes derived from Expression.
  */
-public interface FEVisitor extends FEExprVisitor
+public interface FEVisitor
 {
+    public Object visitExprArray(ExprArray exp);
+    public Object visitExprBinary(ExprBinary exp);
+    public Object visitExprComplex(ExprComplex exp);
+    public Object visitExprConstChar(ExprConstChar exp);
+    public Object visitExprConstFloat(ExprConstFloat exp);
+    public Object visitExprConstInt(ExprConstInt exp);
+    public Object visitExprConstStr(ExprConstStr exp);
+    public Object visitExprField(ExprField exp);
+    public Object visitExprFunCall(ExprFunCall exp);
+    public Object visitExprPeek(ExprPeek exp);
+    public Object visitExprPop(ExprPop exp);
+    public Object visitExprTernary(ExprTernary exp);
+    public Object visitExprUnary(ExprUnary exp);
+    public Object visitExprVar(ExprVar exp);
 }

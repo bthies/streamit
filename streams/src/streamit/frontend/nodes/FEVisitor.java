5e7595c1ad432a71db8b1e93469ad69e7c8fffcf
David Maze <dmaze@cag.csail.mit.edu>
2002-09-06 16:06:51 +0000

    Add all of the new front-end nodes to the visitor interface.

--- streams/src/streamit/frontend/nodes/FEVisitor.java
+++ streams/src/streamit/frontend/nodes/FEVisitor.java
@@ -1,7 +1,7 @@
 /*
  * FEVisitor.java: visit a tree of front-end nodes
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: FEVisitor.java,v 1.5 2002-09-04 19:21:16 dmaze Exp $
+ * $Id: FEVisitor.java,v 1.6 2002-09-06 16:06:51 dmaze Exp $
  */
 
 package streamit.frontend.nodes;
@@ -32,4 +32,32 @@ public interface FEVisitor
     public Object visitExprTernary(ExprTernary exp);
     public Object visitExprUnary(ExprUnary exp);
     public Object visitExprVar(ExprVar exp);
+    public Object visitFunction(Function func);
+    public Object visitFuncWork(FuncWork func);
+    public Object visitProgram(Program prog);
+    public Object visitSCAnon(SCAnon creator);
+    public Object visitSCSimple(SCSimple creator);
+    public Object visitSJDuplicate(SJDuplicate sj);
+    public Object visitSJRoundRobin(SJRoundRobin sj);
+    public Object visitSJWeightedRR(SJWeightedRR sj);
+    public Object visitStmtAdd(StmtAdd stmt);
+    public Object visitStmtAssign(StmtAssign stmt);
+    public Object visitStmtBlock(StmtBlock stmt);
+    public Object visitStmtBody(StmtBody stmt);
+    public Object visitStmtBreak(StmtBreak stmt);
+    public Object visitStmtContinue(StmtContinue stmt);
+    public Object visitStmtDoWhile(StmtDoWhile stmt);
+    public Object visitStmtEnqueue(StmtEnqueue stmt);
+    public Object visitStmtExpr(StmtExpr stmt);
+    public Object visitStmtFor(StmtFor stmt);
+    public Object visitStmtIfThen(StmtIfThen stmt);
+    public Object visitStmtJoin(StmtJoin stmt);
+    public Object visitStmtLoop(StmtLoop stmt);
+    public Object visitStmtPush(StmtPush stmt);
+    public Object visitStmtReturn(StmtReturn stmt);
+    public Object visitStmtSplit(StmtSplit stmt);
+    public Object visitStmtVarDecl(StmtVarDecl stmt);
+    public Object visitStmtWhile(StmtWhile stmt);
+    public Object visitStreamSpec(StreamSpec spec);
+    public Object visitStreamType(StreamType type);
 }

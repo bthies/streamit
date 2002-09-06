687ee0c8240ff98e72b9373c9783767e6ef91837
David Maze <dmaze@cag.csail.mit.edu>
2002-09-06 16:00:31 +0000

    Add the new front-end nodes.

--- streams/src/streamit/frontend/nodes/FEReplacer.java
+++ streams/src/streamit/frontend/nodes/FEReplacer.java
@@ -1,7 +1,7 @@
 /*
  * FEReplacer.java: run through a front-end tree and replace nodes
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: FEReplacer.java,v 1.4 2002-08-20 20:04:28 dmaze Exp $
+ * $Id: FEReplacer.java,v 1.5 2002-09-06 16:00:31 dmaze Exp $
  */
 
 package streamit.frontend.nodes;
@@ -114,4 +114,251 @@ public class FEReplacer implements FEVisitor
     }
     
     public Object visitExprVar(ExprVar exp) { return exp; }
+
+    public Object visitFunction(Function func)
+    {
+        Statement newBody = (Statement)func.getBody().accept(this);
+        if (newBody == func.getBody()) return func;
+        return new Function(func.getContext(), func.getCls(),
+                            func.getName(), func.getReturnType(),
+                            func.getParams(), newBody);
+    }
+    
+    public Object visitFuncWork(FuncWork func)
+    {
+        Statement newBody = (Statement)func.getBody().accept(this);
+        Expression newPeek = (Expression)func.getPeekRate().accept(this);
+        Expression newPop = (Expression)func.getPopRate().accept(this);
+        Expression newPush = (Expression)func.getPushRate().accept(this);
+        if (newBody == func.getBody() && newPeek == func.getPeekRate() &&
+            newPop == func.getPopRate() && newPush == func.getPushRate())
+            return func;
+        return new FuncWork(func.getContext(), func.getName(),
+                            newBody, newPeek, newPop, newPush);
+    }
+    
+    public Object visitProgram(Program prog)
+    {
+        // Don't need to visit types, only streams.  Assume *something*
+        // will change.
+        List newStreams = new ArrayList();
+        for (Iterator iter = prog.getStreams().iterator(); iter.hasNext(); )
+            newStreams.add(((FENode)(iter.next())).accept(this));
+        return new Program(prog.getContext(), newStreams, prog.getStructs());
+    }
+    
+    public Object visitSCAnon(SCAnon creator)
+    {
+        StreamSpec newSpec = (StreamSpec)creator.getSpec().accept(this);
+        if (newSpec == creator.getSpec()) return creator;
+        return new SCAnon(creator.getContext(), newSpec);
+    }
+    
+    public Object visitSCSimple(SCSimple creator) { return creator; }
+    public Object visitSJDuplicate(SJDuplicate sj) { return sj; }
+
+    public Object visitSJRoundRobin(SJRoundRobin sj)
+    {
+        Expression newWeight = (Expression)sj.getWeight().accept(this);
+        if (newWeight == sj.getWeight()) return sj;
+        return new SJRoundRobin(sj.getContext(), newWeight);
+    }
+    
+    public Object visitSJWeightedRR(SJWeightedRR sj)
+    {
+        boolean changed = false;
+        List newWeights = new ArrayList();
+        for (Iterator iter = sj.getWeights().iterator(); iter.hasNext(); )
+        {
+            Expression oldWeight = (Expression)iter.next();
+            Expression newWeight = (Expression)oldWeight.accept(this);
+            if (newWeight != oldWeight) changed = true;
+            newWeights.add(newWeight);
+        }
+        if (!changed) return sj;
+        return new SJWeightedRR(sj.getContext(), newWeights);
+    }
+        
+    public Object visitStmtAdd(StmtAdd stmt)
+    {
+        StreamCreator newCreator =
+            (StreamCreator)stmt.getCreator().accept(this);
+        if (newCreator == stmt.getCreator()) return stmt;
+        return new StmtAdd(stmt.getContext(), newCreator);
+    }
+    
+    public Object visitStmtAssign(StmtAssign stmt)
+    {
+        Expression newLHS = (Expression)stmt.getLHS().accept(this);
+        Expression newRHS = (Expression)stmt.getRHS().accept(this);
+        if (newLHS == stmt.getLHS() && newRHS == stmt.getRHS())
+            return stmt;
+        return new StmtAssign(stmt.getContext(), newLHS, newRHS,
+                              stmt.getOp());
+    }
+    
+    public Object visitStmtBlock(StmtBlock stmt)
+    {
+        boolean changed = false;
+        List newStmts = new ArrayList();
+        for (Iterator iter = stmt.getStmts().iterator(); iter.hasNext(); )
+        {
+            Statement oldStmt = (Statement)iter.next();
+            Statement newStmt = (Statement)oldStmt.accept(this);
+            if (newStmt != oldStmt) changed = true;
+            newStmts.add(newStmt);
+        }
+        if (!changed) return stmt;
+        return new StmtBlock(stmt.getContext(), newStmts);
+    }
+    
+    public Object visitStmtBody(StmtBody stmt)
+    {
+        StreamCreator newCreator =
+            (StreamCreator)stmt.getCreator().accept(this);
+        if (newCreator == stmt.getCreator()) return stmt;
+        return new StmtBody(stmt.getContext(), newCreator);
+    }
+
+    public Object visitStmtBreak(StmtBreak stmt) { return stmt; }
+    public Object visitStmtContinue(StmtContinue stmt) { return stmt; }
+
+    public Object visitStmtDoWhile(StmtDoWhile stmt)
+    {
+        Statement newBody = (Statement)stmt.getBody().accept(this);
+        Expression newCond = (Expression)stmt.getCond().accept(this);
+        if (newBody == stmt.getBody() && newCond == stmt.getCond())
+            return stmt;
+        return new StmtDoWhile(stmt.getContext(), newBody, newCond);
+    }
+    
+    public Object visitStmtEnqueue(StmtEnqueue stmt)
+    {
+        Expression newValue = (Expression)stmt.getValue().accept(this);
+        if (newValue == stmt.getValue()) return stmt;
+        return new StmtEnqueue(stmt.getContext(), newValue);
+    }
+    
+    public Object visitStmtExpr(StmtExpr stmt)
+    {
+        Expression newExpr = (Expression)stmt.getExpression().accept(this);
+        if (newExpr == stmt.getExpression()) return stmt;
+        return new StmtEnqueue(stmt.getContext(), newExpr);
+    }
+
+    public Object visitStmtFor(StmtFor stmt)
+    {
+        Statement newInit = (Statement)stmt.getInit().accept(this);
+        Expression newCond = (Expression)stmt.getCond().accept(this);
+        Statement newIncr = (Statement)stmt.getIncr().accept(this);
+        Statement newBody = (Statement)stmt.getBody().accept(this);
+        if (newInit == stmt.getInit() && newCond == stmt.getCond() &&
+            newIncr == stmt.getIncr() && newBody == stmt.getBody())
+            return stmt;
+        return new StmtFor(stmt.getContext(), newInit, newCond, newIncr,
+                           newBody);
+    }
+    
+    public Object visitStmtIfThen(StmtIfThen stmt)
+    {
+        Expression newCond = (Expression)stmt.getCond().accept(this);
+        Statement newCons = stmt.getCons() == null ? null :
+            (Statement)stmt.getCons().accept(this);
+        Statement newAlt = stmt.getAlt() == null ? null :
+            (Statement)stmt.getAlt().accept(this);
+        if (newCond == stmt.getCond() && newCons == stmt.getCons() &&
+            newAlt == stmt.getAlt())
+            return stmt;
+        return new StmtIfThen(stmt.getContext(), newCond, newCons, newAlt);
+    }
+    
+    public Object visitStmtJoin(StmtJoin stmt)
+    {
+        SplitterJoiner newJoiner =
+            (SplitterJoiner)stmt.getJoiner().accept(this);
+        if (newJoiner == stmt.getJoiner()) return stmt;
+        return new StmtJoin(stmt.getContext(), newJoiner);
+    }
+        
+    public Object visitStmtLoop(StmtLoop stmt)
+    {
+        StreamCreator newCreator =
+            (StreamCreator)stmt.getCreator().accept(this);
+        if (newCreator == stmt.getCreator()) return stmt;
+        return new StmtLoop(stmt.getContext(), newCreator);
+    }
+
+    public Object visitStmtPush(StmtPush stmt)
+    {
+        Expression newValue = (Expression)stmt.getValue().accept(this);
+        if (newValue == stmt.getValue()) return stmt;
+        return new StmtPush(stmt.getContext(), newValue);
+    }
+    
+    public Object visitStmtReturn(StmtReturn stmt)
+    {
+        Expression newValue = stmt.getValue() == null ? null :
+            (Expression)stmt.getValue().accept(this);
+        if (newValue == stmt.getValue()) return stmt;
+        return new StmtReturn(stmt.getContext(), newValue);
+    }
+    
+    public Object visitStmtSplit(StmtSplit stmt)
+    {
+        SplitterJoiner newSplitter =
+            (SplitterJoiner)stmt.getSplitter().accept(this);
+        if (newSplitter == stmt.getSplitter()) return stmt;
+        return new StmtSplit(stmt.getContext(), newSplitter);
+    }
+
+    public Object visitStmtVarDecl(StmtVarDecl stmt)
+    {
+        Expression newInit = stmt.getInit() == null ? null :
+            (Expression)stmt.getInit().accept(this);
+        if (newInit == stmt.getInit()) return stmt;
+        return new StmtVarDecl(stmt.getContext(), stmt.getType(),
+                               stmt.getName(), newInit);
+    }
+    
+    public Object visitStmtWhile(StmtWhile stmt)
+    {
+        Expression newCond = (Expression)stmt.getCond().accept(this);
+        Statement newBody = (Statement)stmt.getBody().accept(this);
+        if (newCond == stmt.getCond() && newBody == stmt.getBody())
+            return stmt;
+        return new StmtWhile(stmt.getContext(), newCond, newBody);
+    }
+
+    public Object visitStreamSpec(StreamSpec spec)
+    {
+        // Oof, there's a lot here.  At least half of it doesn't get
+        // visited...
+        StreamType newST = (StreamType)spec.getStreamType().accept(this);
+        List newVars = new ArrayList();
+        List newFuncs = new ArrayList();
+        boolean changed = false;
+        
+        for (Iterator iter = spec.getVars().iterator(); iter.hasNext(); )
+        {
+            Statement oldVar = (Statement)iter.next();
+            Statement newVar = (Statement)oldVar.accept(this);
+            if (oldVar != newVar) changed = true;
+            newVars.add(newVar);
+        }
+        for (Iterator iter = spec.getFuncs().iterator(); iter.hasNext(); )
+        {
+            Function oldFunc = (Function)iter.next();
+            Function newFunc = (Function)oldFunc.accept(this);
+            if (oldFunc != newFunc) changed = true;
+            newFuncs.add(newFunc);
+        }
+
+        if (!changed && newST == spec.getStreamType()) return spec;
+        return new StreamSpec(spec.getContext(), spec.getType(),
+                              newST, spec.getName(), spec.getParams(),
+                              newVars, newFuncs);
+        
+    }
+    
+    public Object visitStreamType(StreamType type) { return type; }
 }

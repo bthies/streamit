4f3ecc663e0861aa146baf8ebdb410a2e0a855d4
David Maze <dmaze@cag.csail.mit.edu>
2002-09-23 21:18:48 +0000

    Add a list of types parameters.

--- streams/src/streamit/frontend/nodes/SCSimple.java
+++ streams/src/streamit/frontend/nodes/SCSimple.java
@@ -1,7 +1,7 @@
 /*
  * SCSimple.java: simple stream creator
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: SCSimple.java,v 1.2 2002-09-06 16:28:43 dmaze Exp $
+ * $Id: SCSimple.java,v 1.3 2002-09-23 21:18:48 dmaze Exp $
  */
 
 package streamit.frontend.nodes;
@@ -9,19 +9,21 @@ package streamit.frontend.nodes;
 import java.util.List;
 
 /**
- * SCSimple is a stream creator that creates a non-parameterized stream
- * given its name and a parameter list.
+ * SCSimple is a stream creator that creates a stream given its name,
+ * a possibly empty list of type parameters, and a parameter list.
  */
 public class SCSimple extends StreamCreator
 {
     private String name;
+    private List types;
     private List params;
     
     /** Create a stream object given its name and a parameter list. */
-    public SCSimple(FEContext context, String name, List params)
+    public SCSimple(FEContext context, String name, List types, List params)
     {
         super(context);
         this.name = name;
+        this.types = types;
         this.params = params;
     }
     
@@ -30,6 +32,12 @@ public class SCSimple extends StreamCreator
     public String getName()
     {
         return name;
+    }
+
+    /** Return the type parameter list of the stream. */
+    public List getTypes()
+    {
+        return types;
     }
     
     /** Return the parameter list of the stream. */

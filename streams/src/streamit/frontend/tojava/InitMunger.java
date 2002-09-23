2622125b76394574ab70af27145639223f6107a4
David Maze <dmaze@cag.csail.mit.edu>
2002-09-23 16:05:11 +0000

    Added replaceInitWithPrepended(), a helper function that takes a list of functions and some statements, digs out an init function, and replaces it with the same function with those statements prepended.

--- streams/src/streamit/frontend/tojava/InitMunger.java
+++ streams/src/streamit/frontend/tojava/InitMunger.java
@@ -1,7 +1,7 @@
 /*
  * InitMunger.java: base class for visitors that add init statements
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: InitMunger.java,v 1.1 2002-09-23 14:52:22 dmaze Exp $
+ * $Id: InitMunger.java,v 1.2 2002-09-23 16:05:11 dmaze Exp $
  */
 
 package streamit.frontend.tojava;
@@ -32,5 +32,24 @@ abstract public class InitMunger extends FEReplacer
         return Function.newInit(context,
                                 new StmtBlock(context,
                                               Collections.EMPTY_LIST));
-    }    
+    }
+
+    // Finds an init function in fns, or creates one using context.
+    // Removes it from fns, and replaces it with an equivalent function
+    // with stmts at the start of its body.  Returns fns.
+    public static List replaceInitWithPrepended(FEContext context,
+                                                List fns, List stmts)
+    {
+        Function init = findInit(context, fns);
+        fns.remove(init);
+        StmtBlock oldBody = (StmtBlock)init.getBody();
+        List newStmts = new ArrayList(stmts);
+        newStmts.addAll(oldBody.getStmts());
+        Statement newBody = new StmtBlock(oldBody.getContext(), newStmts);
+        init = new Function(init.getContext(), init.getCls(),
+                            init.getName(), init.getReturnType(),
+                            init.getParams(), newBody);
+        fns.add(init);
+        return fns;
+    }
 }

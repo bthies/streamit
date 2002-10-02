a769eb2725c7b23889ded4360d214adff10fe7f8
David Maze <dmaze@cag.csail.mit.edu>
2002-10-02 14:08:11 +0000

    Pipe all those echos to /dev/null so whoever runs this out of cron doesn't get mail with lists of environment variables.

--- streams/regtest/tools/run_results.sh
+++ streams/regtest/tools/run_results.sh
@@ -2,16 +2,16 @@
 # AAL 6/25/2002 Script that runs results gatherer every evening
 # (gets called from cron job on cagfram-46.lcs.mit.edu user
 # aalamb).
-# $Id: run_results.sh,v 1.7 2002-10-02 14:05:23 dmaze Exp $
+# $Id: run_results.sh,v 1.8 2002-10-02 14:08:11 dmaze Exp $
 
-echo CVSROOT: ${CVSROOT:=/projects/raw/cvsroot}
-echo STREAMIT_HOME: ${STREAMIT_HOME:=$HOME/streams}
-echo TOPDIR: ${TOPDIR:=/home/bits6/$USER/starsearch}
+echo CVSROOT: ${CVSROOT:=/projects/raw/cvsroot} >/dev/null
+echo STREAMIT_HOME: ${STREAMIT_HOME:=$HOME/streams} >/dev/null
+echo TOPDIR: ${TOPDIR:=/home/bits6/$USER/starsearch} >/dev/null
 CLASSPATH=.:/usr/local/jdk1.3/jre/lib/rt.jar:$STREAMIT_HOME/compiler/kopi/3rdparty/JFlex/lib:$STREAMIT_HOME/compiler/kopi/3rdparty/getopt:$STREAMIT_HOME/compiler/kopi/classes:$STREAMIT_HOME/apps/libraries:$STREAMIT_HOME/misc/java:$STREAMIT_HOME/scheduler/v1/java:/usr/uns/java/antlr-2.7.1:$STREAMIT_HOME/compiler/frontend:$STREAMIT_HOME/scheduler/v2/java
 
-echo LOGFILE: ${LOGFILE:=$STREAMIT_HOME/regtest/tools/results_log.txt}
+echo LOGFILE: ${LOGFILE:=$STREAMIT_HOME/regtest/tools/results_log.txt} >/dev/null
 # file that gets generated automatically by the regression test framework
-echo RESULT_SCRIPT: ${RESULT_SCRIPT:=$STREAMIT_HOME/regtest/regtest_perf_script.txt}
+echo RESULT_SCRIPT: ${RESULT_SCRIPT:=$STREAMIT_HOME/regtest/regtest_perf_script.txt} >/dev/null
 
 export CVSROOT STREAMIT_HOME TOPDIR LOGFILE RESULT_SCRIPT
 

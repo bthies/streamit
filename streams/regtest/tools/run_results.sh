c1c8356eefd0a12f6f87488ca12e224943afcf10
David Maze <dmaze@cag.csail.mit.edu>
2002-10-02 14:05:23 +0000

    Rewrite in Bourne shell syntax.  Make extensive use of ${FOO:=bar}, which sets FOO to bar but only if $FOO is unset or null.  Also remove all explicit references to Andrew's home directory.

--- streams/regtest/tools/run_results.sh
+++ streams/regtest/tools/run_results.sh
@@ -1,18 +1,19 @@
-#!/bin/csh
+#!/bin/sh
 # AAL 6/25/2002 Script that runs results gatherer every evening
 # (gets called from cron job on cagfram-46.lcs.mit.edu user
 # aalamb).
-# $Id: run_results.sh,v 1.6 2002-07-25 14:36:03 aalamb Exp $
+# $Id: run_results.sh,v 1.7 2002-10-02 14:05:23 dmaze Exp $
 
-setenv LOGFILE /u/aalamb/streams/regtest/tools/results_log.txt
+echo CVSROOT: ${CVSROOT:=/projects/raw/cvsroot}
+echo STREAMIT_HOME: ${STREAMIT_HOME:=$HOME/streams}
+echo TOPDIR: ${TOPDIR:=/home/bits6/$USER/starsearch}
+CLASSPATH=.:/usr/local/jdk1.3/jre/lib/rt.jar:$STREAMIT_HOME/compiler/kopi/3rdparty/JFlex/lib:$STREAMIT_HOME/compiler/kopi/3rdparty/getopt:$STREAMIT_HOME/compiler/kopi/classes:$STREAMIT_HOME/apps/libraries:$STREAMIT_HOME/misc/java:$STREAMIT_HOME/scheduler/v1/java:/usr/uns/java/antlr-2.7.1:$STREAMIT_HOME/compiler/frontend:$STREAMIT_HOME/scheduler/v2/java
+
+echo LOGFILE: ${LOGFILE:=$STREAMIT_HOME/regtest/tools/results_log.txt}
 # file that gets generated automatically by the regression test framework
-setenv RESULT_SCRIPT /u/aalamb/streams/regtest/regtest_perf_script.txt
+echo RESULT_SCRIPT: ${RESULT_SCRIPT:=$STREAMIT_HOME/regtest/regtest_perf_script.txt}
 
-
-setenv CVSROOT /projects/raw/cvsroot
-setenv STREAMIT_HOME /u/aalamb/streams/
-setenv TOPDIR /home/bits6/aalamb/starsearch
-setenv CLASSPATH .:/usr/local/jdk1.3/jre/lib/rt.jar:$STREAMIT_HOME/compiler/kopi/3rdparty/JFlex/lib:$STREAMIT_HOME/compiler/kopi/3rdparty/getopt:$STREAMIT_HOME/compiler/kopi/classes:$STREAMIT_HOME/apps/libraries:$STREAMIT_HOME/misc/java:$STREAMIT_HOME/scheduler/v1/java:/usr/uns/java/antlr-2.7.1:$STREAMIT_HOME/compiler/frontend:$STREAMIT_HOME/scheduler/v2/java
+export CVSROOT STREAMIT_HOME TOPDIR LOGFILE RESULT_SCRIPT
 
 echo "-------------" > $LOGFILE
 echo "Input file:"   >> $LOGFILE
@@ -25,8 +26,7 @@ date                 >> $LOGFILE
 echo "-------------" >> $LOGFILE
 
 # simply run the results script (with the results we want for asplos)
-cd /u/aalamb/streams/regtest/tools/
-reap_results.pl $RESULT_SCRIPT >>& $LOGFILE
+$STREAMIT_HOME/regtest/tools/reap_results.pl $RESULT_SCRIPT >> $LOGFILE 2>&1
 
 echo "-------------" >> $LOGFILE
 echo "Done..."       >> $LOGFILE
@@ -35,7 +35,7 @@ echo "-------------" >> $LOGFILE
 
 
 # mail results to andrew
-cat $LOGFILE | mail -s "Numbers generated" aalamb@mit.edu
+cat $LOGFILE | mail -s "Numbers generated" $USER@cag.lcs.mit.edu
 
 # remove the log file
 rm -rf $LOGFILE

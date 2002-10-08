ae7c0cb632d19ab7b92ea6035d360ddbd33bc9dd
Michael Gordon <michael.ian.gordon@gmail.com>
2002-10-08 06:21:59 +0000

    Added a check to make sure we are not constructing a local var expression with a null local variable reference.

--- streams/src/at/dms/kjc/JLocalVariableExpression.java
+++ streams/src/at/dms/kjc/JLocalVariableExpression.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: JLocalVariableExpression.java,v 1.3 2002-06-22 09:52:14 jasperln Exp $
+ * $Id: JLocalVariableExpression.java,v 1.4 2002-10-08 06:21:59 mgordon Exp $
  */
 
 package at.dms.kjc;
@@ -38,6 +38,8 @@ public class JLocalVariableExpression extends JExpression {
    */
   public JLocalVariableExpression(TokenReference where, JLocalVariable variable) {
     super(where);
+    if (variable == null)
+	throw new RuntimeException();
     this.variable = variable;
   }
 

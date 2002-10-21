6aa134cf0041694b8bb291667fb89c9f63a9424d
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-10-21 20:41:06 +0000

    The perl script wasn't executing properly because it couldn't find the reaplib.pl file that has its library functions. I added an explicit include path command to the perl interpreter, and I think that we should see some results tomorrow morning. Yay!

--- streams/regtest/tools/run_results.sh
+++ streams/regtest/tools/run_results.sh
@@ -2,7 +2,7 @@
 # AAL 6/25/2002 Script that runs results gatherer every evening
 # (gets called from cron job on cagfram-46.lcs.mit.edu user
 # aalamb).
-# $Id: run_results.sh,v 1.9 2002-10-03 14:13:51 dmaze Exp $
+# $Id: run_results.sh,v 1.10 2002-10-21 20:41:06 aalamb Exp $
 
 echo CVSROOT: ${CVSROOT:=/projects/raw/cvsroot} >/dev/null
 echo STREAMIT_HOME: ${STREAMIT_HOME:=$HOME/streams} >/dev/null
@@ -26,7 +26,7 @@ date                 >> $LOGFILE
 echo "-------------" >> $LOGFILE
 
 # simply run the results script (with the results we want for asplos)
-$STREAMIT_HOME/regtest/tools/reap_results.pl $RESULT_SCRIPT >> $LOGFILE 2>&1
+/usr/local/bin/perl -I$STREAMIT_HOME/regtest/tools/ $STREAMIT_HOME/regtest/tools/reap_results.pl  $RESULT_SCRIPT >> $LOGFILE 2>&1
 
 echo "-------------" >> $LOGFILE
 echo "Done..."       >> $LOGFILE

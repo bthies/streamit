5d22f68e4d0fb2a6dca5533b4a74075dc159334e
Bill Thies <bthies@gmail.com>
2002-10-29 01:11:56 +0000

    Adding back --constprop --raw 4 --partition to the regtest.  (The raw tests from 1-8 before were causing problems, but they were the only ones with raw and constprop, so adding back now.)

--- streams/regtest/streamittest/TestAll.java
+++ streams/regtest/streamittest/TestAll.java
@@ -1,6 +1,6 @@
 /**
  * Class which runs all of the test suites
- * $Id: TestAll.java,v 1.13 2002-10-18 00:26:52 aalamb Exp $
+ * $Id: TestAll.java,v 1.14 2002-10-29 01:11:56 thies Exp $
  **/
 package streamittest;
 
@@ -78,13 +78,14 @@ public class TestAll extends TestCase {
 				       CompilerInterface.RAW[8]));
 
 	// try all configurations of raw with constprop and partition
-	// This was causing the regtest to go crazy, so I am removing them.
-// 	for (int i=1; i<=8; i++) {
-// 	    allTests.addTest(makeTestSuite(CompilerInterface.NONE |
-// 					   CompilerInterface.RAW[i] |
-// 					   CompilerInterface.PARTITION |
-// 					   CompilerInterface.CONSTPROP));
-// 	}
+	// This was causing the regtest to go crazy, so I am removing
+	// all but 4
+	// 	for (int i=1; i<=8; i++) {
+ 	    allTests.addTest(makeTestSuite(CompilerInterface.NONE |
+ 					   CompilerInterface.RAW[4] |
+ 					   CompilerInterface.PARTITION |
+ 					   CompilerInterface.CONSTPROP));
+	    // 	}
 
 	// try linear replacement (replace linear filters with a direct implementation).
 // 	allTests.addTest(makeTestSuite(CompilerInterface.NONE |

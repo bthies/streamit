30839b2166b70b885265589f10d8906eb32da8f8
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-10-29 20:39:37 +0000

    First shot at FrequencyReplacer that works -- or at least it generates the correct output for the first iteration of the work function. I am currently working to figure out what is wrong with the second iteration, etc.

--- streams/src/at/dms/kjc/sir/linear/frequency/FFTWFrequencyReplacer.java
+++ streams/src/at/dms/kjc/sir/linear/frequency/FFTWFrequencyReplacer.java
@@ -16,13 +16,13 @@ import at.dms.compiler.*;
  * In so doing, this also increases the peek, pop and push rates to take advantage of
  * the frequency transformation
  * 
- * $Id: FFTWFrequencyReplacer.java,v 1.2 2002-10-28 22:32:31 aalamb Exp $
+ * $Id: FFTWFrequencyReplacer.java,v 1.3 2002-10-29 20:39:37 aalamb Exp $
  **/
 public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
     /** the name of the function in the C library that does fast convolution via the frequency domain. **/
     public static final String FAST_CONV_EXTERNAL = "do_fast_convolution";
     /** the name of the buffer in which to place the input data. */
-    public static final String INPUT_BUFFER_NAME = "freqBuffer";
+    public static final String INPUT_BUFFER_NAME = "timeBuffer";
     
     /** the linear analyzier which keeps mappings from filters-->linear representations**/
     LinearAnalyzer linearityInformation;
@@ -67,8 +67,11 @@ public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
 	
 	/* now is when we get to the fun part, we have a linear representation
 	 * that computes an FIR (ef pop 1, push 1, peek N) and we want to replace it with an FFT. */
-
-	int filterSize = linearRep.getPeekCount();
+	int x = linearRep.getPeekCount();
+	int N = calculateN(10,x);
+	int filterSize = N+2*(x-1);
+	LinearPrinter.println(" creating frequency filter. (N=" + N +
+			      ",x=" + x + ",size=" + filterSize + ")");
 	
 	/* make fields to hold the real and imaginary parts of the weights. */
 	String realWeightName = "freq_weights_r";
@@ -79,15 +82,19 @@ public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
 	/* wrap them up, along with any fields that the filter already has. */
 	JFieldDeclaration[] newFields = makeFieldDeclarations(self.getFields(), realWeightField, imagWeightField);
 	
-	/* make a new work function */
-	JMethodDeclaration freqWork = makeNewWork(realWeightField, imagWeightField, filterSize);
-	
 	/* make a new init function */
 	JMethodDeclaration freqInit = makeNewInit(linearRep, realWeightField, imagWeightField, filterSize);
+
+	/* make a new work function */
+	JMethodDeclaration freqWork = makeNewWork(realWeightField, imagWeightField, filterSize, x, N);
+	
 	
 	LinearPrinter.println(" done building new IR nodes for " + self);
 	
 	/* replace all of the pieces that we just built. */
+	self.setPeek(x+N-1);
+	self.setPop(N);
+	self.setPush(N);
 	self.setWork(freqWork);
 	self.setInit(freqInit);
 	self.setFields(newFields);
@@ -147,11 +154,30 @@ public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
 	body.addStatement(new JExpressionStatement(null, realAssign, null));
 	body.addStatement(new JExpressionStatement(null, imagAssign, null)); 
 
-	/* calculate the weights of the fields based on the filter coefficients that are found in the linear representation. */
-
+	/* calculate the weights of the fields based on the filter
+	 * coefficients that are found in the linear representation. */	
+	float[] frequency_response_r = new float[filterSize];
+	float[] frequency_response_i = new float[filterSize];
+	float[] time_response_r      = getRealArray(linearRep, filterSize);
+	float[] time_response_i      = getImagArray(linearRep, filterSize);
+	LinearFFT.fft_float(filterSize, false, /* false means that we are not doing a reverse transform */
+			    time_response_r, /* real input */
+			    time_response_i, /* imag input */
+			    frequency_response_r,     /* filter's frequency response, real (output) */
+			    frequency_response_i);    /* filter's frequency response, imag (output) */
+				    
 	/* assign the weights that we just calculated (in the compiler) to the weights array in the filter. */
+	for (int i=0; i<filterSize; i++) {
+	    /* create array assignments for both the real and imaginary coefficient arrays. */
+	    body.addStatement(makeArrayAssignment(realWeightField, /* field */
+						  i,                          /* index */
+						  frequency_response_r[i]));  /* value */
+	    body.addStatement(makeArrayAssignment(imagWeightField, /* field */
+						  i,                          /* index */
+						  frequency_response_i[i]));  /* value */
+	}
 
-
+	
 	/** assemble the new pieces that we made and stick them into a new method. */
 	return new JMethodDeclaration(null,                 /* token reference */
 				      ACC_PUBLIC,           /* modifiers */
@@ -164,11 +190,34 @@ public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
 				      null);                /* java style comment */
     }
 
+    /** Creates an assignment expression of the form: this.f[index]=value; **/
+    public JStatement makeArrayAssignment(JLocalVariable field, int index, float value) {
+	/* make the field access expression (eg this.field)*/
+	JFieldAccessExpression fldAccessExpr;
+	fldAccessExpr = new JFieldAccessExpression(null, /* token reference */
+						   new JThisExpression(null), /* base */
+						   field.getIdent()); /* field name */
+	/* make the array access expression (eg this.field[index]*/
+	JArrayAccessExpression arrAccessExpr;
+	arrAccessExpr = new JArrayAccessExpression(null,                    /* token reference */
+						   fldAccessExpr,           /* prefix */
+						   new JIntLiteral(index)); /* accessor */
+	/* the literal value to assign */
+	JFloatLiteral literalValue = new JFloatLiteral(null, value);
+	/* now, make the assignment expression */
+	JAssignmentExpression assignExpr;
+	assignExpr = new JAssignmentExpression(null,          /* token reference */
+					       arrAccessExpr, /* lhs */
+					       literalValue); /* rhs */
+	/* return an expression statement */
+	return new JExpressionStatement(null, assignExpr, new JavaStyleComment[0]);
+    }
+
 
     /* make the new work function */
     public JMethodDeclaration makeNewWork(JVariableDefinition realWeightField,
 					  JVariableDefinition imagWeightField,
-					  int filterSize) {
+					  int filterSize, int x, int N) {
 	/* the body of the new work function */
 	JBlock body = new JBlock();
 
@@ -193,7 +242,7 @@ public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
 						   makeComment("allocate space for input buffer.")));
 
 	/* now, copy the data from the input tape into the buffer, element by element */
-	for (int i=0; i<filterSize; i++) {
+	for (int i=0; i<(x+N-1); i++) {
 	    JLocalVariableExpression currentBuffExpr = new JLocalVariableExpression(null, inputBuffer);
 	    JArrayAccessExpression currentAccessExpr = new JArrayAccessExpression(null, currentBuffExpr, new JIntLiteral(i));
 	    SIRPeekExpression      currentPeekExpr   = new SIRPeekExpression(new JIntLiteral(i), CStdType.Float);
@@ -218,6 +267,29 @@ public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
 						   externalCall, /* expression */
 						   comment));       /* comments */
 	
+
+	/* now, put in code that will push the appropriate values inputBuffer[x-1 to N+x-2] back on the tape */
+	for (int i=(x-1); i<(N+x-1); i++) {
+	    body.addStatement(makeArrayPushStatement(inputBuffer, i));
+	}
+
+	/* now, free the memory that we allocated to buffer (this is a wicked hack,
+	   as there is no notion of "free" in java/streamit */
+	JExpression[] freeArgs = new JExpression[1];
+	freeArgs[0] = new JLocalVariableExpression(null, inputBuffer);
+	JMethodCallExpression freeCall = new JMethodCallExpression(null,     /* token reference */
+								   null,     /* prefix */
+								   "free",   /* ident */
+								   freeArgs);/* args */
+	comment = makeComment("call to free the buffer space");
+	body.addStatement(new JExpressionStatement(null, freeCall, comment));
+	
+	/* stick in the appropriate number (x) of pop calls */
+	for (int i=0; i<x; i++) {
+	    body.addStatement(new JExpressionStatement(null, new SIRPopExpression(CStdType.Float), null));
+	}
+	
+	/* wrap up all the mess that we just made into a new JMethodDeclaration and return it to the caller */
 	return new JMethodDeclaration(null,                 /* token reference */
 				      ACC_PUBLIC,           /* modifiers */
 				      CStdType.Void,        /* return type */
@@ -230,21 +302,32 @@ public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
     }
 
 
-	/** returns the type to use for buffers -- in this case float[] */
-	public CType getArrayType() {
-	    return new CArrayType(CStdType.Float, 1);
-	}
-
-	/** make a JNewArrayStatement that allocates size elements of a float array */
-	public JNewArrayExpression getNewArrayExpression(int size) {
-	    /* make the size array. */
-	    JExpression[] arrayDims = new JExpression[1];
-	    arrayDims[0] = new JIntLiteral(size);
-	    return new JNewArrayExpression(null,           /* token reference */
-					   getArrayType(), /* type */
-					   arrayDims,      /* size */
-					   null);          /* initializer */
-	}
+    /* makes an array push statement of the following form: push(arr[index]) */
+    public JStatement makeArrayPushStatement(JLocalVariable arr, int index) {
+	/* first make the array access expression arr[index]. **/
+	JLocalVariableExpression arrExpr = new JLocalVariableExpression(null, arr);
+	JArrayAccessExpression arrAccessExpr = new JArrayAccessExpression(null, arrExpr, new JIntLiteral(index));
+	/* now make the push expression */
+	SIRPushExpression pushExpr = new SIRPushExpression(arrAccessExpr, CStdType.Float);
+	/* and return an expression statement */
+	return new JExpressionStatement(null, pushExpr, null);
+    }
+	
+    /** returns the type to use for buffers -- in this case float[] */
+    public CType getArrayType() {
+	return new CArrayType(CStdType.Float, 1);
+    }
+    
+    /** make a JNewArrayStatement that allocates size elements of a float array */
+    public JNewArrayExpression getNewArrayExpression(int size) {
+	/* make the size array. */
+	JExpression[] arrayDims = new JExpression[1];
+	arrayDims[0] = new JIntLiteral(size);
+	return new JNewArrayExpression(null,           /* token reference */
+				       getArrayType(), /* type */
+				       arrayDims,      /* size */
+				       null);          /* initializer */
+    }
 
 
     /* make an array of one java comments from a string. */
@@ -258,11 +341,54 @@ public class FrequencyReplacer extends EmptyStreamVisitor implements Constants{
     }
 
 
+    /** returns an array of floating point numbers that correspond to the real part of the FIR filter's weights  **/
+    public float[] getRealArray(LinearFilterRepresentation filterRep, int size) {return getArray(filterRep, size, 0);} 
+    /** returns an array of floating point numbers that correspond to the imaginary part of the FIR filter's weights **/
+    public float[] getImagArray(LinearFilterRepresentation filterRep, int size) {return getArray(filterRep, size, 1);}
+    /**
+     * the method that does the actual work for getRealArray and getImagArray.
+     * part == 0, means get real part, part == 1 means get imaginary part.
+     * if size is greater than the size of the filter rep, the rest of the array is left as 0
+     **/
+    public float[] getArray(LinearFilterRepresentation filterRep, int size, int part) {
+	if (!filterRep.isFIR()) {
+	    throw new RuntimeException("non fir filter passed to getArray()");
+	}
+	/* use the matrix that is hidden inside the filter representation */
+	FilterMatrix source = filterRep.getA();
+	if (source.getRows() > size) {
+	    throw new RuntimeException("freq response size is too small for this filter's response.");
+	}
+	/* allocate the return array */
+	float[] arr = new float[size];
+	/* pull out the parts of the matrix, one by one */
+	for (int i=0; i<source.getRows(); i++) {
+	    ComplexNumber currentElement = source.getElement(i,0);
+	    /* assign different piece depending on part parameter */
+	    arr[i] = (part == 0) ? (float)currentElement.getReal() : (float)currentElement.getImaginary();
+	}
+	return arr;
+    }
 
 
-
-
-
+    /**
+     * calculates the appropriate size FFT to perform. It is passed a target N, the number
+     * of outputs to produce, and x, the length
+     * of the impuse response of the filter, and it returns the actual N, the
+     * number of output points that will be produced by one execution of the
+     * filter.
+     **/
+    public int calculateN(int targetN, int x) {
+	// we know N + 2(x-1) = 2^r
+	// so we calculate r as floor(lg(N+2(x-1))) +1 where lg is log base 2
+	
+	// and then N = 2^r - 2(x-1)
+	int arg = targetN + 2*(x-1);
+	int r = (int)Math.floor(Math.log(arg)/Math.log(2)) + 1;
+	// now, calculate N
+	int N = (int)Math.pow(2,r) - 2*(x-1);
+	return N;
+    }
 
 
 

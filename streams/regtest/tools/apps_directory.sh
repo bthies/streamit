1c43d726e6fca1713fec1d0112e9d46b3c66deb6
Bill Thies <bthies@gmail.com>
2002-11-07 17:58:54 +0000

    Refining report of applications to distinguish between what's not being compiled, what's not being executed, and what's not being verified in regtest.

--- streams/regtest/tools/apps_directory.sh
+++ streams/regtest/tools/apps_directory.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo
-echo "The following directories DO NOT appear anywhere in the regtest:"
+echo "The following directories are NOT being COMPILED in the regtest:"
 for dirname in `ls ${STREAMIT_HOME}apps` 
 do
   for name in `tree -dif ${STREAMIT_HOME}apps/$dirname | grep "apps/$dirname/" | grep -v CVS | grep -v directories | grep -v VocoderTests | grep -v '/c/' | grep -v '/c$'`
     do
-    info=`grep $name $1`
+    info=`grep $name $1 | grep Compile`
     if test "$info" == ""; then
 	echo " " $name
     fi
@@ -14,14 +14,26 @@ do
 done
 
 echo
-echo "The following directories DO appear somewhere in the regtest:"
+echo "The following directories are NOT being EXECUTED in the regtest:"
 for dirname in `ls ${STREAMIT_HOME}apps` 
 do
   for name in `tree -dif ${STREAMIT_HOME}apps/$dirname | grep "apps/$dirname/" | grep -v CVS | grep -v directories | grep -v VocoderTests | grep -v '/c/' | grep -v '/c$'`
-
     do
-    info=`grep $name $1`
-    if test "$info" != ""; then
+    info=`grep $name $1 | grep Exec`
+    if test "$info" == ""; then
+	echo " " $name
+    fi
+  done
+done
+
+echo
+echo "The following directories are NOT being VERIFIED in the regtest:"
+for dirname in `ls ${STREAMIT_HOME}apps` 
+do
+  for name in `tree -dif ${STREAMIT_HOME}apps/$dirname | grep "apps/$dirname/" | grep -v CVS | grep -v directories | grep -v VocoderTests | grep -v '/c/' | grep -v '/c$'`
+    do
+    info=`grep $name $1 | grep Verify`
+    if test "$info" == ""; then
 	echo " " $name
     fi
   done

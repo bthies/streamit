1c7c4ef62f1647efc4d0fd218d2923c13bf5bf73
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-11-26 22:07:15 +0000

    All new code to implement the general case of pipeline combination as we esribed it in the paper (eg as Bill formalized it)

--- streams/src/at/dms/kjc/sir/linear/transform/LinearTransformPipeline.java
+++ streams/src/at/dms/kjc/sir/linear/transform/LinearTransformPipeline.java
@@ -1,5 +1,8 @@
 package at.dms.kjc.sir.linear.transform;
 
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
 import at.dms.kjc.sir.linear.*;
 
 /**
@@ -8,151 +11,108 @@ import at.dms.kjc.sir.linear.*;
  * the same work. This combination might require each of the individual
  * filters to be expanded by some factor, and then a matrix multiplication
  * can be performed.<p>
- * $Id: LinearTransformPipeline.java,v 1.6 2002-11-25 20:33:26 aalamb Exp $
+ * $Id: LinearTransformPipeline.java,v 1.7 2002-11-26 22:07:15 aalamb Exp $
  **/
 public class LinearTransformPipeline extends LinearTransform {
-    /** The upstream filter representation. **/
-    LinearFilterRepresentation upstreamRep;
-    /** the downstream filter representation. **/
-    LinearFilterRepresentation downstreamRep;
-    /** The number of times that we need to expand the upstream rep. **/
-    int upstreamExpandFactor;
-    /** The number of times that we need to expand the downstream rep. **/
-    int downstreamExpandFactor;
-    /** the new pop count for the combined filter after the expansion. **/
-    int newPopCount;
-
-    /**
-     * Creates a new pipeline transformation by expanding the upstream rep
-     * by a factor upFactor, expanding the downstream factor by downfactor
-     * and then combining the two using the matrix multiply (see transform below).
-     **/
-    private LinearTransformPipeline(int upFactor,
-				    LinearFilterRepresentation up,
-				    int downFactor,
-				    LinearFilterRepresentation down,
-				    int newPop) {
-	this.upstreamExpandFactor   = upFactor;
-	this.upstreamRep            = up;
-	this.downstreamExpandFactor = downFactor;
-	this.downstreamRep          = down;
-	this.newPopCount            = newPop;
+    List repList;
+    
+    private LinearTransformPipeline(List l) {
+	// assert that the list has more than one element in it.
+	if (l.size() < 2) {
+	    throw new IllegalArgumentException("Representation list has fewer than two elements: " +
+					       l.size());
+	}
+	// assert that all arguments are LinearFilterRepresentations
+	Iterator iter = l.iterator();
+	while(iter.hasNext()) {
+	    if (!(iter.next() instanceof LinearFilterRepresentation)) {
+		throw new IllegalArgumentException("non LFR in list passed to linear transform pipeline");
+	    }
+	}
+	this.repList = new LinkedList(l);
     }
 
+    public LinearFilterRepresentation transform() throws NoTransformPossibleException {
+	// we know that our rep list has at least two children in it.
+	// start running down the rep list transforming things
+	LinearFilterRepresentation rep1; // the current "upstream" filter
+	LinearFilterRepresentation rep2; // the current "downstream" filter
+
+	Iterator repIter = this.repList.iterator();
+	
+	rep1 = (LinearFilterRepresentation)repIter.next();
+	// iterate over all of the represenations
+	while(repIter.hasNext()) {
+	    rep2 = (LinearFilterRepresentation)repIter.next();
+	    // pull out peek, pop, and push rates so the following code looks as much like
+	    // the paper as possible (e=peek, o=pop, u=push)
+	    int e1 = rep1.getPeekCount();
+	    int o1 = rep1.getPopCount();
+	    int u1 = rep1.getPushCount();
+	    int e2 = rep2.getPeekCount();
+	    int o2 = rep2.getPopCount();
+	    int u2 = rep2.getPushCount();
+
+	    // calculate chanPeek and chanPop
+	    int chanPop = lcm(u1,o2);
+	    int chanPeek = chanPop + (e2-o2);
+
+	    // calculate expanded peek, pop, and push rates
+	    int ee1 = chanPeek*o1/u1 + (e1-o1);
+	    int oe1 = chanPop*o1/u1;
+	    int ue1 = chanPeek;
+	    int ee2 = chanPeek;
+	    int oe2 = chanPop;
+	    int ue2 = chanPop*u2/o2;
+
+	    // now, actually create the expanded reps.
+	    LinearFilterRepresentation rep1Expanded = rep1.expand(ee1, oe1, ue1);
+	    LinearFilterRepresentation rep2Expanded = rep2.expand(ee2, oe2, ue2);
+
+	    // figure out the matrix and vector of the combined rep
+	    // A' = Ae1*Ae2
+	    FilterMatrix Aprime = rep1Expanded.getA().times(rep2Expanded.getA());
+	    // b' = be1*Ae2 + be2
+	    FilterMatrix partialProduct = rep1Expanded.getb().times(rep2Expanded.getA());
+	    FilterVector bprime = FilterVector.toVector(partialProduct.plus(rep2Expanded.getb()));
+	    	    
+	    // now, assemble the overall linear rep.
+	    LinearFilterRepresentation combinedRep;
+	    combinedRep = new LinearFilterRepresentation(Aprime, bprime, oe1);
+
+
+	    LinearPrinter.println("Created new linear rep: \n" +
+				  " peek=" + combinedRep.getPeekCount() + "\n" +
+				  " pop=" + combinedRep.getPopCount() + "\n" +
+				  " push=" + combinedRep.getPushCount());
+				  
+
+	    // now, we set the combined rep to be rep1 and repeat
+	    rep1 = combinedRep;
+	}
+
+	// all we have to do is to return the combined rep and we are done
+	return rep1;
+    }
+
+    
 
     /**
-     * Tries to combine this LinearFilterRepresentation with other.
-     * LFRs represent the calculations that a filter performs, by combining two
-     * LFRs we hope to represent the calculations that the two filters cascaded one
-     * after another form. <p>
-     * 
-     * Combining only makes sense for two filters with the following properties:
-     * <ul>
-     * <li> The push rate of upstream one is equal to the peek rate of the downstream one
-     * </ul>
-     *
-     * It is interesting to note that the above suggests that some filters are not combinable
-     * which we think is the general case. However, we can also possibly do the
-     * equivalent of matrix unrolling on both this LFR and the other LFR to get the above
-     * condition to hold.<p>
+     * Sets up the calculation of the overall linear representation of
+     * a sequential list of linear representations. 
      *
      * If filter one computes y = xA1 + b1 and filter 2 computes y=xA2 + b2 then
      * the overall filter filter1 --> filter 2 will compute
      * y = (xA1 + b1)A2 + b2 = xA1A2 + (b1A2 + b2), which itself can be represented  
-     * with the LFR: A = A1A2 and b = (b1A2 + b2).
+     * with the LFR: A = A1A2 and b = (b1A2 + b2).<p>
      *
-     * The LFR that represents both filters cascaded is returned if we can find it, and
-     * null is returned if we can not.
+     * There are a bunch of subtlties involved with computing the overall representation
+     * due to various size restrictions (eg the sizes of the matrices have to be
+     * compatible. See the pldi-03-linear paper for the gory details.<p> 
      **/
-    public LinearFilterRepresentation transform() throws NoTransformPossibleException {
-	LinearPrinter.println("Transforming pipeline combination (upfact=" +
-			      this.upstreamExpandFactor +
-			      ", downfact=" + this.downstreamExpandFactor + ")");
-
-	// expand if need be
-	LinearFilterRepresentation upstreamExpandedRep  = this.upstreamRep.expand(this.upstreamExpandFactor);
-	LinearFilterRepresentation downstreamExpandedRep = this.downstreamRep.expand(this.downstreamExpandFactor);
-
-	//LinearPrinter.println("Combining pipeline. Upstream A = \n" + upstreamExpandedRep.getA());
-	//LinearPrinter.println("Combining pipeline. Downstream A = \n" + downstreamExpandedRep.getA());
-
-	// If the dimensions match up, then perform the actual matrix
-	// multiplication
-	if (upstreamExpandedRep.getPushCount() == downstreamExpandedRep.getPeekCount()) {
-	    FilterMatrix A1 = upstreamExpandedRep.getA();
-	    FilterVector b1 = upstreamExpandedRep.getb();
-	    FilterMatrix A2 = downstreamExpandedRep.getA();
-	    FilterVector b2 = downstreamExpandedRep.getb();
-	    
-	    // compute the the new A = A1A2
-	    FilterMatrix newA = A1.times(A2);
-	    // compute the new b = (b1A2 + b2)
-	    FilterVector newb = FilterVector.toVector((b1.times(A2)).plus(b2));
-
-	    // return a new LFR with newA and newb 
-	    return new LinearFilterRepresentation(newA, newb, newPopCount);
-	} else {
-	    // we couldn't combine the matricies, complain!
-	    throw new RuntimeException("Pipeline is impossible to expand -- should not be a LinearTransformPipeline");
-	}
+    public static LinearTransform calculate(List linearRepList) {
+	// actually, we are going to punt any actual work until the "transform"
+	// method is called.
+	return new LinearTransformPipeline(linearRepList);
     }
-
-    /**
-     * Calculates a LinearTransform defining how to transform a
-     * pipeline combination of linear reps (eg one right after the next)
-     * into a single linear rep. The filters look like
-     * upstream->downstream, as you might imagine.<p>
-     *
-     * The analysis proceeds the following steps:<p>
-     * <ul>
-     * <li> if the two filters happen to have upstream.push = downstream.peek, we are
-     *      golden, and no expansion is necessary.
-     * <li> if the downstream filter has peek=pop, then we can expand
-     *      the execution of both filters by a factor to ensure
-     *      upstream.push = downstream.peek and we don't end up with
-     *      any buffering issues.
-     * <li> it is unclear what we are going to do in the general case, as we can't just
-     *      get rid of buffering. Instead, we will probably resort to doing
-     *      redundant computations.
-     * </ul>
-     **/
-    public static LinearTransform calculate(LinearFilterRepresentation upstream,
-					    LinearFilterRepresentation downstream) {
-	// if the downstream filter has push equal to their pop, then
-	// we can combine them by expanding to make upstream.pop = downstream.peek
-	// and not trouble ourselves with any state embodied by the difference
-	// between downstream.peek and downstream.pop
-	if (downstream.getPeekCount() == downstream.getPopCount()) {
-	    int expandLcm = lcm(upstream.getPushCount(), downstream.getPopCount());
-	    // a few paranoia checks
-	    if (((expandLcm % upstream.getPushCount()) != 0) ||
-		((expandLcm % downstream.getPopCount()) != 0)) {
-		throw new RuntimeException("Inconsistent expansion factors.");
-	    }
-	    int upFact   = expandLcm / upstream.getPushCount();
-	    int downFact = expandLcm / downstream.getPopCount();
-	    return new LinearTransformPipeline(upFact, upstream,
-					       downFact, downstream,
-					       (upstream.getPopCount() * upFact)); // new pop count
-	    // check for two FIRs in a row, and handle appropriately
-	} else if (upstream.isFIR() && downstream.isFIR()) {
-	    // actually, to combine FIRs, things are easy. Expand the upstream
-	    // filter by the number of outputs that the downstream filter peeks at.
-	    // then, use the pop count of the downstream filter. (which is going to be 1)
-	    return new LinearTransformPipeline(downstream.getPeekCount(), upstream,
-					       1, downstream,
-					       (downstream.getPopCount()));
-	} else {
-	    // we don't know how to transform this pipeline combination
-	    return new LinearTransformNull("don't know how to combine ([peek,pop,push]):" +
-					   "[" + (upstream.getPeekCount() + "," +
-						  upstream.getPopCount() + "," +
-						  upstream.getPushCount()) + "]-->" +
-					   "[" + (downstream.getPeekCount() + "," +
-						  downstream.getPopCount() + "," +
-						  downstream.getPushCount()) + "]");
-	    
-	}
-    }
-
 }

57e7176716d42f1aa3e280f2979a8cd294f63776
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-12-06 19:38:29 +0000

    Changing the argument to the empty setupBufferLenghts implementation to be of type ScheduleBuffers rather than type Schedule so that we don't end up with a compiler error when we try and compile structures (like the Complex one generated by the new frontend, for instance). I think this might have been Karczma....
    Incidentally, the Makefile in this directory simply doesn't work, we might want to fix that at some point. The error I get is below:
    
    
    cagfarm-43 % make
    make: *** No rule to make target `/u/aalamb/streams//compiler/kopi/classes/streamit/scheduler2/iriter/FilterIter.java', needed by `StreamIt.class'.  Stop.
    cagfarm-43 %
    
    That is all

--- streams/src/streamit/library/Structure.java
+++ streams/src/streamit/library/Structure.java
@@ -7,5 +7,5 @@ public abstract class Structure extends Stream
     // Can't add child streams to a structure.
     public void add(Stream s) { ASSERT(false); }
     public void connectGraph() { }
-    public void setupBufferLengths(Schedule schedule) { }
+    public void setupBufferLengths(ScheduleBuffers schedule) { }
 }

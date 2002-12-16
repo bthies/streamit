38151104b73bdbdfe06d2d7b908b918b5d545e5a
Andrew Lamb <aalamb@cag.csail.mit.edu>
2002-12-16 19:15:45 +0000

    Adding in a pointer to CoarseSerializedBeamFormer.java (automatically generate from the Makefile rather than having a static copy).
    All in preparation for new round of performance testing of Linear Analysis

---
+++ streams/apps/benchmarks/pldi-03/doTest.sh
@@ -0,0 +1,7 @@
+make benchmarks
+make fir-results | mhmail aalamb@mit.edu -s "FIR Done"
+make target-results | mhmail aalamb@mit.edu -s "Target Done"
+make filterbank-results | mhmail aalamb@mit.edu -s "Filterbank Done"
+make sampling-results | mhmail aalamb@mit.edu -s "Sampling rate converter Done"
+make fm-results | mhmail aalamb@mit.edu -s "FM Done"
+make beamformer-results | mhmail aalamb@mit.edu -s "Beamformer Done"

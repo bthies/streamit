0a0ceeca9e68ae0730c669c1ef7d7dc3f95f099e
David Maze <dmaze@cag.csail.mit.edu>
2003-01-06 21:54:44 +0000

    Add SIRWorkFunction and SIRPhasedFilter classes.

---
+++ streams/src/at/dms/kjc/sir/SIRPhasedFilter.java
@@ -0,0 +1,116 @@
+package at.dms.kjc.sir;
+
+import at.dms.kjc.lir.LIRStreamType;
+import at.dms.kjc.*;
+import at.dms.util.*;
+
+/**
+ * A StreamIt phased filter.  Like SIRFilter, this has constant
+ * overall I/O rates; however, the filter is divided into a set of
+ * phases, which execute in some statically determined order.  Thus,
+ * where the execution model for a normal filter is "wait until the
+ * peek rate for the entire filter can be satisfied", or a phased
+ * filter we only wait until the current phase can execute.
+ */
+public class SIRPhasedFilter extends SIRStream implements Cloneable 
+{
+    /**
+     * The input and output types.  Each type is void if and only if this
+     * is a source or sink, respectively.  This means that *all* phases
+     * must have a 0 (peek and pop) or (push) rate.
+     */
+    private CType inputType, outputType;
+
+    /**
+     * Array of phases run by the filter.
+     */
+    private SIRWorkFunction[] phases;
+
+    public SIRPhasedFilter() 
+    {
+        this(null);
+    }
+
+    public SIRPhasedFilter(String ident)
+    {
+        super(null, ident, JFieldDeclaration.EMPTY(), JMethodDeclaration.EMPTY());
+    }
+    
+    public SIRPhasedFilter(SIRContainer parent,
+                           String ident,
+                           JFieldDeclaration[] fields,
+                           JMethodDeclaration[] methods,
+                           SIRWorkFunction[] phases,
+                           JMethodDeclaration work,
+                           CType inputType,
+                           CType outputType)
+    {
+        super(parent, ident, fields, methods);
+        this.phases = phases;
+        this.inputType = inputType;
+        this.outputType = outputType;
+        if (work != null)
+        {
+            setWork(work);
+        }
+    }
+
+    /**
+     * Returns the type of this stream.
+     */
+    public LIRStreamType getStreamType() 
+    {
+        // Might want to create a new type.
+        return LIRStreamType.LIR_FILTER;
+    }
+
+    /**
+     * Copies the state of filter other into this.  Fields that are
+     * objects will be shared instead of cloned.
+     */
+    public void copyState(SIRPhasedFilter other)
+    {
+        this.work = other.work;
+        this.init = other.init;
+        this.inputType = other.inputType;
+        this.outputType = other.outputType;
+        this.parent = other.parent;
+        this.fields = other.fields;
+        this.methods = other.methods;
+        this.phases = other.phases;
+        this.ident = other.ident;
+    }
+    
+    /**
+     * Accepts attribute visitor v at this node.
+     */
+    public Object accept(AttributeStreamVisitor v)
+    {
+        return v.visitPhasedFilter(this,
+                                   fields,
+                                   methods,
+                                   init,
+                                   work,
+                                   phases,
+                                   inputType,
+                                   outputType);
+    }
+    
+    public void setInputType(CType t){
+	this.inputType = t;
+    }
+    public CType getInputType(){
+	return inputType;
+    }
+
+    public void setOutputType(CType t) {
+	this.outputType = t;
+    }
+    public CType getOutputType() {
+	return this.outputType;
+    }
+
+    public String toString() {
+	return "SIRPhasedFilter name=" + getName() + " ident=" + getIdent();
+    }
+}
---
+++ streams/src/at/dms/kjc/sir/SIRWorkFunction.java
@@ -0,0 +1,157 @@
+package at.dms.kjc.sir;
+
+import at.dms.kjc.*;
+import at.dms.util.*;
+
+/**
+ * A work function performs computation in a (phased) filter.  Work
+ * functions will generally be contained within SIRPhasedFilters.
+ * They have code and constants for I/O rates.
+ */
+public class SIRWorkFunction extends SIROperator
+{
+    /**
+     * The number of items that are peeked in each execution.
+     */
+    private JExpression peek;
+    /**
+     * The number of items that are popped in each execution.
+     */
+    private JExpression pop;
+    /**
+     * The number of items that are pushed in each execution.
+     */
+    private JExpression push;
+    /**
+     * The actual work function.
+     */
+    private JMethodDeclaration work;
+    
+    public SIRWorkFunction() 
+    {
+        this.pop = new JIntLiteral(0);
+        this.push = new JIntLiteral(0);
+        this.peek = new JIntLiteral(0);
+        this.work = null;
+    }
+
+    public SIRWorkFunction(SIRContainer parent,
+                           JExpression peek,
+                           JExpression pop,
+                           JExpression push, 
+                           JMethodDeclaration work)
+    {
+        // super(parent);
+        this.peek = peek;
+        this.pop = pop;
+        this.push = push;
+        this.work = work;
+    }
+
+    public void setPeek(JExpression p)
+    {
+        this.peek = p;
+    }
+    
+    public void setPop(JExpression p)
+    {
+        this.pop = p;
+    }
+    
+    public void setPush(JExpression p)
+    {
+        this.push = p;
+    }
+    
+    public void setPeek(int p)
+    {
+        setPeek(new JIntLiteral(p));
+    }
+    
+    public void setPop(int p)
+    {
+        setPop(new JIntLiteral(p));
+    }
+    
+    public void setPush(int p)
+    {
+        setPush(new JIntLiteral(p));
+    }
+    
+    public JExpression getPeek()
+    {
+        return this.peek;
+    }
+    
+    public JExpression getPop()
+    {
+        return this.pop;
+    }
+    
+    public JExpression getPush()
+    {
+        return this.push;
+    }
+    
+    /**
+     * Returns how many items are popped.  This will throw an
+     * exception if the integral numbers haven't been calculated
+     * yet--in this case one can only get the JExpression, but calling
+     * getPop.
+     */
+    public int getPopInt() {
+      if (pop instanceof JFloatLiteral) { //clleger
+	pop = new JIntLiteral(null, (int) ((JFloatLiteral)pop).floatValue());
+      }
+	// need int literal to get number
+	if (!(pop instanceof JIntLiteral)) {
+	    Utils.fail("Trying to get integer value for pop value in filter " + this.getName() + ", but the constant hasn't been resolved yet. " + pop);
+	}
+	return ((JIntLiteral)pop).intValue();
+    }
+
+    /**
+     * Returns how many items are peeked.  This will throw an
+     * exception if the integral numbers haven't been calculated
+     * yet--in this case one can only get the JExpression, but calling
+     * getPeek.
+     */
+    public int getPeekInt() {
+      if (peek instanceof JFloatLiteral) { //clleger
+	peek = new JIntLiteral(null, (int) ((JFloatLiteral)peek).floatValue());
+      }
+	// need int literal to get number
+	if (!(peek instanceof JIntLiteral)) {
+	    Utils.fail("Trying to get integer value for peek value in filter " + this.getIdent() + ", but the constant hasn't been resolved yet. " + peek);
+	}
+	return ((JIntLiteral)peek).intValue();
+    }
+
+    /**
+     * Returns how many items are pushed.This will throw an
+     * exception if the integral numbers haven't been calculated
+     * yet--in this case one can only get the JExpression, but calling
+     * getPush.
+     */
+    public int getPushInt() {
+	// need int literal to get number
+      if (push instanceof JFloatLiteral) { //clleger
+	push = new JIntLiteral(null, (int) ((JFloatLiteral)push).floatValue());
+      }
+
+	if (!(push instanceof JIntLiteral)) {
+	    Utils.fail("Trying to get integer value for push value in filter " + this.getIdent() + ", but the constant hasn't been resolved yet. " + push);
+	}
+	return ((JIntLiteral)push).intValue();
+    }
+
+    public String getIdent()
+    {
+        return "<<work function>>";
+    }
+
+    public Object accept(AttributeStreamVisitor v)
+    {
+        return v.visitWorkFunction(this, this.work);
+    }
+}

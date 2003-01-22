c064dfacf5e91643a0bb7e7a4cb73e03097c693b
Andrew Lamb <aalamb@cag.csail.mit.edu>
2003-01-22 15:27:24 +0000

    Adding hooks for linear redundancy analysis -- I would have waited until I had a complete checkout to do this, but the make system doesn't seem to want to let me recompile after I checked out Bill's changes. Therefore I am checking in all of my work in progress changes so I can recheck out the whole repository and compile from scratch.

--- streams/src/at/dms/kjc/sir/linear/LinearAnalyzer.java
+++ streams/src/at/dms/kjc/sir/linear/LinearAnalyzer.java
@@ -14,7 +14,7 @@ import at.dms.kjc.iterator.*;
  * functions of their inputs, and for those that do, it keeps a mapping from
  * the filter name to the filter's matrix representation.
  *
- * $Id: LinearAnalyzer.java,v 1.15 2002-12-10 20:05:34 aalamb Exp $
+ * $Id: LinearAnalyzer.java,v 1.16 2003-01-22 15:27:24 aalamb Exp $
  **/
 public class LinearAnalyzer extends EmptyStreamVisitor {
     /** Mapping from filters to linear representations. never would have guessed that, would you? **/
@@ -31,8 +31,7 @@ public class LinearAnalyzer extends EmptyStreamVisitor {
 	checkRep();
     }
 
-    ///////// Accessors
-    
+    ///////// Accessors    
     /** Returns true if the specified filter has a linear representation that we have found. **/
     public boolean hasLinearRepresentation(SIRStream stream) {
 	checkRep();
@@ -71,6 +70,10 @@ public class LinearAnalyzer extends EmptyStreamVisitor {
 	this.filtersToLinearRepresentation.put(key,rep);
 	checkRep();
     }
+    /** gets an iterator over all of the linear representations that this LinearAnalyzer knows about **/
+    public Iterator getFilterIterator() {
+	return this.filtersToLinearRepresentation.keySet().iterator();
+    }
     
     /**
      * Main entry point -- searches the passed stream for
@@ -84,8 +87,14 @@ public class LinearAnalyzer extends EmptyStreamVisitor {
 	LinearPrinter.println("aal--In linear filter visitor");
 	LinearAnalyzer lfa = new LinearAnalyzer();
 	IterFactory.createIter(str).accept(lfa);
+
 	// generate a report and print it.
 	LinearPrinter.println(lfa.generateReport());
+	
+	// now, run a redundancy analysis pass and print the results
+	LinearRedundancyAnalyzer redundancy = new LinearRedundancyAnalyzer(lfa);
+	LinearPrinter.println(redundancy.toString());
+
 	return lfa;
     }
     

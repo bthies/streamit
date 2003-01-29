c85357c7148c5ee7d8386cb70bdd1d5f42e96d64
Andrew Lamb <aalamb@cag.csail.mit.edu>
2003-01-29 22:12:31 +0000

    Bring the regtest template up to date based on my experience of adding the PLDI benchmarks to the regtest (in their own TestSuite).

--- streams/regtest/streamittest/TestTemplate.java
+++ streams/regtest/streamittest/TestTemplate.java
@@ -6,7 +6,7 @@
  * 4. Add a line in suite() with the new test method name
  *
  * You can then use the CompilerInterface compiler to run compiler sessions.
- * $Id: TestTemplate.java,v 1.3 2002-06-28 22:18:38 aalamb Exp $
+ * $Id: TestTemplate.java,v 1.4 2003-01-29 22:12:31 aalamb Exp $
  **/
 package streamittest;
 
@@ -26,12 +26,32 @@ public class TestTemplate extends StreamITTestCase {
 	}
     }
 
+
+    /**
+     * The main entry point for running alone. Eg if we run
+     * java junit.textui.TestRunner streamittest.TestTemplate
+     * this method will be called to determine which tests to run.
+     * If this method is not here, then the default action of the
+     * test runner is to execute all public void methods that start
+     * with "test" which it determines by reflection. Note that this
+     * method does not have the flags argument
+     **/
+    public static Test suite() {
+	int testFlags = CompilerInterface.NONE;
+	TestSuite suite = new TestSuite();
+	suite.addTest(TestTemplate.suite(testFlags));
+	
+	return suite;
+    }
+
+    
+    /** Returns the full suite of tests for this particular set of apps. **/
     public static Test suite(int flags) {
 	TestSuite suite = new TestSuite();
 	suite.addTest(new TestTemplate("testSimple", flags));
 	
 	return suite;
-    }
+    }    
     
     public void testSimple() {
 	assertTrue("was true", true);

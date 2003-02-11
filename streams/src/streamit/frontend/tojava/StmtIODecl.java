21b8bcb3369f70003c20404eb8caab2e2c8fe16a
David Maze <dmaze@cag.csail.mit.edu>
2003-02-11 20:46:57 +0000

    I'm off on this javadoc kick.  Yay.  Add javadoc comments everywhere appropriate.

--- streams/src/streamit/frontend/tojava/StmtIODecl.java
+++ streams/src/streamit/frontend/tojava/StmtIODecl.java
@@ -1,9 +1,3 @@
-/*
- * StmtIODecl.java: an old-syntax I/O rate declaration
- * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: StmtIODecl.java,v 1.2 2002-09-23 16:32:54 dmaze Exp $
- */
-
 package streamit.frontend.tojava;
 
 import streamit.frontend.nodes.*;
@@ -11,7 +5,12 @@ import streamit.frontend.nodes.*;
 /**
  * An old-syntax I/O rate declaration.  This has a name, a type, and one
  * or two integer rates.  It gets translated to a statement like
- *   name = new Channel(type.class, rate1, rate2);
+ * <pre>
+ * name = new Channel(type.class, rate1, rate2);
+ * </pre>
+ *
+ * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
+ * @version $Id: StmtIODecl.java,v 1.3 2003-02-11 20:46:57 dmaze Exp $
  */
 public class StmtIODecl extends streamit.frontend.nodes.Statement
 {
@@ -19,6 +18,20 @@ public class StmtIODecl extends streamit.frontend.nodes.Statement
     private Type type;
     private Expression rate1, rate2;
     
+    /**
+     * Creates a new I/O rate declaration.  The name should be either
+     * "input" or "output".  If it is "output", <code>rate1</code> is the
+     * push rate.  If the name is "input", <code>rate1</code> is the
+     * pop rate, and <code>rate2</code> is the peek rate.  The second rate
+     * can be <code>null</code> if it is unnecessary or it is equal to the
+     * first rate.
+     *
+     * @param context  Context this statement appears in
+     * @param name     Name of the channel
+     * @param type     Type of objects on this channel
+     * @param rate1    Push or pop rate
+     * @param rate2    Peek rate or null
+     */
     public StmtIODecl(FEContext context, String name, Type type,
                       Expression rate1, Expression rate2)
     {
@@ -29,27 +42,41 @@ public class StmtIODecl extends streamit.frontend.nodes.Statement
         this.rate2 = rate2;
     }
     
+    /**
+     * Creates a new I/O rate declaration with a single rate.
+     *
+     * @param context  Context this statement appears in
+     * @param name     Name of the channel
+     * @param type     Type of objects on this channel
+     * @param rate     Push or pop rate
+     */
     public StmtIODecl(FEContext context, String name, Type type,
                       Expression rate)
     {
         this(context, name, type, rate, null);
     }
     
+    /** Returns the name of the channel being declared. */
     public String getName()
     {
         return name;
     }
     
+    /** Returns the type of items on the channel. */
     public Type getType()
     {
         return type;
     }
     
+    /** Returns the pop or push rate of the stream. */
     public Expression getRate1()
     {
         return rate1;
     }
     
+    /** Returns the peek rate of the stream.  Returns null if this is an
+     * output channel or if the peek rate is undeclared and implicitly
+     * equal to the pop rate. */
     public Expression getRate2()
     {
         return rate2;

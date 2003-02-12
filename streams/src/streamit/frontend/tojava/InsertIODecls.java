c37b63b8435719c0a2fdf878145692a197a5fcc0
David Maze <dmaze@cag.csail.mit.edu>
2003-02-12 22:27:04 +0000

    Add a code path to output the Java library syntax for phased filters. This requires that the NoticePhasedFilters pass hasn't been run. While we're at it, update the javadocs (whee!).

--- streams/src/streamit/frontend/tojava/InsertIODecls.java
+++ streams/src/streamit/frontend/tojava/InsertIODecls.java
@@ -1,9 +1,3 @@
-/*
- * InsertIODecls.java: insert rate declarations in init functions
- * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: InsertIODecls.java,v 1.2 2002-09-23 19:11:57 dmaze Exp $
- */
-
 package streamit.frontend.tojava;
 
 import streamit.frontend.nodes.*;
@@ -14,13 +8,42 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
- * Insert I/O rate declarations into filters' init functions.
+ * Insert I/O rate declarations into filters' init functions.  For
+ * programs destined for the Java library, this also inserts the phase
+ * declarations into the init function.  For programs headed for the
+ * StreamIt compiler, phase construction happens implicitly with calls
+ * inserted in <code>NodesToJava</code>.
+ *
+ * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
+ * @version $Id: InsertIODecls.java,v 1.3 2003-02-12 22:27:04 dmaze Exp $
  */
 public class InsertIODecls extends InitMunger
 {
-    // Look for a work function.  Preferentially return an anonymous
-    // work function, if one exists; otherwise, use the first one we
-    // find.  Return null if there is no work function in fns.
+    private boolean libraryFormat;
+    
+    /**
+     * Creates a new pass object.  If <code>library</code> is true,
+     * create code suitable for the Java library.  This affects the
+     * output of phased filters; a filter's work function must either
+     * declare I/O rates or consist entirely of phase calls for
+     * library-form output.
+     *
+     * @param library  Generate code for the Java library
+     */
+    public InsertIODecls(boolean library)
+    {
+        libraryFormat = library;
+    }
+
+    /**
+     * Given a list of functions, find a work function.  If the list
+     * contains an anonymous work function, return that; otherwise,
+     * return the first work function, or null if there are no work
+     * functions at all.
+     *
+     * @param fns   List of functions to search
+     * @return      Primary work function in fns
+     */
     public static Function findWork(List fns)
     {
         Function work = null;
@@ -45,6 +68,62 @@ public class InsertIODecls extends InitMunger
         // Only visit filters.
         if (spec.getType() != StreamSpec.STREAM_FILTER)
             return spec;
+
+        if (libraryFormat)
+            return ssLibrary(spec);
+        else
+            return ssCompiler(spec);
+    }
+    
+    /** Do the rewrite work for the library flow. */
+    private Object ssLibrary(StreamSpec spec)
+    {
+        List fns = new ArrayList(spec.getFuncs());
+        // Assert that this class cast works.
+        FuncWork work = (FuncWork)findWork(fns);
+        StreamType st = spec.getStreamType();
+        List newStmts = new ArrayList();
+        newStmts.add(new StmtSetTypes(work.getContext(), st));
+        // We need to add phases.  Is this a phased filter?
+        if (work.getPopRate() == null && work.getPushRate() == null)
+        {
+            // Analyze the phased filter's work function's body to get the
+            // list of phases.
+            StmtBlock body = (StmtBlock)work.getBody();
+            for (Iterator iter = body.getStmts().iterator(); iter.hasNext(); )
+            {
+                Statement stmt = (Statement)iter.next();
+                // The statement should be a function call to a phase
+                // function, nothing else.  If you're looking at this
+                // code because you're getting an unexpected
+                // ClassCastException, it's probably because your
+                // code's work function contained something other than
+                // calls to phase functions.
+                StmtExpr se = (StmtExpr)stmt;
+                ExprFunCall fc = (ExprFunCall)se.getExpression();
+                FuncWork phase = (FuncWork)spec.getFuncNamed(fc.getName());
+                
+                // Now add the phase.
+                newStmts.add
+                    (new StmtAddPhase(work.getContext(), false, phase));
+            }
+        }
+        else
+        {
+            // Add the work function as the only phase.
+            newStmts.add(new StmtAddPhase(work.getContext(), false, work));
+        }
+
+        fns = replaceInitWithPrepended(spec.getContext(), fns, newStmts);
+        
+        return new StreamSpec(spec.getContext(), spec.getType(),
+                              spec.getStreamType(), spec.getName(),
+                              spec.getParams(), spec.getVars(), fns);
+    }
+
+    /** Do the rewrite work for the compiler flow. */
+    private Object ssCompiler(StreamSpec spec)
+    {
         List fns = new ArrayList(spec.getFuncs());
         // Assert that this class cast works.
         FuncWork work = (FuncWork)findWork(fns);

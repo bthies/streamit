eb8bd56005edc9e0a907b055a1e80d331abe56db
David Maze <dmaze@cag.csail.mit.edu>
2003-02-19 18:55:12 +0000

    Copy the setIOTypes() from karczma's Filter phased syntax.  Turns out if this isn't there, nothing sets the I/O types of phased filters. "Oops."

--- streams/src/streamit/library/PhasedFilter.java
+++ streams/src/streamit/library/PhasedFilter.java
@@ -4,16 +4,28 @@ import streamit.scheduler2.ScheduleBuffers;
 
 public abstract class PhasedFilter extends Stream
 {
+    private Class inType, outType;
+    
     public PhasedFilter() { super(); }
     public PhasedFilter(int a) { super(a); }
     
     // add was present in Operator, but is not defined in Filter anymore
     public void add(Stream s) { ASSERT (false); }
 
+    public void setIOTypes(Class in, Class out)
+    {
+        inType = in;
+        outType = out;
+    }
+
     // This function should definitely do something.  Don't support
     // old-style init functions, though.
     public void connectGraph()
     {
+        // Create channels from the setIOTypes() types.
+        input = new Channel(inType);
+        output = new Channel(outType);
+        
         Channel myInput = getInputChannel();
         Channel myOutput = getOutputChannel();
         

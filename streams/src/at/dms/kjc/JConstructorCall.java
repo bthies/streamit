e15281e2fe3a2644438e40661f17b985a6f7b51e
Bill Thies <bthies@gmail.com>
2003-03-14 19:40:07 +0000

    Removing check for constructor existence, and changing it to a warning instead.  Looks something like this:
    WARNING:  Referring to constructor that doesn't exist in Java library:
        StreamIt(int, int, int, int, int, int, int, int, int, int);
      This program will compile fine, but it will not run in the Java library
      without adding support for the above init function signature.  To extend
      the library to support this signature, follow the instructions in:
        streams/docs/implementation-notes/library-init-functions.txt

--- streams/src/at/dms/kjc/JConstructorCall.java
+++ streams/src/at/dms/kjc/JConstructorCall.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: JConstructorCall.java,v 1.2 2001-10-02 19:25:04 mgordon Exp $
+ * $Id: JConstructorCall.java,v 1.3 2003-03-14 19:40:07 thies Exp $
  */
 
 package at.dms.kjc;
@@ -29,174 +29,193 @@ import at.dms.compiler.UnpositionedError;
  */
 public class JConstructorCall extends JExpression {
 
-  /**
-   * Construct a node in the parsing tree
-   * This method is directly called by the parser
-   * @param	where		the line of this node in the source code
-   * @param	functorIsThis	true if functor is "this" (else "super")
-   * @param	arguments	the argument of the call
-   */
-  public JConstructorCall(TokenReference where,
-			  boolean functorIsThis,
-			  JExpression[] arguments)
-  {
-    super(where);
+    /**
+     * Construct a node in the parsing tree
+     * This method is directly called by the parser
+     * @param	where		the line of this node in the source code
+     * @param	functorIsThis	true if functor is "this" (else "super")
+     * @param	arguments	the argument of the call
+     */
+    public JConstructorCall(TokenReference where,
+			    boolean functorIsThis,
+			    JExpression[] arguments)
+    {
+	super(where);
 
-    this.functorIsThis = functorIsThis;
-    this.arguments = arguments;
-  }
-
-  // ----------------------------------------------------------------------
-  // ACCESSORS
-  // ----------------------------------------------------------------------
-
-  /**
-   * Returns the called method.
-   */
-  public CMethod getMethod() {
-    return method;
-  }
-
-  /**
-   * Returns true if it's this() else it's super().
-   */
-  final boolean isThisInvoke() {
-    return functorIsThis;
-  }
-
-  /**
-   * !!!
-   */
-  public CType getType() {
-    return null;
-  }
-
-  // ----------------------------------------------------------------------
-  // SEMANTIC ANALYSIS
-  // ----------------------------------------------------------------------
-
-  /**
-   * Analyses the expression (semantically).
-   * @param	context		the analysis context
-   * @return	an equivalent, analysed expression
-   * @exception	PositionedError	the analysis detected an error
-   */
-  public JExpression analyse(CExpressionContext context) throws PositionedError {
-    ((CConstructorContext)context.getMethodContext()).setSuperConstructorCalled(false);
-
-    // !!! check in constructor !!!
-
-    CType[]	argsType = new CType[arguments.length];
-    for (int i = 0; i < argsType.length; i++) {
-      arguments[i] = arguments[i].analyse(context);
-
-      argsType[i] = arguments[i].getType();
-      assert(argsType[i] != null);
-    }
-    inClass = context.getClassContext().getCClass();
-
-    if (functorIsThis) {
-      clazz = context.getClassContext().getCClass();
-    } else {
-      clazz = context.getClassContext().getCClass().getSuperClass();
+	this.functorIsThis = functorIsThis;
+	this.arguments = arguments;
     }
 
-    assert(clazz != null);
-    try {
-      method = clazz.lookupMethod(context.getClassContext().getCClass(), JAV_CONSTRUCTOR, argsType);
-    } catch (UnpositionedError e) {
-      throw e.addPosition(getTokenReference());
-    }
-    check(context, method != null, KjcMessages.CONSTRUCTOR_NOTFOUND, clazz.getIdent());
+    // ----------------------------------------------------------------------
+    // ACCESSORS
+    // ----------------------------------------------------------------------
 
-    if (method.getOwner() != clazz) {
-      // May be an inner class
-      if (clazz.isNested()) {
-	CType[]		argsType2 = new CType[argsType.length + 1];
-	System.arraycopy(argsType, 0, argsType2, 0, argsType.length);
-	argsType2[argsType.length] = clazz.getOwner().getType();
-	try {
-	  method = clazz.lookupMethod(context.getClassContext().getCClass(), JAV_CONSTRUCTOR, argsType2);
-	} catch (UnpositionedError e) {
-	  throw e.addPosition(getTokenReference());
+    /**
+     * Returns the called method.
+     */
+    public CMethod getMethod() {
+	return method;
+    }
+
+    /**
+     * Returns true if it's this() else it's super().
+     */
+    final boolean isThisInvoke() {
+	return functorIsThis;
+    }
+
+    /**
+     * !!!
+     */
+    public CType getType() {
+	return null;
+    }
+
+    // ----------------------------------------------------------------------
+    // SEMANTIC ANALYSIS
+    // ----------------------------------------------------------------------
+
+    /**
+     * Analyses the expression (semantically).
+     * @param	context		the analysis context
+     * @return	an equivalent, analysed expression
+     * @exception	PositionedError	the analysis detected an error
+     */
+    public JExpression analyse(CExpressionContext context) throws PositionedError {
+	((CConstructorContext)context.getMethodContext()).setSuperConstructorCalled(false);
+
+	// !!! check in constructor !!!
+
+	CType[]	argsType = new CType[arguments.length];
+	for (int i = 0; i < argsType.length; i++) {
+	    arguments[i] = arguments[i].analyse(context);
+
+	    argsType[i] = arguments[i].getType();
+	    assert(argsType[i] != null);
 	}
-      }
-      if (method.getOwner() != clazz) {
-	// do not want a super constructor !
-	throw new CMethodNotFoundError(getTokenReference(), null, clazz.getType().toString(), argsType);
-      }
+	inClass = context.getClassContext().getCClass();
+
+	if (functorIsThis) {
+	    clazz = context.getClassContext().getCClass();
+	} else {
+	    clazz = context.getClassContext().getCClass().getSuperClass();
+	}
+
+	assert(clazz != null);
+	try {
+	    method = clazz.lookupMethod(context.getClassContext().getCClass(), JAV_CONSTRUCTOR, argsType);
+	} catch (UnpositionedError e) {
+	    throw e.addPosition(getTokenReference());
+	}
+
+	// changed for streamit: only give warning if can't find
+	// superclass constructor, since this probably represents a case
+	// where a Pipeline, etc. constructor doesn't exist (but won't be called)
+	if (method==null) {
+	    System.err.print("WARNING:  Referring to constructor that doesn't exist in Java library:\n" +
+			     "    " + clazz.getIdent() + "(");
+	    for (int i=0; i<argsType.length; i++) {
+		System.err.print(argsType[i]);
+		if (i<argsType.length-1) {
+		    System.err.print(", ");
+		}
+	    }
+	    System.err.println(");");
+	    System.err.println("  This program will compile fine, but it will not run in the Java library\n" +
+			       "  without adding support for the above init function signature.  To extend\n" + 
+			       "  the library to support this signature, follow the instructions in:\n" +
+			       "    streams/docs/implementation-notes/library-init-functions.txt\n");
+	} else {
+	    //check(context, method != null, KjcMessages.CONSTRUCTOR_NOTFOUND, clazz.getIdent());
+
+	    if (method.getOwner() != clazz) {
+		// May be an inner class
+		if (clazz.isNested()) {
+		    CType[]		argsType2 = new CType[argsType.length + 1];
+		    System.arraycopy(argsType, 0, argsType2, 0, argsType.length);
+		    argsType2[argsType.length] = clazz.getOwner().getType();
+		    try {
+			method = clazz.lookupMethod(context.getClassContext().getCClass(), JAV_CONSTRUCTOR, argsType2);
+		    } catch (UnpositionedError e) {
+			throw e.addPosition(getTokenReference());
+		    }
+		}
+		if (method.getOwner() != clazz) {
+		    // do not want a super constructor !
+		    throw new CMethodNotFoundError(getTokenReference(), null, clazz.getType().toString(), argsType);
+		}
+	    }
+
+	    CClassType[]	exceptions = method.getThrowables();
+	    for (int i = 0; i < exceptions.length; i++) {
+		if (exceptions[i].isCheckedException()) {
+		    context.getBodyContext().addThrowable(new CThrowableInfo(exceptions[i], this));
+		}
+	    }
+
+	    check(context, !context.getMethodContext().getCMethod().isStatic(), KjcMessages.BAD_THIS_STATIC);
+
+	    argsType = method.getParameters();
+	    for (int i = 0; i < arguments.length; i++) {
+		arguments[i] = arguments[i].convertType(argsType[i], context);
+	    }
+	    ((CConstructorContext)context.getMethodContext()).setSuperConstructorCalled(true);
+	}
+	return this;
     }
 
-    CClassType[]	exceptions = method.getThrowables();
-    for (int i = 0; i < exceptions.length; i++) {
-      if (exceptions[i].isCheckedException()) {
-	context.getBodyContext().addThrowable(new CThrowableInfo(exceptions[i], this));
-      }
+    // ----------------------------------------------------------------------
+    // CODE GENERATION
+    // ----------------------------------------------------------------------
+
+    /**
+     * Accepts the specified visitor
+     * @param	p		the visitor
+     */
+    public void accept(KjcVisitor p) {
+	p.visitConstructorCall(this, functorIsThis, arguments);
     }
 
-    check(context, !context.getMethodContext().getCMethod().isStatic(), KjcMessages.BAD_THIS_STATIC);
-
-    argsType = method.getParameters();
-    for (int i = 0; i < arguments.length; i++) {
-      arguments[i] = arguments[i].convertType(argsType[i], context);
-    }
-    ((CConstructorContext)context.getMethodContext()).setSuperConstructorCalled(true);
-
-    return this;
-  }
-
-  // ----------------------------------------------------------------------
-  // CODE GENERATION
-  // ----------------------------------------------------------------------
-
-  /**
-   * Accepts the specified visitor
-   * @param	p		the visitor
-   */
-  public void accept(KjcVisitor p) {
-    p.visitConstructorCall(this, functorIsThis, arguments);
-  }
-
- /**
-   * Accepts the specified attribute visitor
-   * @param	p		the visitor
-   */
-  public Object accept(AttributeVisitor p) {
-      return    p.visitConstructorCall(this, functorIsThis, arguments);
-  }
-
-  /**
-   * Generates JVM bytecode to evaluate this expression.
-   *
-   * @param	code		the bytecode sequence
-   * @param	discardValue	discard the result of the evaluation ?
-   */
-  public void genCode(CodeSequence code, boolean discardValue) {
-    setLineNumber(code);
-    code.plantLoadThis();
-
-    CClass      owner = method.getOwner();
-
-    if (owner.isNested() && owner.hasOuterThis()) {
-      clazz.genSyntheticParamsFromExplicitSuper(inClass.isQualifiedAndAnonymous(), code);
-    }
-    for (int i = 0; i < arguments.length; i++) {
-      arguments[i].genCode(code, false);
+    /**
+     * Accepts the specified attribute visitor
+     * @param	p		the visitor
+     */
+    public Object accept(AttributeVisitor p) {
+	return    p.visitConstructorCall(this, functorIsThis, arguments);
     }
 
-    method.genCode(code, true);
+    /**
+     * Generates JVM bytecode to evaluate this expression.
+     *
+     * @param	code		the bytecode sequence
+     * @param	discardValue	discard the result of the evaluation ?
+     */
+    public void genCode(CodeSequence code, boolean discardValue) {
+	setLineNumber(code);
+	code.plantLoadThis();
 
-    // The return type is void : there is no result value.
-  }
+	CClass      owner = method.getOwner();
 
-  // ----------------------------------------------------------------------
-  // DATA MEMBERS
-  // ----------------------------------------------------------------------
+	if (owner.isNested() && owner.hasOuterThis()) {
+	    clazz.genSyntheticParamsFromExplicitSuper(inClass.isQualifiedAndAnonymous(), code);
+	}
+	for (int i = 0; i < arguments.length; i++) {
+	    arguments[i].genCode(code, false);
+	}
 
-  private boolean		functorIsThis;
-  private JExpression[]		arguments;
+	method.genCode(code, true);
 
-  private CClass		clazz;
-  private CClass		inClass;
-  private CMethod		method;
+	// The return type is void : there is no result value.
+    }
+
+    // ----------------------------------------------------------------------
+    // DATA MEMBERS
+    // ----------------------------------------------------------------------
+
+    private boolean		functorIsThis;
+    private JExpression[]		arguments;
+
+    private CClass		clazz;
+    private CClass		inClass;
+    private CMethod		method;
 }

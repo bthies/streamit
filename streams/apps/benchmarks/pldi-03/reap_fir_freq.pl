02705b18b5a00f92a019b74ec910699e76864ddb
Andrew Lamb <aalamb@cag.csail.mit.edu>
2003-03-17 16:05:41 +0000

    Refactoring the scripts that I have been using to gather numbers for the PLDI linear paper. Some name changes are iminent...

--- streams/apps/benchmarks/pldi-03/reap_fir_freq.pl
+++ streams/apps/benchmarks/pldi-03/reap_fir_freq.pl
@@ -5,22 +5,13 @@
 # operation reductions.
 
 use strict;
-
-# the streamit frontend, the streamit compiler, the C compiler and dynamorio
-my $STREAMIT_COMPILER    = "java -Xmx1500M  at.dms.kjc.Main -s";
-my $STREAMIT_GCC         = "gcc -O2 -I/u/aalamb/streams/library/c -L/u/aalamb/streams/library/c";
-my $STREAMIT_GCC_POSTFIX = "-lstreamit -lsrfftw -lsfftw -lm";
-my $STREAMIT_DYNAMORIO   = "dynamorio";
+require "reaplib.pl";
 
 my $STANDARD_OPTIONS = "--unroll 100000 --debug";
 my $FREQ_OPTIONS     = "--frequencyreplacement 3";
 
 # the filename to write out the results to. (append first command line arg to name)
 my $RESULTS_FILENAME = "freq_fir_results.tsv";
-
-# the number of iterations to run the program for
-my $NUM_ITERS = 10000;
-
 my $PROGRAM_NAME = "Test";
 
 # array to hold results
@@ -40,7 +31,7 @@ my $i;
 # for various FIR lengths
 my @fir_lengths;
 #for ($i=1; $i<32; $i*=sqrt(2)) {
-for ($i=1; $i<32; $i++) {
+for ($i=1; $i<2; $i++) {
     push(@fir_lengths, int($i));
 }
 
@@ -50,36 +41,16 @@ foreach $firLength (@fir_lengths) {
     set_fir_length("$PROGRAM_NAME.java", $firLength);
 
     # compile normally
-    print "$PROGRAM_NAME($firLength, normal):";
-    do_compile(".", $PROGRAM_NAME, "$STANDARD_OPTIONS");
-    
-    # figure out how many outputs are produced 
-    my $normal_outputs = get_output_count(".", $PROGRAM_NAME) * $NUM_ITERS;
-    
-    # run the dynamo rio test and get back the results
-    my $report = run_test(".", $PROGRAM_NAME, $NUM_ITERS);
-    print "\n";
-    
-    # extract the flops, fadds and fmul count from the report
-    my ($normal_flops) =  $report =~ m/saw (.*) flops/;
-    my ($normal_fadds) =  $report =~ m/saw (.*) fadds/;
-    my ($normal_fmuls) =  $report =~ m/saw (.*) fmuls/;
-    
+    my ($normal_outputs, $normal_flops, 
+	$normal_fadds, $normal_fmuls) = do_test(".", $PROGRAM_NAME, 
+						"$STANDARD_OPTIONS",
+						"normal($firLength)");
 
-    print "$PROGRAM_NAME($firLength, freq):";
-    do_compile(".", $PROGRAM_NAME, "$STANDARD_OPTIONS $FREQ_OPTIONS");
-    
-    # figure out how many outputs are produced 
-    my $freq_outputs = get_output_count(".", $PROGRAM_NAME) * $NUM_ITERS;
-	
-    # run the dynamo rio test and get back the results
-    $report = run_test(".", $PROGRAM_NAME, $NUM_ITERS);
-    print "\n";
-    
-    # extract the flops, fadds and fmul count from the report
-    my ($freq_flops) =  $report =~ m/saw (.*) flops/;
-    my ($freq_fadds) =  $report =~ m/saw (.*) fadds/;
-    my ($freq_fmuls) =  $report =~ m/saw (.*) fmuls/;
+    # compile with frequency replacement
+    my ($freq_outputs, $freq_flops, 
+	$freq_fadds, $freq_fmuls) = do_test(".", $PROGRAM_NAME, 
+						"$STANDARD_OPTIONS $FREQ_OPTIONS",
+						"freq  ($firLength)");
 
     push(@result_lines, 
 	 "$PROGRAM_NAME\t$firLength\t".
@@ -96,117 +67,3 @@ close RFILE;
 print "done\n";
 
 
-########
-# Subroutine to compile the specified file.
-# usage: do_test($path, $filename, $compiler_options);
-########
-sub do_compile {
-    my $new_path     = shift || die ("no path passed to do_test.");
-    my $filename_base = shift || die ("no filename passed to do_test.");
-    my $options  = shift;
-
-    # run streamit compiler to generate C code.
-    print "(java->c)";
-    `$STREAMIT_COMPILER $options $new_path/$filename_base.java >& $new_path/$filename_base.c`;
-    # compile the C code to generate an executable
-    print "(c->exe)";
-    `$STREAMIT_GCC $new_path/$filename_base.c $STREAMIT_GCC_POSTFIX -o $new_path/$filename_base.exe`;
-
-}
-
-#######
-# Subroutine to execute the program with dynamo
-sub run_test {
-    my $new_path = shift || die ("no new path passed");
-    my $filename_base = shift || die("no filename base passed");
-    my $iters = shift || die ("no iters passed");
-
-    # remove the old countflops file (if things in rio don't go well, we want to report real results)
-    `rm -f $new_path/countflops.log`;
-
-    # run dynamo rio (with the assumed countflops module installed)
-    print "(dynamo $iters)";
-    `cd $new_path; $STREAMIT_DYNAMORIO $new_path/$filename_base.exe -i $iters >& /dev/null`;
-
-    # get the report from the countflops.log file and clean up
-    my $report = read_file("$new_path/countflops.log");
-    return($report);
-
-}
-
-
-
-# replaces the line 
-# add(new LowPassFilter(10));
-# with 
-# add(new LowPassFilter($targetLength));
-sub set_fir_length {
-    my $filename   = shift || die ("no target length");
-    my $fir_length = shift || die ("no fir length");
-
-    my $contents = read_file($filename);
-    # do the find and replace
-    $contents =~ s/new LowPassFilter\(1, \(3.141592653589793f \/ 3\), \d*\)/new LowPassFilter\(1, \(3.141592653589793f \/ 3\), $fir_length\)/i;
-    
-    # write back the modified file
-    write_file($contents, $filename);
-}
-
-
-# Gets the number of outputs produced by one iteration of the stream steady state
-sub get_output_count {
-    my $path = shift || die ("no path");
-    my $filename_base = shift || die ("no filename base");
-    
-    # run the program for one iter piping its output to wc
-    # whose output we will parse and return a value.
-    my $wc_results = `$path/$filename_base.exe -i 1 | wc -l`;
-    my ($count) = $wc_results =~ m/(\d*)\n/gi;
-    return $count;
-}
-
-# get the actual N of the output.
-sub get_N {
-    my $path = shift || die ("no path");
-    my $filename_base = shift || die ("no filename base");
-    # read in the c file and extract the information from the comments.
-    my $contents = read_file("$path/$filename_base.c");
-    my ($N) = $contents =~ m/N=(\d*)/gi;
-    return $N;
-}
-
-
-# reads in a file and returns its contents as a scalar variable
-# usage: read_file($filename)
-sub read_file {
-    my $filename = shift || die ("no filename passed to read_file\n");
-
-    open(INFILE, "<$filename") || die ("could not open $filename");
-    
-    # save the old standard delimiter
-    my $old_delim = local($/);
-    # set the standad delimiter to undef so we can read
-    # in the file in one go
-    local($/) = undef; 
-
-    # read in file contents
-    my $file_contents = <INFILE>;
-    close(INFILE);
-    
-    # restore the standard delimiter
-    local($/) = $old_delim;
-
-    return $file_contents;
-}
-
-# writes the contents of the first scalar argument to the 
-# filename in the second argument
-# usage: write_file($data, $filename)
-sub write_file {
-    my $data = shift || die("No data passed to write_file");
-    my $filename = shift || die("No filename passed to write_file");
-    
-    open(OUTFILE, ">$filename");
-    print OUTFILE $data;
-    close(OUTFILE);
-}
--- streams/apps/benchmarks/pldi-03/reap_optimizations.pl
+++ streams/apps/benchmarks/pldi-03/reap_optimizations.pl
@@ -17,22 +17,10 @@
 # this script just uses the default one.
 
 use strict;
-
-# the streamit frontend, the streamit compiler, the C compiler and dynamorio
-my $STREAMIT_FRONTEND    = "java streamit.frontend.ToJava --full";
-my $STREAMIT_COMPILER    = "java -Xmx1500M  at.dms.kjc.Main -s";
-my $STREAMIT_GCC         = "gcc -O2 -lm -I/u/aalamb/streams/library/c -L/u/aalamb/streams/library/c";
-my $STREAMIT_GCC_POSTFIX = "-lstreamit -lsrfftw -lsfftw -lm";
-my $STREAMIT_DYNAMORIO   = "dynamorio";
-
-# the program to use to compare output
-my $CMP = "/u/aalamb/streams/regtest/tools/compare_uni/pl";
+require "reaplib.pl";
 
 # the filename to write out the results to.
 my $RESULTS_FILENAME = "freq_results.tsv";
-
-# the number of iterations to run the program for
-my $NUM_ITERS = 10000;
 
 # format for script programs is 
 #directory:filename:max size for frequency
@@ -117,9 +105,10 @@ foreach $current_program (@programs) {
    # now, run with redundant elimination
    my ($redund_outputs, $redund_flops, 
        $redund_fadds, $redund_fmuls) = do_test($path, $base_filename, 
-					       "--unroll 100000 --debug --redundantreplacement",
+					       "--debug --redundantreplacement",
+					       #"--unroll 100000 --debug --redundantreplacement",
 					       "$base_filename(redund)");
-   save_output($path, $base_filename, "redund");
+   save_output($path, $base_filename, "redu");
 
    
    my $new_data_line = 	     ("$base_filename\t".
@@ -153,155 +142,4 @@ close(MHMAIL);
 print "(done)\n";
 
 
-#########
-# subroutine to do a compile test, and parse results.
-# Return value is (outputs, flops, fadds, fmuls).
-# do_test($path, $base_filename, $options, $descr)
-#########
-sub do_test {
-    my $path = shift || die ("no flops");
-    my $base_filename = shift || die ("no base");
-    my $options = shift || die("no options");
-    my $descr = shift || die ("no description");
-    
-    # compile with specified options
-    print "$descr:";
-    do_compile($path, $base_filename, $options);
-    
-    # figure out how many outputs are produced
-    my $outputs = get_output_count($path, $base_filename, $NUM_ITERS);
-    
-    # run the dynamo rio test and get back the results
-    my $report = run_rio($path, $base_filename, $descr, $NUM_ITERS);
-    print "\n";
-    
-    # extract the flops, fadds and fmul count from the report
-    my ($flops) =  $report =~ m/saw (.*) flops/;
-    my ($fadds) =  $report =~ m/saw (.*) fadds/;
-    my ($fmuls) =  $report =~ m/saw (.*) fmuls/;
 
-    return ($outputs, $flops, $fadds, $fmuls);
-}
-
-#############
-# This subroutine saves the current c and exe file in path
-# by appending the tag to their names.
-#############
-sub save_output {
-    my $path          = shift || die("no path");
-    my $base_filename = shift || die("no base filename");
-    my $tag           = shift || die("no tag");
-    
-    #print "(saving $tag)\n";
-    # copy c file
-    #print `cp $path/$base_filename.c $path/$base_filename-$tag.c`;
-    # copy exe file
-    #print `cp $path/$base_filename.exe $path/$base_filename-$tag.exe`;
-}
-
-
-
-
-
-########
-# Subroutine to compile the specified file.
-# usage: do_compile($path, $filename, $compiler_options);
-########
-sub do_compile {
-    my $new_path      = shift || die ("no path passed to do_compile.");
-    my $filename_base = shift || die ("no filename passed to do_compile.");
-    my $options       = shift;
-
-    # run streamit compiler to generate C code.
-    print "(java->c)";
-    `cd $new_path; $STREAMIT_COMPILER $options $filename_base.java >& $filename_base.c`;
-    # compile the C code to generate an executable
-    print "(c->exe)";
-    `cd $new_path; $STREAMIT_GCC $filename_base.c -o $filename_base.exe $STREAMIT_GCC_POSTFIX`;
-
-}
-
-#######
-# Subroutine to execute the program with dynamo
-sub run_rio {
-    my $new_path      = shift || die ("no new path passed");
-    my $filename_base = shift || die ("no filename base passed");
-    my $postfix       = shift || die ("no postfix specified.");
-    my $iters         = shift || die ("no iters passed");
-
-    # remove the old countflops file (if things in rio don't go well, we want to report real results)
-    print `rm -f $new_path/countflops.log`;
-
-    # run dynamo rio (with the assumed countflops module installed)
-    print "(dynamo $iters)";
-    print `cd $new_path; $STREAMIT_DYNAMORIO $filename_base.exe -i $iters >& /dev/null`;
-
-    # get the report from the countflops.log file and clean up
-    my $report = read_file("$new_path/countflops.log");
-    return($report);
-
-}
-
-
-# Gets the number of outputs produced by executing
-# the stream for the specified number of iterations
-# usage: get_output_count($path, $filenamebase, $num_iters)
-sub get_output_count {
-    my $path = shift || die ("no path");
-    my $filename_base = shift || die ("no filename base");
-    my $num_iters = shift || die ("no iter count ");
-
-    print "(output count)";
-    
-    # run the program for one iter piping its output to wc
-    # whose output we will parse and return a value.
-    my $wc_results = `$path/$filename_base.exe -i $num_iters | wc -l`;
-    my ($count) = $wc_results =~ m/(\d*)\n/gi;
-    return $count;
-}
-
-# get the actual N of the output.
-sub get_N {
-    my $path = shift || die ("no path");
-    my $filename_base = shift || die ("no filename base");
-    # read in the c file and extract the information from the comments.
-    my $contents = read_file("$path/$filename_base.c");
-    my ($N) = $contents =~ m/N=(\d*)/gi;
-    return $N;
-}
-
-
-# reads in a file and returns its contents as a scalar variable
-# usage: read_file($filename)
-sub read_file {
-    my $filename = shift || die ("no filename passed to read_file\n");
-
-    open(INFILE, "<$filename") || die ("could not open $filename");
-    
-    # save the old standard delimiter
-    my $old_delim = local($/);
-    # set the standad delimiter to undef so we can read
-    # in the file in one go
-    local($/) = undef; 
-
-    # read in file contents
-    my $file_contents = <INFILE>;
-    close(INFILE);
-    
-    # restore the standard delimiter
-    local($/) = $old_delim;
-
-    return $file_contents;
-}
-
-# writes the contents of the first scalar argument to the 
-# filename in the second argument
-# usage: write_file($data, $filename)
-sub write_file {
-    my $data = shift || die("No data passed to write_file");
-    my $filename = shift || die("No filename passed to write_file");
-    
-    open(OUTFILE, ">$filename");
-    print OUTFILE $data;
-    close(OUTFILE);
-}

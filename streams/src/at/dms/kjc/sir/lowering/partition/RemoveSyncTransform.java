39203fcaeb57c16a27b213c4e341511f05c06581
Bill Thies <bthies@gmail.com>
2003-04-09 20:53:49 +0000

    Forgot new sync transform.

---
+++ streams/src/at/dms/kjc/sir/lowering/partition/RemoveSyncTransform.java
@@ -0,0 +1,32 @@
+package at.dms.kjc.sir.lowering.partition;
+
+import java.util.*;
+
+import at.dms.kjc.*;
+import at.dms.util.*;
+import at.dms.kjc.sir.*;
+import at.dms.kjc.sir.lowering.*;
+import at.dms.kjc.sir.lowering.fusion.*;
+
+/**
+ * Removes *matching* synchronization in this pipeline.
+ */
+public final class RemoveSyncTransform extends IdempotentTransform {
+    public RemoveSyncTransform() {
+	super();
+    }
+
+    /**
+     * Perform the transform on <str> and return new stream.
+     */
+    public SIRStream doMyTransform(SIRStream str) {
+	Utils.assert(str instanceof SIRPipeline);
+	boolean ok = RefactorSplitJoin.removeMatchingSyncPoints((SIRPipeline)str);
+	Utils.assert(ok, "Remove matching sync failed.");
+	return str;
+    }
+
+    public String toString() {
+	return "Remove matching sync transform.";
+    }
+}

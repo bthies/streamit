77f2041c98ebc3157d53ea39a369652b74ee49e3
David Maze <dmaze@cag.csail.mit.edu>
2003-04-14 20:28:35 +0000

    Keep track of more information, including the statement that defines a variable and what sort of variable it is.  Keep the old registerVar() function around so existing code still works.

--- streams/src/streamit/frontend/nodes/SymbolTable.java
+++ streams/src/streamit/frontend/nodes/SymbolTable.java
@@ -1,9 +1,3 @@
-/*
- * SymbolTable.java: symbol table for StreamIt programs
- * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: SymbolTable.java,v 1.4 2002-11-20 21:49:10 dmaze Exp $
- */
-
 package streamit.frontend.nodes;
 
 import java.util.Iterator;
@@ -14,17 +8,48 @@ import java.util.HashMap;
 
 /**
  * A symbol table for StreamIt programs.  This keeps a mapping from a
- * string name to a front-end type, and has a parent symbol table
- * (possibly null).  A name can be registered in the current symbol
- * table.  When resolving a name's type, the name is searched for
- * first in the current symbol table, and if not present than in the
- * parent symbol table.
+ * string name to a front-end type and an origin object, and has a
+ * parent symbol table (possibly null).  A name can be registered in
+ * the current symbol table.  When resolving a name's type, the name
+ * is searched for first in the current symbol table, and if not
+ * present than in the parent symbol table.
+ *
+ * <p>Each symbol may be associated with an <i>origin</i>.  This is
+ * the object that initially defines the symbol, typically a
+ * <code>StmtVarDecl</code> for local variables or filter fields or a
+ * <code>Parameter</code> for stream parameters.  Each symbol also
+ * has an integer kind to distinguish which of these it is.
+ *
+ * @see     streamit.frontend.passes.SymbolTableVisitor
+ * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
+ * @version $Id: SymbolTable.java,v 1.5 2003-04-14 20:28:35 dmaze Exp $
  */
 public class SymbolTable
 {
+    /** Kind of a local variable. */
+    public static final int KIND_LOCAL = 1;
+    /** Kind of a filter field. */
+    public static final int KIND_FIELD = 2;
+    /** Kind of a stream parameter. */
+    public static final int KIND_STREAM_PARAM = 3;
+    /** Kind of a function parameter. */
+    public static final int KIND_FUNC_PARAM = 4;
+    
     private Map vars, fns;
     private SymbolTable parent;
     private List includedFns;
+
+    private static class VarInfo
+    {
+        public VarInfo(Type type, Object origin, int kind)
+        {
+            this.type = type;
+            this.origin = origin;
+        }
+        public Type type;
+        public Object origin;
+        public int kind;
+    }
     
     /** Creates a new symbol table with the specified parent (possibly
      * null). */
@@ -36,10 +61,24 @@ public class SymbolTable
         this.includedFns = null;
     }
 
-    /** Registers a new symbol in the symbol table. */
+    /** Registers a new symbol in the symbol table, using default
+     * origin and kind. */
     public void registerVar(String name, Type type)
     {
-        vars.put(name, type);
+        registerVar(name, type, null, 0);
+    }
+
+    /**
+     * Registers a new symbol in the symbol table.
+     *
+     * @param  name    name of the variable
+     * @param  type    front-end type of the variable
+     * @param  origin  statement or other object defining the variable
+     * @param  kind    KIND_* constant describing the variable
+     */
+    public void registerVar(String name, Type type, Object origin, int kind)
+    {
+        vars.put(name, new VarInfo(type, origin, kind));
     }
 
     /** Registers a new function in the symbol table. */
@@ -50,16 +89,71 @@ public class SymbolTable
             fns.put(fn.getName(), fn);
     }
     
-    /** Looks up the type for a symbol.  If that symbol is not in the
-     * current symbol table, search in the parent.  If the parent is null,
-     * returns null. */
+    /** Helper method to get the VarInfo for a name.  If the symbol is
+     * not in the current symbol table, search in the parent.  If the
+     * parent is null, return null. */
+    private VarInfo lookupVarInfo(String name)
+    {
+        VarInfo info = (VarInfo)vars.get(name);
+        if (info != null)
+            return info;
+        if (parent != null)
+            return parent.lookupVarInfo(name);
+        return null;
+    }
+
+    /**
+     * Looks up the type for a symbol.  If that symbol is not in the
+     * current symbol table, search in the parent.
+     *
+     * @param   name  name of the variable to search for
+     * @return  the front-end type of the variable, if defined
+     * @throws  UnrecognizedVariableException if the variable
+     *          is defined in neither this nor any of its ancestor
+     *          symbol tables
+     */
     public Type lookupVar(String name)
     {
-        Type type = (Type)vars.get(name);
-        if (type != null)
-            return type;
-        if (parent != null)
-            return parent.lookupVar(name);
+        VarInfo info = lookupVarInfo(name);
+        if (info != null)
+            return info.type;
+        throw new UnrecognizedVariableException(name);
+    }
+
+    /**
+     * Finds the object that declared a particular symbol.  If that
+     * symbol is not in the current symbol table, search in the parent.
+     *
+     * @param   name  name of the variable to search for
+     * @return  the object that declares the variable, if defined
+     * @throws  UnrecognizedVariableException if the variable
+     *          is defined in neither this nor any of its ancestor
+     *          symbol tables
+     */
+    public Object lookupOrigin(String name)
+    {
+        VarInfo info = lookupVarInfo(name);
+        if (info != null)
+            return info.origin;
+        throw new UnrecognizedVariableException(name);
+    }
+
+    /**
+     * Gets the kind (local, field, etc.) of a particular symbol.  If
+     * that symbol is not in the current symbol table, search in the
+     * parent.
+     *
+     * @param   name  name of the variable to search for
+     * @return  KIND_* constant describing the variable
+     * @throws  UnrecognizedVariableException if the variable
+     *          is defined in neither this nor any of its ancestor
+     *          symbol tables
+     */
+    public int lookupKind(String name)
+    {
+        VarInfo info = lookupVarInfo(name);
+        if (info != null)
+            return info.kind;
         throw new UnrecognizedVariableException(name);
     }
 

419524e840170f5edebb448b103078a1a06aa782
David Maze <dmaze@cag.csail.mit.edu>
2003-05-20 17:19:46 +0000

    Use lisp to generate jde-sourcepath, rather than hoping JDE will expand $STREAMIT_HOME on its own.

--- streams/prj.el
+++ streams/prj.el
@@ -1,13 +1,14 @@
 ;;
 ;; prj.el: Emacs Java Development Environment project for StreamIt
 ;; David Maze <dmaze@cag.lcs.mit.edu>
-;; $Id: prj.el,v 1.2 2003-05-15 20:37:11 dmaze Exp $
+;; $Id: prj.el,v 1.3 2003-05-20 17:19:46 dmaze Exp $
 ;;
 
-(setq jde-build-function '(jde-ant-build)
+(setq jde-build-function '(jde-make)
       jde-ant-enable-find t
       jde-make-args "-C $STREAMIT_HOME/compiler JAVA_OPT=\"-nowarn +F +E\""
       ; jde-run-application-class "streamit.frontend.ToJava"
       jde-run-application-class "at.dms.kjc.Main"
-      jde-sourcepath '("$STREAMIT_HOME/compiler"
-		       "$STREAMIT_HOME/library/java"))
+      jde-sourcepath (mapcar (lambda (p)
+			       (concat (getenv "STREAMIT_HOME") "/" p))
+			     '("compiler" "library/java")))

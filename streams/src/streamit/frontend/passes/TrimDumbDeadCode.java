c3dfd7ea40e8e2547cea229c8a0353bb78937627
David Maze <dmaze@cag.csail.mit.edu>
2003-07-01 15:13:01 +0000

    Extend to also discard statements that consist only of an array or field expression.  For complex pop(), for example, we can get
      Complex __temp_var_1;
      __temp_var_1 = (Complex)input.pop();
      __temp_var_1.real;
      __temp_var_1.imag;
    
    Discard the last two statements, since they have no effect and aren't
    legal Java.  Note that we could conceivably be discarding things with
    side effects, such as
    
      a[i++];
    
    but we hope that won't be a problem.

--- streams/src/streamit/frontend/passes/TrimDumbDeadCode.java
+++ streams/src/streamit/frontend/passes/TrimDumbDeadCode.java
@@ -9,18 +9,23 @@ import streamit.frontend.nodes.*;
  * expression, that requires effort.)
  *
  * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
- * @version $Id: TrimDumbDeadCode.java,v 1.1 2003-05-20 18:37:39 dmaze Exp $
+ * @version $Id: TrimDumbDeadCode.java,v 1.2 2003-07-01 15:13:01 dmaze Exp $
  */
 public class TrimDumbDeadCode extends FEReplacer
 {
     public Object visitStmtExpr(StmtExpr stmt)
     {
         Expression expr = stmt.getExpression();
-        if (expr instanceof ExprConstBoolean ||
+        // NB: for array and field expressions, we really should look
+        // for side-effect-causing children.  At this point in the
+        // process it's probably okay, though.
+        if (expr instanceof ExprArray ||
+            expr instanceof ExprConstBoolean ||
             expr instanceof ExprConstChar ||
             expr instanceof ExprConstFloat ||
             expr instanceof ExprConstInt ||
             expr instanceof ExprConstStr ||
+            expr instanceof ExprField ||
             expr instanceof ExprVar)
             return null;
         return stmt;

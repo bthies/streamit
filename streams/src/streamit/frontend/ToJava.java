0e63a59508e14c105314ea3588847cec011968fa
David Maze <dmaze@cag.csail.mit.edu>
2003-07-02 19:24:13 +0000

    General clean-up.  Add some javadocs, break out the parse-files and run-ir-passes bits into separate functions.

--- streams/src/streamit/frontend/ToJava.java
+++ streams/src/streamit/frontend/ToJava.java
@@ -1,13 +1,7 @@
 package streamit.frontend;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.util.List;
 import java.util.Iterator;
-import java.util.ArrayList;
 import streamit.frontend.nodes.*;
 import streamit.frontend.passes.*;
 import streamit.frontend.tojava.*;
@@ -17,17 +11,19 @@ import at.dms.kjc.sir.lowering.*;
 import at.dms.kjc.iterator.*;
 import at.dms.util.*;
 
+/**
+ * Convert StreamIt programs to legal Java code.  This is the main
+ * entry point for the StreamIt syntax converter.  Running it as
+ * a standalone program reads the list of files provided on the
+ * command line and produces equivalent Java code on standard
+ * output or the file named in the <tt>--output</tt> command-line
+ * parameter.
+ *
+ * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
+ * @version $Id: ToJava.java,v 1.32 2003-07-02 19:24:13 dmaze Exp $
+ */
 public class ToJava
 {
-    public int indent = 0;
-    public String getIndent ()
-    {
-        int x = indent;
-        String r = "";
-        while (x-- > 0) r = r + "  ";
-        return r;
-    }
-
     public void printUsage()
     {
         System.err.println(
@@ -44,8 +40,8 @@ public class ToJava
     private boolean printHelp = false;
     private boolean libraryFormat = false;
     private String outputFile = null;
-  private boolean straightToSIR = false;
-    private List inputFiles = new ArrayList();
+    private boolean straightToSIR = false;
+    private List inputFiles = new java.util.ArrayList();
 
     public void doOptions(String[] args)
     {
@@ -73,6 +69,100 @@ public class ToJava
         }
     }
 
+    /**
+     * Read, parse, and combine all of the StreamIt code in a list of
+     * files.  Reads each of the files in <code>inputFiles</code> in
+     * turn and runs <code>streamit.frontend.StreamItParserFE</code>
+     * over it.  This produces a
+     * <code>streamit.frontend.nodes.Program</code> containing lists
+     * of structures and streams; combine these into a single
+     * <code>streamit.frontend.nodes.Program</code> with all of the
+     * structures and streams.
+     *
+     * @param inputFiles  list of strings naming the files to be read
+     * @returns a representation of the entire program, composed of the
+     *          code in all of the input files
+     * @throws java.io.IOException if an error occurs reading the input
+     *         files
+     * @throws antlr.RecognitionException if an error occurs parsing
+     *         the input files; that is, if the code is syntactically
+     *         incorrect
+     * @throws antlr.TokenStreamException if an error occurs producing
+     *         the input token stream
+     */
+    public static Program parseFiles(List inputFiles)
+        throws java.io.IOException,
+               antlr.RecognitionException, 
+               antlr.TokenStreamException
+    {
+        Program prog = null;
+        for (Iterator iter = inputFiles.iterator(); iter.hasNext(); )
+        {
+            String fileName = (String)iter.next();
+            InputStream inStream = new FileInputStream(fileName);
+            DataInputStream dis = new DataInputStream(inStream);
+            StreamItLex lexer = new StreamItLex(dis);
+            StreamItParserFE parser = new StreamItParserFE(lexer);
+            parser.setFilename(fileName);
+            Program pprog = parser.program();
+            if (prog == null)
+                prog = pprog;
+            else if (pprog != null)
+            {
+                List newStreams, newStructs;
+                newStreams = new java.util.ArrayList();
+                newStreams.addAll(prog.getStreams());
+                newStreams.addAll(pprog.getStreams());
+                newStructs = new java.util.ArrayList();
+                newStructs.addAll(prog.getStructs());
+                newStructs.addAll(pprog.getStructs());
+                prog = new Program(null, newStreams, newStructs);
+            }
+        }
+        return prog;
+    }
+
+    /**
+     * Transform front-end code to have the Java syntax.  Goes through
+     * a series of lowering passes to convert an IR tree from the
+     * "new" syntax to the "old" Java syntax understood by the main
+     * StreamIt compiler.  Conversion directed towards the StreamIt
+     * Java library, as opposed to the compiler, has slightly
+     * different output, mostly centered around phased filters.
+     *
+     * @param prog  the complete IR tree to lower
+     * @param libraryFormat  true if the program is being converted
+     *        to run under the StreamIt Java library
+     * @returns the converted IR tree
+     */
+    public static Program lowerIRToJava(Program prog, boolean libraryFormat)
+    {
+        /* What's the right order for these?  Clearly generic
+         * things like MakeBodiesBlocks need to happen first.
+         * I don't think there's actually a problem running
+         * MoveStreamParameters after DoComplexProp, since
+         * this introduces only straight assignments which the
+         * Java front-end can handle.  OTOH,
+         * MoveStreamParameters introduces references to
+         * "this", which doesn't exist. */
+        TempVarGen varGen = new TempVarGen();
+        prog = (Program)prog.accept(new MakeBodiesBlocks());
+        prog = (Program)prog.accept(new DisambiguateUnaries(varGen));
+        prog = (Program)prog.accept(new NoRefTypes());
+        prog = (Program)prog.accept(new RenameBitVars());
+        prog = (Program)prog.accept(new FindFreeVariables());
+        if (!libraryFormat)
+            prog = (Program)prog.accept(new NoticePhasedFilters());
+        prog = (Program)prog.accept(new DoComplexProp(varGen));
+        prog = (Program)prog.accept(new TranslateEnqueue());
+        prog = (Program)prog.accept(new InsertIODecls(libraryFormat));
+        prog = (Program)prog.accept(new InsertInitConstructors());
+        prog = (Program)prog.accept(new MoveStreamParameters());
+        prog = (Program)prog.accept(new NameAnonymousFunctions());
+        prog = (Program)prog.accept(new TrimDumbDeadCode());
+        return prog;
+    }
+
     public void run(String[] args)
     {
         doOptions(args);
@@ -82,35 +172,12 @@ public class ToJava
             return;
         }
         
-        StreamItParserFE parser = null;
         Program prog = null;
         Writer outWriter;
 
         try
         {
-            for (Iterator iter = inputFiles.iterator(); iter.hasNext(); )
-            {
-                String fileName = (String)iter.next();
-                InputStream inStream = new FileInputStream(fileName);
-                DataInputStream dis = new DataInputStream(inStream);
-                StreamItLex lexer = new StreamItLex(dis);
-                parser = new StreamItParserFE(lexer);
-                parser.setFilename(fileName);
-                Program pprog = parser.program();
-                if (prog == null)
-                    prog = pprog;
-                else if (pprog != null)
-                {
-                    List newStreams, newStructs;
-                    newStreams = new java.util.ArrayList();
-                    newStreams.addAll(prog.getStreams());
-                    newStreams.addAll(pprog.getStreams());
-                    newStructs = new java.util.ArrayList();
-                    newStructs.addAll(prog.getStructs());
-                    newStructs.addAll(pprog.getStructs());
-                    prog = new Program(null, newStreams, newStructs);
-                }
-            }
+            prog = parseFiles(inputFiles);
         }
         catch (java.io.IOException e) {e.printStackTrace(System.err);}
         catch (antlr.RecognitionException e) {e.printStackTrace(System.err);}
@@ -121,6 +188,8 @@ public class ToJava
             System.err.println("Compilation didn't generate a parse tree.");
             return;
         }
+
+        prog = lowerIRToJava(prog, libraryFormat);
 
         try
         {
@@ -137,29 +206,6 @@ public class ToJava
                             "  public float imag;\n" +
                             "}\n");
 
-            /* What's the right order for these?  Clearly generic
-             * things like MakeBodiesBlocks need to happen first.
-             * I don't think there's actually a problem running
-             * MoveStreamParameters after DoComplexProp, since
-             * this introduces only straight assignments which the
-             * Java front-end can handle.  OTOH,
-             * MoveStreamParameters introduces references to
-             * "this", which doesn't exist. */
-            TempVarGen varGen = new TempVarGen();
-            prog = (Program)prog.accept(new MakeBodiesBlocks());
-            prog = (Program)prog.accept(new DisambiguateUnaries(varGen));
-            prog = (Program)prog.accept(new NoRefTypes());
-            prog = (Program)prog.accept(new RenameBitVars());
-            prog = (Program)prog.accept(new FindFreeVariables());
-            if (!libraryFormat)
-                prog = (Program)prog.accept(new NoticePhasedFilters());
-            prog = (Program)prog.accept(new DoComplexProp(varGen));
-            prog = (Program)prog.accept(new TranslateEnqueue());
-            prog = (Program)prog.accept(new InsertIODecls(libraryFormat));
-            prog = (Program)prog.accept(new InsertInitConstructors());
-            prog = (Program)prog.accept(new MoveStreamParameters());
-            prog = (Program)prog.accept(new NameAnonymousFunctions());
-            prog = (Program)prog.accept(new TrimDumbDeadCode());
             if (straightToSIR) {
                 SIRStream s = (SIRStream) prog.accept(new FEIRToSIR());
                 SIRPrinter sirPrinter = new SIRPrinter();

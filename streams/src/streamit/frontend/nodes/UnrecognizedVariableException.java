5db4dcd65bc833414d04cf24c65124e5b4482583
David Maze <dmaze@cag.csail.mit.edu>
2003-07-07 21:16:45 +0000

    Add some more intelligent constructors.

--- streams/src/streamit/frontend/nodes/UnrecognizedVariableException.java
+++ streams/src/streamit/frontend/nodes/UnrecognizedVariableException.java
@@ -1,13 +1,23 @@
 /*
  * UnrecognizedVariableException.java: unchecked generic exception
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: UnrecognizedVariableException.java,v 1.1 2002-08-12 18:48:39 dmaze Exp $
+ * $Id: UnrecognizedVariableException.java,v 1.2 2003-07-07 21:16:45 dmaze Exp $
  */
 
 package streamit.frontend.nodes;
 
 public class UnrecognizedVariableException extends RuntimeException
 {
+    public UnrecognizedVariableException(FENode node, String var)
+    {
+        this("Unrecognized variable: " + var + " at " + node.getContext());
+    }
+    
+    public UnrecognizedVariableExcpetion(ExprVar var)
+    {
+        this(var, var.getName());
+    }
+    
     public UnrecognizedVariableException(String s)
     {
         super(s);

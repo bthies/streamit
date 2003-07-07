f9e2809d268d982414881dc021abc59fc4bfef2b
David Maze <dmaze@cag.csail.mit.edu>
2003-07-07 20:57:27 +0000

    Create Type object for message portals.

---
+++ streams/src/streamit/frontend/nodes/TypePortal.java
@@ -0,0 +1,42 @@
+package streamit.frontend.nodes;
+
+/**
+ * A message-portal type.  This is a type used to send messages; it
+ * appears in the portals list of
+ * <code>streamit.frontend.nodes.StreamCreator</code>, and as the
+ * receiver object of
+ * <code>streamit.frontend.nodes.StmtSendMessage</code>.  A portal
+ * corresponds to a single named stream type.  Because of the way
+ * <code>FEReplacer</code> works, attempting to keep a pointer to
+ * the actual referenced stream type here is useless; we only keep
+ * the name.
+ *
+ * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
+ * @version $Id: TypePortal.java,v 1.1 2003-07-07 20:57:27 dmaze Exp $
+ */
+public class TypePortal extends Type
+{
+    private String name;
+    
+    /**
+     * Creates a new portal type.
+     *
+     * @param name  name of the target filter type
+     */
+    public TypePortal(String name)
+    {
+        this.name = name;
+    }
+    
+    /**
+     * Gets the name of the target filter type.  Other code will be
+     * needed to resolve this to the actual <code>StreamSpec</code>
+     * object.
+     *
+     * @returns  name of the target filter type
+     */
+    public String getName()
+    {
+        return name;
+    }
+}

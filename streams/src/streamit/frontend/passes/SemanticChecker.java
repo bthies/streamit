9735d962a2e52c99bad7120bb765451b445e4bc2
David Maze <dmaze@cag.csail.mit.edu>
2003-07-09 14:28:53 +0000

    Add checks that add, split, join, loop, body, enqueue, push, pop, and peek statements only appear in the correct place in the code.  Fix duplicate-function-name code to not bail if two functions have 'null' names, but still trap multiple anonymous init and work functions.

--- streams/src/streamit/frontend/passes/SemanticChecker.java
+++ streams/src/streamit/frontend/passes/SemanticChecker.java
@@ -11,7 +11,7 @@ import java.util.*;
  * semantic errors.
  *
  * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
- * @version $Id: SemanticChecker.java,v 1.2 2003-07-08 21:40:26 dmaze Exp $
+ * @version $Id: SemanticChecker.java,v 1.3 2003-07-09 14:28:53 dmaze Exp $
  */
 public class SemanticChecker
 {
@@ -27,9 +27,12 @@ public class SemanticChecker
     {
         SemanticChecker checker = new SemanticChecker();
         Map streamNames = checker.checkStreamNames(prog);
+        checker.checkDupFieldNames(prog, streamNames);
+        checker.checkStatementPlacement(prog);
         return checker.good;
     }
     
+    // true if we haven't found any errors
     private boolean good;
 
     private void report(FENode node, String message)
@@ -120,8 +123,32 @@ public class SemanticChecker
             for (i2 = spec.getFuncs().iterator(); i2.hasNext(); )
             {
                 Function func = (Function)i2.next();
-                checkADupFieldName(localNames, streamNames,
-                                   func.getName(), func.getContext());
+                // Some functions get alternate names if their real
+                // name is null:
+                String name = func.getName();
+                if (name == null)
+                {
+                    switch(func.getCls())
+                    {
+                    case Function.FUNC_INIT: name = "init"; break;
+                    case Function.FUNC_WORK: name = "work"; break;
+                    case Function.FUNC_PREWORK: name = "prework"; break;
+                    case Function.FUNC_HANDLER:
+                        report(func, "message handlers must have names");
+                        break;
+                    case Function.FUNC_HELPER:
+                        report(func, "helper functions must have names");
+                        break;
+                    case Function.FUNC_PHASE:
+                        report(func, "phase functions must have names");
+                        break;
+                    default:
+                        // is BUILTIN_HELPER and CONST_HELPER.  Ignore
+                    }
+                }
+                if (name != null)
+                    checkADupFieldName(localNames, streamNames,
+                                       name, func.getContext());
             }
         }
         for (Iterator iter = prog.getStructs().iterator(); iter.hasNext(); )
@@ -153,5 +180,155 @@ public class SemanticChecker
                 report(octx, "a stream or structure");
             }
         }
+    }
+
+    /**
+     * Checks that statements exist in valid contexts for the
+     * type of statement.  This checks that add, split, join,
+     * loop, and body statements are only in appropriate
+     * initialization code, and that push, pop, peek, and keep
+     * statements are only in appropriate work function code.
+     *
+     * @param prog  parsed program object to check
+     */
+    public void checkStatementPlacement(Program prog)
+    {
+        // This is easiest to implement as a visitor, since otherwise
+        // we'll end up trying to recursively call ourselves if there's
+        // an anonymous stream declaration.  Ick.
+        prog.accept(new FEReplacer() {
+                // Track the current streamspec and function:
+                private StreamSpec spec = null;
+                private Function func = null;
+                
+                public Object visitStreamSpec(StreamSpec ss)
+                {
+                    StreamSpec oldSpec = spec;
+                    spec = ss;
+                    Object result = super.visitStreamSpec(ss);
+                    spec = oldSpec;
+                    return result;
+                }
+
+                public Object visitFunction(Function func2)
+                {
+                    Function oldFunc = func;
+                    func = func2;
+                    Object result = super.visitFunction(func2);
+                    func = oldFunc;
+                    return result;
+                }
+
+                public Object visitFuncWork(FuncWork func2)
+                {
+                    Function oldFunc = func;
+                    func = func2;
+                    Object result = super.visitFuncWork(func2);
+                    func = oldFunc;
+                    return result;
+                }
+
+                // So the remainder of this just needs to check
+                // spec.getType() and func.getCls() and that they're
+                // correct vs. the type of statement.
+                public Object visitStmtAdd(StmtAdd stmt)
+                {
+                    if ((func.getCls() != Function.FUNC_INIT) ||
+                        (spec.getType() != StreamSpec.STREAM_PIPELINE &&
+                         spec.getType() != StreamSpec.STREAM_SPLITJOIN))
+                        report(stmt,
+                               "add statement only allowed " +
+                               "in pipeline/splitjoin");
+                    return super.visitStmtAdd(stmt);
+                }
+
+                public Object visitStmtBody(StmtBody stmt)
+                {
+                    if (func.getCls() != Function.FUNC_INIT ||
+                        spec.getType() != StreamSpec.STREAM_FEEDBACKLOOP)
+                        report(stmt,
+                               "body statement only allowed " +
+                               "in feedbackloop");
+                    return super.visitStmtBody(stmt);
+                }
+
+                public Object visitStmtEnqueue(StmtEnqueue stmt)
+                {
+                    if (func.getCls() != Function.FUNC_INIT ||
+                        spec.getType() != StreamSpec.STREAM_FEEDBACKLOOP)
+                        report(stmt,
+                               "enqueue statement only allowed " +
+                               "in feedbackloop");
+                    return super.visitStmtEnqueue(stmt);
+                }
+
+                public Object visitStmtJoin(StmtJoin stmt)
+                {
+                    if ((func.getCls() != Function.FUNC_INIT) ||
+                        (spec.getType() != StreamSpec.STREAM_FEEDBACKLOOP &&
+                         spec.getType() != StreamSpec.STREAM_SPLITJOIN))
+                        report(stmt,
+                               "join statement only allowed " +
+                               "in splitjoin/feedbackloop");
+                    return super.visitStmtJoin(stmt);
+                }
+
+                public Object visitStmtLoop(StmtLoop stmt)
+                {
+                    if (func.getCls() != Function.FUNC_INIT ||
+                        spec.getType() != StreamSpec.STREAM_FEEDBACKLOOP)
+                        report(stmt,
+                               "loop statement only allowed " +
+                               "in feedbackloop");
+                    return super.visitStmtLoop(stmt);
+                }
+
+                public Object visitExprPeek(ExprPeek expr)
+                {
+                    if ((func.getCls() != Function.FUNC_PHASE &&
+                         func.getCls() != Function.FUNC_PREWORK &&
+                         func.getCls() != Function.FUNC_WORK) ||
+                        (spec.getType() != StreamSpec.STREAM_FILTER))
+                        report(expr,
+                               "peek expression only allowed " +
+                               "in filter work functions");
+                    return super.visitExprPeek(expr);
+                }
+
+                public Object visitExprPop(ExprPop expr)
+                {
+                    if ((func.getCls() != Function.FUNC_PHASE &&
+                         func.getCls() != Function.FUNC_PREWORK &&
+                         func.getCls() != Function.FUNC_WORK) ||
+                        (spec.getType() != StreamSpec.STREAM_FILTER))
+                        report(expr,
+                               "pop expression only allowed " +
+                               "in filter work functions");
+                    return super.visitExprPop(expr);
+                }
+
+                public Object visitStmtPush(StmtPush stmt)
+                {
+                    if ((func.getCls() != Function.FUNC_PHASE &&
+                         func.getCls() != Function.FUNC_PREWORK &&
+                         func.getCls() != Function.FUNC_WORK) ||
+                        (spec.getType() != StreamSpec.STREAM_FILTER))
+                        report(stmt,
+                               "push statement only allowed " +
+                               "in filter work functions");
+                    return super.visitStmtPush(stmt);
+                }
+
+                public Object visitStmtSplit(StmtSplit stmt)
+                {
+                    if ((func.getCls() != Function.FUNC_INIT) ||
+                        (spec.getType() != StreamSpec.STREAM_FEEDBACKLOOP &&
+                         spec.getType() != StreamSpec.STREAM_SPLITJOIN))
+                        report(stmt,
+                               "split statement only allowed " +
+                               "in splitjoin/feedbackloop");
+                    return super.visitStmtSplit(stmt);
+                }
+            });
     }
 }

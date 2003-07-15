c9b643b1bad4662911ae7487f46e01219a3ed2ef
David Maze <dmaze@cag.csail.mit.edu>
2003-07-15 18:55:54 +0000

    Add toString(), equals(), hashCode().

--- streams/src/streamit/frontend/nodes/TypeArray.java
+++ streams/src/streamit/frontend/nodes/TypeArray.java
@@ -1,15 +1,12 @@
-/*
- * TypeArray.java: a fixed-length homogenous array type
- * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: TypeArray.java,v 1.1 2002-07-15 15:44:08 dmaze Exp $
- */
-
 package streamit.frontend.nodes;
 
 /**
  * A fixed-length homogenous array type.  This type has a base type and
  * an expression for the length.  The expression must be real and integral,
  * but may contain variables if they can be resolved by constant propagation.
+ *
+ * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
+ * @version $Id: TypeArray.java,v 1.2 2003-07-15 18:55:54 dmaze Exp $
  */
 public class TypeArray extends Type
 {
@@ -34,5 +31,27 @@ public class TypeArray extends Type
     public Expression getLength()
     {
         return length;
+    }
+
+    public String toString()
+    {
+        return base + "[" + length + "]";
+    }
+    
+    public boolean equals(Object other)
+    {
+        if (!(other instanceof TypeArray))
+            return false;
+        TypeArray that = (TypeArray)other;
+        if (!(this.getBase().equals(that.getBase())))
+            return false;
+        if (!(this.getLength().equals(that.getLength())))
+            return false;
+        return true;
+    }
+    
+    public int hashCode()
+    {
+        return base.hashCode() ^ length.hashCode();
     }
 }

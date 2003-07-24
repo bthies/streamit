fbf30433ab80f270aa7730a8f549480113ab68c5
David Maze <dmaze@cag.csail.mit.edu>
2003-07-24 19:12:44 +0000

    Add portal methods to set the next message's latency.  These need to be generated by the front-end and interpreted by Kopi2SIR.

--- streams/src/streamit/library/Portal.java
+++ streams/src/streamit/library/Portal.java
@@ -8,10 +8,28 @@ package streamit;
  * whose name ends in "Portal" should be defined; that class is the portal
  * object.  Receiver objects should also implement the interface.
  *
- * @version $Id: Portal.java,v 1.1 2003-07-23 19:31:48 dmaze Exp $
+ * @version $Id: Portal.java,v 1.2 2003-07-24 19:12:44 dmaze Exp $
  */
 public class Portal
 {
+    public void setAnyLatency()
+    {
+        throw new UnsupportedOperationException
+            ("StreamIt Java library does not support messaging");
+    }
+    
+    public void setMaxLatency(int b)
+    {
+        throw new UnsupportedOperationException
+            ("StreamIt Java library does not support messaging");
+    }
+
+    public void setLatency(int a, int b)
+    {
+        throw new UnsupportedOperationException
+            ("StreamIt Java library does not support messaging");
+    }
+
     public void regReceiver(Object o)
     {
         throw new UnsupportedOperationException

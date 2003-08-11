868ec597aa695ff7a2d593077f6249d062d9d4ca
Michael Gordon <michael.ian.gordon@gmail.com>
2003-08-11 23:12:09 +0000

    Added some accessor methods to Trace.java.

--- streams/src/at/dms/kjc/spacetime/Trace.java
+++ streams/src/at/dms/kjc/spacetime/Trace.java
@@ -1,6 +1,31 @@
 package at.dms.kjc.spacetime;
 
+/** 
+ *
+ **/
 public class Trace 
 {
+    private Trace[] edges;
+    private TraceNode head;
+
+    public Trace (Trace[] edges, TraceNode head) 
+    {
+	this.edges = edges;
+	this.head = head;
+    }
+
+    public void setHead(TraceNode node) 
+    {
+	head = node;
+    }
     
+    public TraceNode getHead() 
+    {
+	return head;
+    }
+    
+    public Trace[] getEdges() 
+    {
+	return edges;
+    }
 }

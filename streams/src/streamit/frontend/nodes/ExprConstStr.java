8520e2308b451bdb9cce70f89c6f78f6fcc0875f
David Maze <dmaze@cag.csail.mit.edu>
2003-08-12 13:47:15 +0000

    Update Javadocs.  This is actually relevant: the string includes "the leading and trailing quotes" along with any "embedded\nescape sequences".

--- streams/src/streamit/frontend/nodes/ExprConstStr.java
+++ streams/src/streamit/frontend/nodes/ExprConstStr.java
@@ -1,26 +1,37 @@
-/*
- * ExprConstStr.java: a string literal
- * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: ExprConstStr.java,v 1.2 2002-08-20 20:04:28 dmaze Exp $
- */
-
 package streamit.frontend.nodes;
 
 /**
- * A string literal.  Are these even legal in StreamIt?
+ * A string literal.  The only place these currently appear in StreamIt
+ * is as the filename argument to file reader and writer filters.
+ * For convenience, these are stored in their original program-source
+ * representation.
+ *
+ * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
+ * @version $Id: ExprConstStr.java,v 1.3 2003-08-12 13:47:15 dmaze Exp $
  */
 public class ExprConstStr extends Expression
 {
     private String val;
     
-    /** Create a new ExprConstStr. */
+    /**
+     * Create a new ExprConstStr.
+     *
+     * @param context  file and line number in the source file
+     * @param val      source-format representation of the string
+     */
     public ExprConstStr(FEContext context, String val)
     {
         super(context);
         this.val = val;
     }
     
-    /** Returns the value of this. */
+    /**
+     * Returns the value of this.  The returned string will be in
+     * the format it appeared in in the original source file,
+     * including leading and trailing double quotes.
+     *
+     * @return  source-format representation of the string
+     */
     public String getVal() { return val; }
 
     /** Accept a front-end visitor. */

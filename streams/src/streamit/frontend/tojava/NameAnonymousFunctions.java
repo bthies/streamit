cf8c936fe3b8449d03269a6d324ae39e164f6cc7
Bill Thies <bthies@gmail.com>
2003-08-29 22:51:24 +0000

    Output "prework" for name of prework function instead of "work".

--- streams/src/streamit/frontend/tojava/NameAnonymousFunctions.java
+++ streams/src/streamit/frontend/tojava/NameAnonymousFunctions.java
@@ -1,7 +1,7 @@
 /*
  * NameAnonymousFunctions.java: replace anonymous functions with named ones
  * David Maze <dmaze@cag.lcs.mit.edu>
- * $Id: NameAnonymousFunctions.java,v 1.3 2003-01-09 19:48:37 dmaze Exp $
+ * $Id: NameAnonymousFunctions.java,v 1.4 2003-08-29 22:51:24 thies Exp $
  */
 
 package streamit.frontend.tojava;
@@ -32,7 +32,14 @@ public class NameAnonymousFunctions extends FEReplacer
     {
         func = (FuncWork)super.visitFuncWork(func);
         if (func.getName() != null) return func;
-        return new FuncWork(func.getContext(), func.getCls(), "work",
+	String name = null;
+        switch(func.getCls())
+        {
+        case Function.FUNC_WORK: name = "work"; break;
+        case Function.FUNC_PREWORK: name = "prework"; break;
+        default: return func;
+        }
+        return new FuncWork(func.getContext(), func.getCls(), name,
                             func.getBody(), func.getPeekRate(),
                             func.getPopRate(), func.getPushRate());
     }

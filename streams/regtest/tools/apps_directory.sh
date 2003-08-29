8326cedda0361dddab19b575d2fafd9f292bbced
Bill Thies <bthies@gmail.com>
2003-08-29 22:28:20 +0000

    Changing back to 1.2 format (sorry).

--- streams/regtest/tools/apps_directory.sh
+++ streams/regtest/tools/apps_directory.sh
@@ -1,7 +1,40 @@
 #!/bin/sh
 
 echo
-for name in `ls` 
+echo "The following directories are NOT being COMPILED in the regtest:"
+for dirname in `ls ${STREAMIT_HOME}apps` 
 do
-  dummy=`echo 23 >! temp && echo 35 >> temp && pro $name -f $name_s`
+  for name in `tree -dif ${STREAMIT_HOME}apps/$dirname | grep "apps/$dirname/" | grep -v CVS | grep -v directories | grep -v VocoderTests | grep -v '/c/' | grep -v '/c$'`
+    do
+    info=`grep $name $1 | grep Compile`
+    if test "$info" == ""; then
+	echo " " $name
+    fi
+  done
+done
+
+echo
+echo "The following directories are NOT being EXECUTED in the regtest:"
+for dirname in `ls ${STREAMIT_HOME}apps` 
+do
+  for name in `tree -dif ${STREAMIT_HOME}apps/$dirname | grep "apps/$dirname/" | grep -v CVS | grep -v directories | grep -v VocoderTests | grep -v '/c/' | grep -v '/c$'`
+    do
+    info=`grep $name $1 | grep Exec`
+    if test "$info" == ""; then
+	echo " " $name
+    fi
+  done
+done
+
+echo
+echo "The following directories are NOT being VERIFIED in the regtest:"
+for dirname in `ls ${STREAMIT_HOME}apps` 
+do
+  for name in `tree -dif ${STREAMIT_HOME}apps/$dirname | grep "apps/$dirname/" | grep -v CVS | grep -v directories | grep -v VocoderTests | grep -v '/c/' | grep -v '/c$'`
+    do
+    info=`grep $name $1 | grep Verify`
+    if test "$info" == ""; then
+	echo " " $name
+    fi
+  done
 done

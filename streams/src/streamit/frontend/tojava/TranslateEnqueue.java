47deeade60090205dcae95b1877aaf7eb2d537e2
David Maze <dmaze@cag.csail.mit.edu>
2003-09-02 17:53:38 +0000

    Revert to 1.4 (and throwing out all of Bill's code to possibly generate multiple initPath() functions).  Since we should in theory have the loop type (from AssignLoopTypes), use that instead of the input type of the stream (which could be void).

--- streams/src/streamit/frontend/tojava/TranslateEnqueue.java
+++ streams/src/streamit/frontend/tojava/TranslateEnqueue.java
@@ -5,7 +5,6 @@ import streamit.frontend.nodes.*;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.LinkedList;
 
 import java.util.ArrayList;
 
@@ -24,7 +23,7 @@ import java.util.ArrayList;
  * loops, though.
  * 
  * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
- * @version $Id: TranslateEnqueue.java,v 1.7 2003-08-29 22:26:35 thies Exp $
+ * @version $Id: TranslateEnqueue.java,v 1.8 2003-09-02 17:53:38 dmaze Exp $
  */
 public class TranslateEnqueue extends FEReplacer
 {
@@ -39,21 +38,14 @@ public class TranslateEnqueue extends FEReplacer
      * we need to remember the prevailing StreamSpec, since we might
      * alter it, but we always want a recursive visit. */
 
-    /** Creates one or more helper <code>Function</code>'s that return
-     * a value out of <code>vals</code>.  Multiple
-     * <code>Function</code>'s will be generated if there is any
-     * uncertainty about the type signature of <code>ss</code>. */
-    private List makeInitPath(StreamSpec ss)
+    /** Creates a helper <code>Function</code> that returns a value out of
+     * <code>vals</code>. */
+    private Function makeInitPath(StreamSpec ss)
     {
         FEContext context = ss.getContext();
         List stmts = new ArrayList();
         Expression n = new ExprVar(context, "n");
         int i = 0;
-	// keep track of the most general type of value we have.  Make
-	// this int by default (since we can handle int); float if we
-	// ever encounter a float; and void otherwise (indicating
-	// something we can't handle yet.)
-	TypePrimitive inferredType = new TypePrimitive(TypePrimitive.TYPE_INT);
         for (Iterator iter = vals.iterator(); iter.hasNext(); )
         {
             Expression val = (Expression)iter.next();
@@ -62,67 +54,43 @@ public class TranslateEnqueue extends FEReplacer
             stmts.add(new StmtIfThen(context, cond,
                                      new StmtReturn(context, val), null));
             i++;
-	    // try inferring type
-	    if (val instanceof ExprConstFloat && inferredType.getType()!=TypePrimitive.TYPE_VOID) {
-		inferredType = new TypePrimitive(TypePrimitive.TYPE_FLOAT);
-	    } else if (!(val instanceof ExprConstInt)) {
-		inferredType = new TypePrimitive(TypePrimitive.TYPE_VOID);
-	    }
-	}
+        }
         StmtBlock body = new StmtBlock(context, stmts);
-        // Figure out the return type and name; guess from the input type of
-        // the stream specification.  (This implies that typeless anonymous
-        // feedback loops won't work, at least for now.)
-        Type returnType = ss.getStreamType().getIn();
+        // Figure out the return type and name; use the loop type, which
+        // should be assigned by the AssignLoopTypes pass.
+        Type returnType = ss.getStreamType().getLoop();
         // Claim: it's a primitive type, we can't deal otherwise.
         TypePrimitive tp = (TypePrimitive)returnType;
-	List result = new LinkedList();
-
-        if (tp.getType() == TypePrimitive.TYPE_FLOAT ||
-	    // generate a float initPath if we inferred either int or
-	    // float types, as either are legal code.
-	    (tp.getType() == TypePrimitive.TYPE_VOID && 
-	     (inferredType.getType() == TypePrimitive.TYPE_INT || inferredType.getType() == TypePrimitive.TYPE_FLOAT))) {
-	    ArrayList stmts2 = new ArrayList(stmts);
-            stmts2.add(new StmtReturn(context, new ExprConstFloat(context, 0.0)));
-	    Parameter param = new Parameter(new TypePrimitive(TypePrimitive.TYPE_INT), "n");
-	    result.add(Function.newHelper(context, "initPathFloat", 
-					  new TypePrimitive(TypePrimitive.TYPE_FLOAT),
-					  Collections.singletonList(param),
-					  new StmtBlock(context, stmts2)));
-        }
-        
-	if (tp.getType() == TypePrimitive.TYPE_INT ||
-	    (tp.getType() == TypePrimitive.TYPE_VOID && inferredType.getType() == TypePrimitive.TYPE_INT))
+        String name = null;
+        if (tp.getType() == TypePrimitive.TYPE_FLOAT)
         {
-	    ArrayList stmts2 = new ArrayList(stmts);
-            stmts2.add(new StmtReturn(context, new ExprConstInt(context, 0)));
-	    Parameter param = new Parameter(new TypePrimitive(TypePrimitive.TYPE_INT), "n");
-	    result.add(Function.newHelper(context, "initPathInt", 
-					  new TypePrimitive(TypePrimitive.TYPE_INT),
-					  Collections.singletonList(param),
-					  new StmtBlock(context, stmts2)));
+            name = "initPathFloat";
+            stmts.add(new StmtReturn(context,
+                                     new ExprConstFloat(context, 0.0)));
         }
-        
-	if (tp.getType() == TypePrimitive.TYPE_COMPLEX)
+        else if (tp.getType() == TypePrimitive.TYPE_INT)
         {
-	    ArrayList stmts2 = new ArrayList(stmts);
-            stmts2.add(new StmtReturn(context, new ExprConstFloat(context, 0.0)));
-	    Parameter param = new Parameter(new TypePrimitive(TypePrimitive.TYPE_INT), "n");
-	    result.add(Function.newHelper(context, "initPath", 
-					  new TypePrimitive(TypePrimitive.TYPE_COMPLEX),
-					  Collections.singletonList(param),
-					  new StmtBlock(context, stmts2)));
+            returnType = new TypePrimitive(TypePrimitive.TYPE_INT);
+            name = "initPathInt";
+            stmts.add(new StmtReturn(context,
+                                     new ExprConstInt(context, 0)));
         }
-
-	if (result.size()==0)
+        else if (tp.getType() == TypePrimitive.TYPE_COMPLEX)
+        {
+            name = "initPath";
+            stmts.add(new StmtReturn(context,
+                                     new ExprConstFloat(context, 0.0)));
+        }
+        else
         {
             // char, string don't have Types.  Yay corner cases.
             throw new IllegalStateException("can't translate enqueue: " +
                                             "stream's input type is " + tp);
         }
-
-        return result;
+        Parameter param =
+            new Parameter(new TypePrimitive(TypePrimitive.TYPE_INT), "n");
+        return Function.newHelper(context, name, returnType,
+                                  Collections.singletonList(param), body);
     }
 
     public Object visitStreamSpec(StreamSpec ss)
@@ -134,7 +102,7 @@ public class TranslateEnqueue extends FEReplacer
         if (!vals.isEmpty())
         {
             List fns = new ArrayList(ssNew.getFuncs());
-            fns.addAll(makeInitPath(ss));
+            fns.add(makeInitPath(ss));
             ssNew = new StreamSpec(ssNew.getContext(), ssNew.getType(),
                                    ssNew.getStreamType(), ssNew.getName(),
                                    ssNew.getParams(), ssNew.getVars(), fns);

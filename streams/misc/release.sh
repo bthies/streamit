f89d2a2149b98d82f7fd57e6e1d8108898c957e8
David Maze <dmaze@cag.csail.mit.edu>
2003-09-29 15:25:34 +0000

    Try to get all of the autoconf stuff right.

--- streams/misc/release.sh
+++ streams/misc/release.sh
@@ -2,7 +2,7 @@
 #
 # release.sh: assemble a StreamIt release
 # David Maze <dmaze@cag.lcs.mit.edu>
-# $Id: release.sh,v 1.16 2003-09-29 01:50:09 dmaze Exp $
+# $Id: release.sh,v 1.17 2003-09-29 15:25:34 dmaze Exp $
 #
 
 # Interesting/configurable variables:
@@ -59,12 +59,24 @@ builddirs() {
 # Get a checked-out copy of the source tree.
 mkdir $WORKING/streams
 DIRS="streams/knit streams/README.source"
-builddirs streams compiler library include misc
+builddirs streams 3rdparty src library include misc configure.in Makefile.in
 builddirs streams/apps benchmarks examples libraries sorts
 builddirs streams/docs cookbook implementation-notes manual runtime-interface
 builddirs streams/docs release syntax
 
 cvs export -r $TAG -d $WORKING $DIRS
+
+# Run autoconf to get a configure script.
+autoconf $WORKING/streams/configure.in > $WORKING/streams/configure
+chmod 0755 $WORKING/streams/configure
+
+# Generate .in files.  (See also the tail of configure.in.)
+INFILES="knit Makefile library/c/Makefile misc/Makefile.vars"
+for f in $INFILES; do
+  if test -f "$WORKING/streams/$f"; then
+    $WORKING/streams/misc/make-dot-in.pl "$WORKING/streams/$f"
+  fi
+done
 
 # Some benchmarks we can't (or won't) export; trim those here.
 rm -rf $WORKING/streams/apps/benchmarks/beamformer/c
@@ -74,10 +86,10 @@ rm -rf $WORKING/streams/apps/benchmarks/nokia
 rm -rf $WORKING/streams/apps/benchmarks/perftest4
 
 # Some parts of the compiler aren't useful to release; trim those here.
-rm -rf $WORKING/streams/compiler/at/dms/kjc/cluster
-rm -rf $WORKING/streams/compiler/at/dms/kjc/flatgraph2
-rm -rf $WORKING/streams/compiler/at/dms/kjc/raw2
-rm -rf $WORKING/streams/compiler/at/dms/kjc/spacetime
+rm -rf $WORKING/streams/src/at/dms/kjc/cluster
+rm -rf $WORKING/streams/src/at/dms/kjc/flatgraph2
+rm -rf $WORKING/streams/src/at/dms/kjc/raw2
+rm -rf $WORKING/streams/src/at/dms/kjc/spacetime
 
 # Build interesting bits of the documentation; they go in both releases.
 for d in cookbook manual release syntax; do
@@ -92,30 +104,36 @@ done
 # Make stable copies for all of the trees.  Clean the binary tree a little
 # in the process.
 cp -R $WORKING/streams $BINDIR
-rm -rf $BINDIR/compiler $BINDIR/eclipse $BINDIR/README.source
+rm -rf $BINDIR/src $BINDIR/README.source
 rm -rf $BINDIR/include/dot-bashrc
 rm -rf $BINDIR/include/dot-cshrc $BINDIR/misc/release.sh
 rm -rf $BINDIR/misc/get-antlr
 find $BINDIR/docs \( -name '*.hva' -o -name '*.tex' -o -name Makefile \
   -o -name '*.mp' \) -print0 | xargs -0 rm
 rm -f $BINDIR/misc/htmlformat.pl
+for f in $INFILES; do
+  rm -f "$BINDIR/$f"
+done
 
 # Build the source tarball:
 cp -R $WORKING/streams $WORKING/streamit-src-$VERSION
+for f in $INFILES; do
+  rm -f "$WORKING/streamit-src-$VERSION/$f"
+done
 tar cf $SRCTAR -C $WORKING streamit-src-$VERSION
 
 # Use the build magic to get an ANTLR jar file.
-$SRCDIR/misc/get-antlr $SRCDIR/compiler/antlr.jar
-CLASSPATH=$SRCDIR/compiler/antlr.jar
+$SRCDIR/misc/get-antlr $SRCDIR/3rdparty/antlr.jar
+CLASSPATH=$SRCDIR/3rdparty/antlr.jar
 
 # Now do a reference build.
 STREAMIT_HOME=$SRCDIR
 export CLASSPATH STREAMIT_HOME
 . $STREAMIT_HOME/include/dot-bashrc
-make -C $SRCDIR/compiler jar
+make -C $SRCDIR/src jar CAG_BUILD=0
 
 # Build binary jar file:
-cp $SRCDIR/compiler/streamit.jar $BINDIR
+cp $SRCDIR/src/streamit.jar $BINDIR
 tar cf $BINTAR -C $WORKING streamit-$VERSION
 
 # gzip the tarball and move it here.

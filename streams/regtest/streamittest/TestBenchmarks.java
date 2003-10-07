4fe43486bdef7cf3b73ae7069582bd7b3362497c
David Maze <dmaze@cag.csail.mit.edu>
2003-10-07 17:36:21 +0000

    doCompileRunVerifyTest expects the Java filename for perftest.java, not the StreamIt filename.

--- streams/regtest/streamittest/TestBenchmarks.java
+++ streams/regtest/streamittest/TestBenchmarks.java
@@ -2,7 +2,7 @@
  * For running the 
  *
  * You can then use the CompilerInterface compiler to run compiler sessions.
- * $Id: TestBenchmarks.java,v 1.38 2003-10-06 21:02:04 dmaze Exp $
+ * $Id: TestBenchmarks.java,v 1.39 2003-10-07 17:36:21 dmaze Exp $
  **/
 package streamittest;
 
@@ -189,7 +189,7 @@ public class TestBenchmarks extends StreamITTestCase {
 	doCompileRunVerifyTest(root, "Linkedperftest4.java", "Linkedperftest4.out", 0, 4);
 
         doSyntaxConvertTest(root, "perftest.str", "perftest.java");
-        doCompileRunVerifyTest(root, "perftest.str", "Linkedperftest4.out", 0, 4);
+        doCompileRunVerifyTest(root, "perftest.java", "Linkedperftest4.out", 0, 4);
     }
 
     public void testMP3Simple()

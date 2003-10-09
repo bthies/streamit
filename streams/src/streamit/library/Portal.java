f7de495354e3feaaa8dc6ecca2414d073217095f
David Maze <dmaze@cag.csail.mit.edu>
2003-10-09 20:51:31 +0000

    Add the MIT license text.

--- streams/src/streamit/library/Portal.java
+++ streams/src/streamit/library/Portal.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2003 by the Massachusetts Institute of Technology.
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+
 package streamit.library;
 
 /**
@@ -8,7 +24,7 @@ package streamit.library;
  * whose name ends in "Portal" should be defined; that class is the portal
  * object.  Receiver objects should also implement the interface.
  *
- * @version $Id: Portal.java,v 1.3 2003-09-29 09:19:00 thies Exp $
+ * @version $Id: Portal.java,v 1.4 2003-10-09 20:51:27 dmaze Exp $
  */
 public class Portal
 {

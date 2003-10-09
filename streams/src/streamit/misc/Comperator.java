372682b4720254cd91453d8f4e95684ab049988f
David Maze <dmaze@cag.csail.mit.edu>
2003-10-09 21:03:16 +0000

    Add the MIT license text.

--- streams/src/streamit/misc/Comperator.java
+++ streams/src/streamit/misc/Comperator.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2003 by the Massachusetts Institute of Technology.
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+
 package streamit.misc;
 
-/* $Id: Comperator.java,v 1.1 2003-03-13 23:18:05 karczma Exp $ */
+/* $Id: Comperator.java,v 1.2 2003-10-09 21:03:16 dmaze Exp $ */
 
 public interface Comperator
 {
--- streams/src/streamit/misc/UniquePairContainer.java
+++ streams/src/streamit/misc/UniquePairContainer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2003 by the Massachusetts Institute of Technology.
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+
 package streamit.misc;
 
 import streamit.misc.AssertedClass;
@@ -5,7 +21,7 @@ import java.util.Map;
 import java.util.HashMap;
 import streamit.misc.Pair;
 
-/* $Id: UniquePairContainer.java,v 1.1 2002-05-24 23:10:36 karczma Exp $ */
+/* $Id: UniquePairContainer.java,v 1.2 2003-10-09 21:03:16 dmaze Exp $ */
 
 /**
  * <dl>

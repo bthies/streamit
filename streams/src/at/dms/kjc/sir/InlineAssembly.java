b255966ee882739dfe7dd3ec3a1282d9ec4d1c0e
Jasper Lin <jasperln@cag.csail.mit.edu>
2003-11-07 10:42:24 +0000

    Supports adding to input portion of extended inline asm

--- streams/src/at/dms/kjc/sir/InlineAssembly.java
+++ streams/src/at/dms/kjc/sir/InlineAssembly.java
@@ -12,20 +12,40 @@ import java.util.ArrayList;
 public class InlineAssembly extends JStatement {
     //private String asm;
     private ArrayList inst;
+    private ArrayList input;
 
     public InlineAssembly() {
 	super(null,null);
 	//this.asm=asm;
 	inst=new ArrayList();
+	input=new ArrayList();
     }
-
+    
+    public InlineAssembly(String asm) {
+	super(null,null);
+	//this.asm=asm;
+	inst=new ArrayList();
+	inst.add(asm);
+	input=new ArrayList();
+    }
+    
     public void add(String instr) {
 	inst.add(instr);
+    }
+
+    public void addInput(String input) {
+	this.input.add(input);
     }
 
     public String[] getInstructions() {
 	String[] out=new String[inst.size()];
 	inst.toArray(out);
+	return out;
+    }
+
+    public String[] getInput() {
+	String[] out=new String[input.size()];
+	input.toArray(out);
 	return out;
     }
 
@@ -44,7 +64,7 @@ public class InlineAssembly extends JStatement {
 
     public void accept(KjcVisitor p) {
 	if(p instanceof SLIREmptyVisitor)
-	    ((SLIREmptyVisitor)p).visitInlineAssembly(this,getInstructions());
+	    ((SLIREmptyVisitor)p).visitInlineAssembly(this,getInstructions(),getInput());
     }
 }
 

2417f2719c05fcaff17920231461a3d95673e480
David Maze <dmaze@cag.csail.mit.edu>
2003-11-18 21:13:11 +0000

    Infrastructure to run StreamIt benchmarks under QMTest.  This includes a script to build the QMTest directory tree, a control file (regtest.xml), some documentation, and a few files that need to be copied into a top-level directory.

---
+++ streams/regtest/qmtest/build-qmtest.py
@@ -0,0 +1,411 @@
+#!/usr/uns/bin/python
+#
+# build-qmtest.py: build QMTest XML files from the StreamIt tree
+# David Maze <dmaze@cag.lcs.mit.edu>
+# $Id: build-qmtest.py,v 1.1 2003-11-18 21:13:10 dmaze Exp $
+#
+
+import os
+import os.path
+import re
+import shutil
+import sys
+from   types import *
+import xml.dom.minidom
+
+# TODO: some option parsing.
+# For now, assume sys.argv[0] has the path to the regtest control file,
+# and that we're in STREAMIT_HOME.
+
+def main():
+    # Read the regtest control file.
+    control = ReadControlFile(sys.argv[1])
+    BuildQMTestTree(control)
+
+def ReadControlFile(fn):
+    """Read a regtest XML control file.
+
+    Reads an XML file, and looks for <test> and <option> tags in it.
+    Each of the <test>s is read for its root attribute, naming the
+    subdirectories of the top-level directory to test.  The <option>s
+    are read for their target attribute, naming which compiler backend
+    to use, and the content of these tags are taken as additional
+    options to strc.
+
+    'fn' -- Name of the regtest control file.
+
+    returns -- A dictionary, where the 'testroots' element contains
+    a list of the root strings and the 'targets' element contains
+    a list of pairs of (target, options)."""
+    
+    dom = xml.dom.minidom.parse(fn)
+    # From this, get the list of test roots:
+    testroots = []
+    tests = dom.getElementsByTagName('test')
+    for node in tests:
+        testroots.append(node.getAttribute('root'))
+    # And the set of targets, as pairs of (target, options).
+    targets = []
+    options = dom.getElementsByTagName('option')
+    for node in options:
+        target = node.getAttribute('target')
+        onode = node.firstChild
+        # ASSERT: onode is a text node, or None
+        opts = ''
+        if onode:
+            opts = onode.data
+        targets.append((target, opts))
+    dom.unlink()
+    return { 'testroots': testroots, 'targets': targets }
+
+def BuildQMTestTree(control):
+    """Build a QMTest XML tree.
+
+    The tree is built under the 'QMTest' directory of the current
+    working directory.
+
+    'control' -- Results of parsing the regtest control file, as
+    returned from 'ReadControlFile()'."""
+
+    def visit(arg, dirname, names):
+        if 'benchmark.xml' in names:
+            DoQMTestDir(dirname, control)
+
+    for r in control['testroots']:
+        os.path.walk(r, visit, None)
+
+def DoQMTestDir(path, control):
+    """Create a QMTest directory for a given path.
+
+    The provided directory must have a 'benchmark.xml' file.
+    This creates the corresponding QMTest directories, and copies
+    files from the original directory to the test directories.
+
+    'path' -- Path to the benchmark in the source tree.
+
+    'control' -- Results of parsing the regtest control file, as
+    returned from 'ReadControlFile()'."""
+
+    benchmarkname = os.path.join(path, 'benchmark.xml')
+    qmname = '.'.join(SplitAll(path))
+    qmdir = DirToQMDir(path)
+    dom = xml.dom.minidom.parse(benchmarkname)
+    # We basically want to ignore the entire benchmark.xml file, except
+    # for the <implementations>.
+    implementationss = dom.getElementsByTagName('implementations')
+    implementations = implementationss[0]
+    impls = implementations.getElementsByTagName('impl')
+    seq = 0
+    for impl in impls:
+        # Only StreamIt implementations are interesting.
+        if impl.getAttribute('lang') == 'StreamIt':
+            # Where are the source files?  If the implementation has
+            # a dir attribute, they're in that subdirectory.
+            srcdir = path
+            subdir = impl.getAttribute('dir')
+            if subdir: srcdir = os.path.join(srcdir, subdir)
+
+            # What is the name of this implementation?  Number them
+            # sequentially, but override if the implementation has an
+            # id attribute.
+            seq = seq + 1
+            id = impl.getAttribute('id')
+            if not id: id = "impl%d" % seq
+            benchname = qmname + "." + id
+
+            # Create the benchmark directory.
+            benchdir = os.path.join(qmdir, id + ".qms")
+            if not os.path.exists(benchdir):
+                os.makedirs(benchdir)
+
+            # Walk through the files.  Classify them by their class
+            # attribute, and also copy them into benchdir.
+            fileset = {}
+            files = impl.getElementsByTagName('file')
+            for file in files:
+                fnode = file.firstChild
+                fn = fnode.data
+                cls = file.getAttribute('class')
+                if not cls: cls = 'source'
+                if cls not in fileset: fileset[cls] = []
+                fileset[cls].append(fn)
+
+            ActuallyBuildTests(srcdir, benchdir, fileset, benchname, control)
+
+    dom.unlink()
+
+def DirToQMDir(path):
+    """Convert a source-tree path to a QMTest path.
+
+    We replicate the normal directory tree for use by QMTest.  Each
+    component of the directory name gets a '.qms' extension added so
+    that QMTest will recognize it.
+
+    'path' -- Name of the directory to convert.
+
+    returns -- Name of the directory for QMTest's benefit."""
+
+    parts = SplitAll(path)
+    parts = map(lambda p: p + '.qms', parts)
+    path = apply(os.path.join, parts)
+    return path
+
+def SplitAll(path):
+    """Break a path into its component parts.
+
+    The normal Python 'os.path.split()' only removes the tail part of
+    a path; this function splits it fully.
+
+    'path' -- A directory name.
+
+    returns -- A list containing the components of the directory name."""
+    
+    parts = []
+    while path != '':
+        (head, tail) = os.path.split(path)
+        parts = [tail] + parts
+        path = head
+    return parts
+
+def ActuallyBuildTests(srcdir, benchdir, fileset, benchname, control):
+    """Actually construct the QMTest XML test files in a directory.
+
+    'srcdir' -- Directory containing the original benchmark.
+
+    'benchdir' -- Directory in which to create the test files.
+
+    'fileset' -- Mapping from type of file ('source' and 'output'
+    matter) to lists of filenames.
+
+    'benchname' -- QMTest base name for this set of tests.
+
+    'control' -- Regtest control structure."""
+
+    for (target, opts) in control['targets']:
+        testname = MakeOptionName(target, opts)
+        testdir = os.path.join(benchdir, testname + ".qms")
+        if not os.path.exists(testdir):
+            os.makedirs(testdir)
+        # Start by copying all of the files over.
+        for l in fileset.itervalues():
+            for fn in l:
+                src = os.path.join(srcdir, fn)
+                dst = os.path.join(testdir, fn)
+                shutil.copyfile(src, dst)
+        # Three stages, write out the files.
+        extras = { 'opts': opts,
+                   'testname': benchname + '.' + testname }
+        for (GetDOM, fn) in [(GetCompileDOM, 'compile.qmt'),
+                             (GetRunDOM, 'run.qmt'),
+                             (GetVerifyDOM, 'verify.qmt')]:
+            dom = GetDOM(target, fileset, extras)
+            if dom:
+                f = open(os.path.join(testdir, fn), 'w')
+                dom.writexml(f)
+                f.close()
+                dom.unlink()
+
+def GetCompileDOM(target, fileset, extras):
+    """Build an XML DOM for the 'compile' part of the test.
+
+    'target' -- String target for the compiler.
+
+    'fileset' -- Mapping from type of file ('source' matters) to lists
+    of filenames.
+
+    'extras' -- Mapping describing additional parameters available.
+    'opts' should map to a string or list containing extra parameters
+    to the StreamIt compiler, 'strc'."""
+
+    impl = xml.dom.minidom.getDOMImplementation()
+    doc = impl.createDocument(None, 'extension', None)
+    extension = doc.documentElement
+    extension.setAttribute('class', 'streamit.RunStrcTest')
+    extension.setAttribute('kind', 'test')
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'backend')
+    CreateTextElement(doc, argument, 'enumeral', target)
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'options')
+    set = doc.createElement('set')
+    argument.appendChild(set)
+    for opt in extras['opts']:
+        CreateTextElement(doc, set, 'text', opt)
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'filenames')
+    set = doc.createElement('set')
+    argument.appendChild(set)
+    for fn in fileset['source']:
+        CreateTextElement(doc, set, 'text', fn)
+
+    # (Does benchmark.xml give us a way to specify number of iterations?)
+    CreateRunOpts(doc, extension, iters=100, output='output')
+    
+    return doc
+
+def GetRunDOM(target, fileset, extras):
+    """Build an XML DOM for the 'run' part of the test.
+
+    'target' -- String target for the compiler.
+
+    'fileset' -- Mapping from type of file ('source' matters) to lists
+    of filenames.
+
+    'extras' -- Mapping describing additional parameters available.
+    'testname' should contain the basename of this set of tests."""
+
+    # Only deal with the uniprocessor and RAW paths.
+    if not (target == 'uni' or target == 'raw4'):
+        return None
+    impl = xml.dom.minidom.getDOMImplementation()
+    doc = impl.createDocument(None, 'extension', None)
+    extension = doc.documentElement
+    extension.setAttribute('class', 'streamit.RunProgramTest')
+    extension.setAttribute('kind', 'test')
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'prerequisites')
+    set = doc.createElement('set')
+    argument.appendChild(set)
+    tuple = doc.createElement('tuple')
+    set.appendChild(tuple)
+    CreateTextElement(doc, tuple, 'text', extras['testname'] + '.compile')
+    CreateTextElement(doc, tuple, 'enumeral', 'PASS')
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'backend')
+    CreateTextElement(doc, argument, 'enumeral', target)
+
+    # (Does benchmark.xml give us a way to specify number of iterations?)
+    CreateRunOpts(doc, extension, iters=100, output='output')
+    
+    return doc
+
+def GetVerifyDOM(target, fileset, extras):
+    """Build an XML DOM for the 'verify' part of the test.
+
+    'target' -- String target for the compiler.
+
+    'fileset' -- Mapping from type of file ('output' matters) to lists
+    of filenames.
+
+    'extras' -- Mapping describing additional parameters available.
+    'testname' should contain the basename of this set of tests."""
+
+    # Punt now if there's no output file.
+    if 'output' not in fileset:
+        return None
+
+    impl = xml.dom.minidom.getDOMImplementation()
+    doc = impl.createDocument(None, 'extension', None)
+    extension = doc.documentElement
+    extension.setAttribute('class', 'streamit.CompareResultsTest')
+    extension.setAttribute('kind', 'test')
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'prerequisites')
+    set = doc.createElement('set')
+    argument.appendChild(set)
+    tuple = doc.createElement('tuple')
+    set.appendChild(tuple)
+    # What's the actual name of the prereq?  Library doesn't have a
+    # "run" stage.
+    prereq = extras['testname']
+    if target == 'uni' or target == 'raw4':
+        prereq = prereq + '.run'
+    else:
+        prereq = prereq + '.compile'
+    CreateTextElement(doc, tuple, 'text', prereq)
+    CreateTextElement(doc, tuple, 'enumeral', 'PASS')
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'backend')
+    CreateTextElement(doc, argument, 'enumeral', target)
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'output')
+    CreateTextElement(doc, argument, 'text', 'output')
+
+    argument = doc.createElement('argument')
+    extension.appendChild(argument)
+    argument.setAttribute('name', 'expected')
+    CreateTextElement(doc, argument, 'text', fileset['output'][0])
+
+    return doc
+
+def CreateRunOpts(doc, parent, iters=100, output='output'):
+    """Create an XML node describing StreamIt runtime options.
+
+    When invoking a StreamIt program, we need to know how long the
+    program runs for and where its output goes.  This is a single
+    QMTest XML field (described by the 'streamit.RunOptionsField'
+    object).  This function creates that XML 'argument' field and adds
+    it to the specified 'parent'.
+
+    'doc' -- XML document node.
+
+    'parent' -- Parent of the node to create.
+
+    'iters' -- Number of iterations to run the program for.
+
+    'output' -- Name of the output file."""
+    argument = doc.createElement('argument')
+    parent.appendChild(argument)
+    argument.setAttribute('name', 'runopts')
+    tuple = doc.createElement('tuple')
+    argument.appendChild(tuple)
+    CreateTextElement(doc, tuple, 'text', output)
+    CreateTextElement(doc, tuple, 'integer', str(iters))
+
+def CreateTextElement(doc, parent, name, content):
+    """Create an XML node with a text element for a child.
+
+    Shorthand to create an XML node containing only text and insert
+    it in the parent node.  This is for nodes like
+    <node>Contents</node>, which show up somewhat regularly.
+
+    'doc' -- XML document node.
+
+    'parent' -- Parent of the node to create.
+
+    'name' -- Name of the XML element to create.
+
+    'content' -- Text content of the element."""
+    node = doc.createElement(name)
+    node.appendChild(doc.createTextNode(content))
+    parent.appendChild(node)
+
+def MakeOptionName(target, opts):
+    """Return a test name suitable for QMTest.
+
+    This test name should be the directory part of a test name.
+    It encapsulates the target and option list, but is still a valid
+    QMTest test name.
+
+    'target' -- Name of the compiler backend target, should be one
+    of 'library', 'uni', or 'raw4'.
+
+    'opts' -- Additional options to pass to strc."""
+
+    name = target
+    if opts: name = "%s %s" % (target, opts)
+    # Well, that was simple.  But now we need to sanitize it.
+    # A test name can only contain lowercase letters, numbers,
+    # and _.  So, let's sanitize a little:
+    name = name.lower()
+    name = re.sub(" +", "_", name)
+    name = re.sub("[^a-z0-9_]+", "", name)
+    return name
+
+main()
---
+++ streams/regtest/qmtest/streamit.py
@@ -0,0 +1,287 @@
+#
+# streamit.py: Python extensions to QMTest for StreamIt
+# David Maze <dmaze@cag.lcs.mit.edu>
+# $Id: streamit.py,v 1.1 2003-11-18 21:13:11 dmaze Exp $
+#
+
+# This file just defines some extra test classes that QMTest can use.
+# For now it assumes the standard XML database class, though it doesn't
+# explicitly require it.
+
+import os
+import os.path
+import qm.executable
+from   qm.fields import *
+import qm.test.test
+import re
+
+class BackendField(EnumerationField):
+    """A field containing a StreamIt compiler backend."""
+
+    backend_names = ['Uniprocessor', 'Library', 'RAW 4x4']
+    backend_tags = ['uni', 'library', 'raw4']    
+
+    # TODO: think about some way to present the backend_names
+    # to the user, but use the backend_tags internally.
+    def __init__(self, name, default_value=None, **properties):
+        EnumerationField.__init__(self, name, default_value,
+                                  self.backend_tags, **properties)
+
+class RunOptionsField(TupleField):
+    """A field containins options to run a StreamIt program."""
+
+    def __init__(self, name, **properties):
+        fields = [qm.fields.TextField(name="output",
+                                      default_value="output"),
+                  qm.fields.IntegerField(name="iters",
+                                         default_value=100)]
+        TupleField.__init__(self, name, fields, **properties)
+
+def InterpretExitCode(result, status, expected, component):
+    """Interpret the exit code of a program and populate 'result'.
+
+    Populate a QMTest 'result' object, as is passed into a
+    'Test' object's 'Run' method.
+
+    'result' -- Result object to populate.
+
+    'status' -- Exit status of the program.
+
+    'component' -- Name of the component to label messages with."""
+
+    # Figure out what happened; crib from the QMTest ExecTest class.
+    # (But don't worry about Win32.)
+    if os.WIFEXITED(status):
+        exit_code = os.WEXITSTATUS(status)
+        if expected != None and exit_code != expected:
+            result[component + '.expected_exit_code'] \
+                             = str(expected)
+            result[component + '.exit_code'] = str(exit_code)
+            result.Fail("Unexpected exit code.")
+    elif os.WIFSIGNALED(status):
+        signum = os.WTERMSIG(status)
+        result[component + '.signal'] = str(signum)
+        result.Fail("Exited with signal.")
+    elif os.WIFSTOPPED(status):
+        signum = os.WSTOPSIG(status)
+        result[component + '.signal'] = str(signum)
+        result.Fail("Stopped with signal.")
+    else:
+        result.Fail("Program terminated unexpectedly.")
+
+# "What about qm.test.test.Test?  That's only got a little test
+# in it..."  -- paraphrasing Monty Python
+class RunStrcTest(qm.test.test.Test):
+    """Run the StreamIt compiler as a QMTest test.
+
+    Invoke the StreamIt compiler, 'strc', within QMTest.  This only
+    validates the exit status of 'strc', not its output.  In this way
+    it differs from 'qm.test.classes.command.ExecTest', which insists
+    on a consistent standard output and standard error."""
+
+    arguments = [
+        BackendField(name='backend', title='Backend'),
+
+        SetField(contained=
+                 TextField(name='options',
+                           title='Compiler flags',
+                           description=
+                           """Extra compiler flags to pass to 'strc'.
+                                     
+        Options should not contain spaces, so for example '--raw' and
+        '4' should be separate options here.""")),
+        
+        SetField(contained=
+                 TextField(name='filenames',
+                           title='Filenames',
+                           description=
+                           """File names to pass to 'strc'.
+                           
+        Each of these files should be a StreamIt source file.""")),
+        
+        IntegerField(name='exit_code',
+                     title='Exit Code',
+                     description="""The expected exit code.
+
+        The StreamIt compiler should exit with an exit status of 0 if
+        compilation is successful.  For the library backend, this includes
+        executing the program; for the other backends, this involves
+        producing a set of one or more C files."""),
+        
+        RunOptionsField(name='runopts', title='Runtime options',
+                        description="""Options used running the program.
+                        
+        Specifies the number of iterations and the output file for
+        the program.  Whether and how these are used depends on
+        the backend.  The iteration count here is always passed to
+        'strc', but only affects actual compilation for the RAW
+        backend.  For the library backend, 'strc' also runs the
+        program, so the iteration count and output file here
+        affect the program execution.""")
+        ]
+
+    def Run(self, context, result):
+        """Actually run the StreamIt compiler."""
+        path = os.path.join(os.environ['STREAMIT_HOME'], 'strc')
+        # Figure out what flags to use for the backend
+        if self.backend == 'uni':
+            backend = []
+        elif self.backend == 'library':
+            backend = ['--library']
+        elif self.backend == 'raw4':
+            backend = ['--raw', '4']
+        # List of args to the program, starting with the program name,
+        # and always including the iteration count:
+        arguments = [path] + backend + \
+                    ["--iterations", str(self.runopts[1])] + \
+                    self.options + self.filenames
+        e = qm.executable.RedirectedExecutable()
+        status = e.Run(arguments, path=path)
+
+        # In all cases, save stderr.
+        result['RunStrcTest.stderr'] = e.stderr
+        # For the library backend, write stdout to a file;
+        # for other backends, save it.
+        if self.backend == 'library':
+            f = open(self.runopts[0], 'w')
+            f.write(e.stdout)
+            f.close()
+        else:
+            result['RunStrcTest.stdout'] = e.stdout
+
+        InterpretExitCode(result, status, self.exit_code, 'RunStrcTest')
+        
+class RunProgramTest(qm.test.test.Test):
+    """Run a compiled program as a QMTest test.
+
+    This runs the output of the StreamIt compiler.  This is only
+    meaningful for the uniprocessor and RAW backends, since 'strc'
+    runs the program itself for the library backend."""
+
+    arguments = [
+        BackendField(name='backend', title='Backend'),
+
+        RunOptionsField(name='runopts', title='Runtime options',
+                        description="""Options used running the program.
+                        
+        Specifies the number of iterations and the output file for
+        the program.  Whether and how these are used depends on
+        the backend.  In both cases, the output is written to
+        the specified output file.  The iteration count is used
+        for the uniprocessor backend; for the RAW backend, the
+        iteration count used at compile time is used instead.""")
+        ]
+
+    def Run(self, context, result):
+        """Actually run the target program."""
+        if self.backend == 'raw4':
+            return self._RunRaw(context, result)
+        elif self.backend == 'uni':
+            return self._RunUni(context, result)
+        else:
+            # Should raise an exception
+            pass
+
+    def _RunRaw(self, context, result):
+        e = qm.executable.RedirectedExecutable()
+        status = e.Run(['make', '-f', 'Makefile.streamit', 'run'])
+
+        # TODO: see what processing happens on this output, if any.
+        f = open(self.runopts[0], 'w')
+        f.write(e.stdout)
+        f.close()
+
+        InterpretExitCode(result, status, None, 'RunProgramTest')
+
+    def _RunUni(self, context, result):
+        # Unless magic has happened, the binary should be a.out.
+        path = os.path.join('.', 'a.out')
+        arguments = [path, '-i' + str(self.runopts[1])]
+        e = qm.executable.RedirectedExecutable()
+        status = e.Run(arguments, path=path)
+
+        # Dump stdout to the file; ignore stderr.
+        f = open(self.runopts[0], 'w')
+        f.write(e.stdout)
+        f.close()
+
+        # Mostly, note if the program died an unholy death.
+        InterpretExitCode(result, status, None, 'RunProgramTest')
+
+class CompareResultsTest(qm.test.test.Test):
+    """Compare the results from a program run to the expected output."""
+
+    arguments = [
+        BackendField(name='backend', title='Backend'),
+
+        TextField(name='output', title='Output file',
+                  default_value='output',
+                  description="""Name of the file containing the output.
+
+        This file contains the output from running the program.  For
+        the uniprocessor and library backends, this should be a list
+        of values, one to a line; for the RAW backend, this is the
+        output of the RAW simulator 'btl'."""),
+
+        TextField(name='expected', title='Expected results file',
+                  description="""Name of file containing expected results.
+
+        This file contains the expected output from running the
+        program.  This should be a list of values, one to a line, for
+        all backends.""")
+        ]
+
+    # Should these be frobbable fields?
+    # Percent difference between expected and actual allowed:
+    tolerance = 0.2
+    # Difference smaller than this is treated as zero (round-off error)
+    zero = 1e-6
+
+    def Run(self, context, result):
+        """Actually do the comparison."""
+
+        failed = 0
+        
+        # First off, read the expected results file:
+        f = open(self.expected, 'r')
+        expected = f.readlines()
+        f.close()
+
+        # Next, read the actual results file:
+        f = open(self.output, 'r')
+        actual = f.readlines()
+        f.close()
+
+        # For the RAW backend, cook 'actual'.
+        if self.backend == 'raw4':
+            actual = map(lambda s: re.subn(r'^\[(.+:) (.+)\]: (.*)$',
+                                           r'\3', s),
+                         actual)
+            actual = filter(lambda p: p[1] > 0, actual)
+            actual = map(lambda p: p[0], actual)
+
+        # Build pairs of values, converted to floats.
+        pairs = []
+        for i in range(min(len(expected), len(actual))):
+            ev = float(expected[i])
+            av = float(actual[i])
+            p = (ev, av)
+            pairs.append(p)
+
+        # Now do the actual comparison:
+        for i in range(len(pairs)):
+            (ev, av) = pairs[i]
+            difference = abs(av - ev)
+            max_difference = abs(self.tolerance * ev)
+            if difference > max_difference and difference > self.zero:
+                failed = 1
+                tag = 'CompareResultsTest.line_%d' % i
+                result[tag + '.expected'] = ev
+                result[tag + '.actual'] = av
+                if ev != 0:
+                    percent = (difference / ev) * 100
+                    result[tag + '.percent'] = percent
+
+        # All done.
+        if failed:
+            result.Fail('Output mismatch.')

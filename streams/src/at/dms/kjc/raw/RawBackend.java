c358433a1f97b252b8ff70928e7f36275f8f9f0b
Jasper Lin <jasperln@cag.csail.mit.edu>
2003-11-21 00:55:54 +0000

    Don't default to dynamicpartitioner if greedier is on

--- streams/src/at/dms/kjc/raw/RawBackend.java
+++ streams/src/at/dms/kjc/raw/RawBackend.java
@@ -128,7 +128,7 @@ public class RawBackend {
 	// the filters
 	int count = new GraphFlattener(str).getNumTiles();
 	int numTiles = RawBackend.rawRows * RawBackend.rawColumns;
-	boolean partitioning = KjcOptions.partition_dp || KjcOptions.partition_greedy || KjcOptions.partition_ilp;
+	boolean partitioning = KjcOptions.partition_dp || KjcOptions.partition_greedy || KjcOptions.partition_greedier || KjcOptions.partition_ilp;
 	if (count>numTiles && !partitioning) {
 	    System.out.println("Need " + count + " tiles, so turning on partitioning...");
 	    KjcOptions.partition_dp = true;

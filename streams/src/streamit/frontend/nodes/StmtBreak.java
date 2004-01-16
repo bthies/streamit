8f04194aefae2718fa3b0eba210df0c1c064d034
David Maze <dmaze@cag.csail.mit.edu>
2004-01-16 21:18:39 +0000

    Add equals(), hashCode(), toString().

--- streams/src/streamit/frontend/nodes/StmtBreak.java
+++ streams/src/streamit/frontend/nodes/StmtBreak.java
@@ -21,7 +21,7 @@ package streamit.frontend.nodes;
  * innermost section of control flow, such as a for or while loop.
  *
  * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
- * @version $Id: StmtBreak.java,v 1.3 2003-10-09 19:51:00 dmaze Exp $
+ * @version $Id: StmtBreak.java,v 1.4 2004-01-16 21:18:39 dmaze Exp $
  */
 public class StmtBreak extends Statement
 {
@@ -35,6 +35,25 @@ public class StmtBreak extends Statement
     public Object accept(FEVisitor v)
     {
         return v.visitStmtBreak(this);
+    }
+
+    public boolean equals(Object other)
+    {
+        // No state; any two break statements are equal.
+        if (other instanceof StmtBreak)
+            return true;
+        return false;
+    }
+
+    public int hashCode()
+    {
+        // No state, so...
+        return 17;
+    }
+    
+    public String toString()
+    {
+        return "break";
     }
 }
 
--- streams/src/streamit/frontend/nodes/StmtContinue.java
+++ streams/src/streamit/frontend/nodes/StmtContinue.java
@@ -22,7 +22,7 @@ package streamit.frontend.nodes;
  * increment statement if the innermost loop is a for loop.
  *
  * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
- * @version $Id: StmtContinue.java,v 1.3 2003-10-09 19:51:00 dmaze Exp $
+ * @version $Id: StmtContinue.java,v 1.4 2004-01-16 21:18:39 dmaze Exp $
  */
 public class StmtContinue extends Statement
 {
@@ -36,6 +36,25 @@ public class StmtContinue extends Statement
     public Object accept(FEVisitor v)
     {
         return v.visitStmtContinue(this);
+    }
+
+    public boolean equals(Object other)
+    {
+        // No state; any two continue statements are equal.
+        if (other instanceof StmtContinue)
+            return true;
+        return false;
+    }
+
+    public int hashCode()
+    {
+        // No state, so...
+        return 17;
+    }
+    
+    public String toString()
+    {
+        return "continue";
     }
 }
 

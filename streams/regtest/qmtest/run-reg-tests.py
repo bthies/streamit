1c93323c88c60efbf91461a4ae2ad7a25c794860
David Maze <dmaze@cag.csail.mit.edu>
2004-01-21 22:30:27 +0000

    Also add all of the Eclipse packages to $CLASSPATH.  Yay having twelve jar files for one application.

--- streams/regtest/qmtest/run-reg-tests.py
+++ streams/regtest/qmtest/run-reg-tests.py
@@ -2,7 +2,7 @@
 #
 # run-reg-tests.py: Yet another test to run regression tests
 # David Maze <dmaze@cag.lcs.mit.edu>
-# $Id: run-reg-tests.py,v 1.10 2004-01-21 22:10:18 dmaze Exp $
+# $Id: run-reg-tests.py,v 1.11 2004-01-21 22:30:27 dmaze Exp $
 #
 # Taking history from run_reg_tests.pl: this is the third implementation
 # of a script to run StreamIt regression tests.  It is written in Python,
@@ -72,6 +72,27 @@ class RunRegTests:
         class_path = ['.',
                       '/usr/uns/jre/lib/rt.jar',
                       '/usr/uns/java/antlr.jar'] + class_path
+
+        # Eclipse crud:
+        eclipse_base = '/home/bits7/NO_BACKUP/streamit/eclipse/plugins'
+        ecl = eclipse_base + '/org.eclipse.'
+        ecl_ver = '2.1.1'
+        class_path = class_path + \
+                     map(lambda (p, q):
+                         "%s%s_%s/%s" % (ecl, p, ecl_ver, q),
+                         [('ui.workbench.texteditor', 'texteditor.jar'),
+                          ('jface', 'jface.jar'),
+                          ('ui.editors', 'editors.jar'),
+                          ('jface.text', 'jfacetext.jar'),
+                          ('swt.motif', 'ws/motif/swt.jar'),
+                          ('ui.views', 'views.jar'),
+                          ('core.runtime', 'runtime.jar'),
+                          ('ui.workbench', 'workbench.jar'),
+                          ('text', 'text.jar'),
+                          ('jdt.core', 'jdtcore.jar'),
+                          ('jdt.ui', 'jdt.jar'),
+                          ('core.resources', 'resources.jar')])
+        
         os.environ['CLASSPATH'] = ':'.join(class_path)
 
     def run_and_log(self, cmd, filename, action, permissible=0):

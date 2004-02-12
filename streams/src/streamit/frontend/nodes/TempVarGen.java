e72b98aa4d1b74729638ee43967cddcb78274d14
David Maze <dmaze@cag.csail.mit.edu>
2004-02-12 21:34:44 +0000

    Add a "prefix" string which is included in variable names.  This way if we run the front-end multiple times on the same code we won't use the same variable names each time.

--- streams/src/streamit/frontend/nodes/TempVarGen.java
+++ streams/src/streamit/frontend/nodes/TempVarGen.java
@@ -17,10 +17,10 @@
 package streamit.frontend.nodes;
 
 /**
- * Generate a sequence of numbered temporaary variable.  These will have
- * names like __temp_var_1, __temp_var_2, ....  These will primarily be
- * used for separating complex arithmetic, and for serializing pop and
- * peek operations.  That is, code like
+ * Generate a sequence of numbered temporaary variable.  These will
+ * have names like __sa1, __sa2, ....  These will primarily be used
+ * for separating complex arithmetic, and for serializing pop and peek
+ * operations.  That is, code like
  *
  * <pre>
  * push(sum - seq + seq * 1.0i);
@@ -29,26 +29,82 @@ package streamit.frontend.nodes;
  * would get converted into
  *
  * <pre>
- * complex float __temp_var_1;
- * __temp_var_1.real = sum - seq;
- * __temp_var_1.imag = seq;
- * push __temp_var_1;
+ * complex float __sa1;
+ * __sa1.real = sum - seq;
+ * __sa1.imag = seq;
+ * push __sa1;
  * </pre>
  *
+ * The object also carries around a <i>prefix</i>, here <tt>a</tt>,
+ * which is inserted just before the number.  This may be manually
+ * specified, or gleaned from a program representation.
+ *
  * @author  David Maze &lt;dmaze@cag.lcs.mit.edu&gt;
- * @version $Id: TempVarGen.java,v 1.3 2003-12-04 21:03:49 dmaze Exp $
+ * @version $Id: TempVarGen.java,v 1.4 2004-02-12 21:34:44 dmaze Exp $
  */
 public class TempVarGen
 {
     private int seq;
+    private String prefix;
 
     /**
      * Create a new temporary variable generator, which will generate
-     * variables starting with 0.
+     * variables starting with 0 and use a prefix of "a".
      */
     public TempVarGen()
     {
-        seq = 0;
+        this("a");
+    }
+
+    /**
+     * Create a new temporary variable generator, which will generate
+     * variables starting with 0 but use a user-specified prefix.
+     *
+     * @param prefix  prefix string to use
+     */
+    public TempVarGen(String prefix)
+    {
+        this.seq = 0;
+        this.prefix = prefix;
+    }
+
+    /**
+     * Create a new temporary variable generator, guessing the prefix
+     * from the program code.  This is useful when the output of
+     * the frontend is converted back to StreamIt, where we need
+     * to avoid variable name conflicts.
+     *
+     * @param prog  program object to scan for prefixed variables
+     */
+    public TempVarGen(Program prog)
+    {
+        this.seq = 0;
+        this.prefix = "a";
+
+        // Find all local-variable expressions in the program.  If
+        // they start with __s, the next character is the old prefix.
+        // Use the lowest character higher than any of the old
+        // prefixes.
+        prog.accept(new FEReplacer() {
+                public Object visitExprVar(ExprVar expr)
+                {
+                    String name = expr.getName();
+                    if (name.startsWith("__s") && name.length() > 3)
+                    {
+                        char pp = name.charAt(3);
+                        if (Character.isLetter(pp))
+                        {
+                            char op = prefix.charAt(0);
+                            if (pp >= op)
+                            {
+                                char np = (char)((int)pp + 1);
+                                prefix = String.valueOf(np);
+                            }
+                        }
+                    }
+                    return expr;
+                }
+            });
     }
     
     /**
@@ -69,9 +125,19 @@ public class TempVarGen
      * @param num  variable number to get a name for
      * @return     name of the variable
      */
-    public static String varName(int num)
+    public String varName(int num)
     {
-        return "__s" + num;
+        return "__s" + prefix + num;
+    }
+
+    /**
+     * Get the prefix for this variable generator.
+     *
+     * @return  string prefix for the variable generator
+     */
+    public String getPrefix()
+    {
+        return prefix;
     }
 
     /**

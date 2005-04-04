55a1d058ef83735bea464f537f4c954b71907f8a
Bill Thies <bthies@gmail.com>
2005-04-04 20:14:00 +0000

    Moving init_array_1D_* functions from Operator to Misc so they can be shared by library and frontend.

--- streams/src/streamit/misc/Misc.java
+++ streams/src/streamit/misc/Misc.java
@@ -16,6 +16,10 @@
 
 package streamit.misc;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Misc extends AssertedClass
 {
     public static int MAX (int a, int b)
@@ -27,4 +31,72 @@ public class Misc extends AssertedClass
     {
         return (a < b ? a : b);
     }
+
+    /**
+     * FILE READING FUNCTIONS -----------------------------------------------------------
+     *
+     * These functions live here so that they can be shared by
+     * frontend and library without introducing a dependence from one
+     * to the other.
+     */
+
+    /**
+     * Returns int[size] array of elements read from <filename>, which
+     * should be text with one value per line.
+     */
+    public static int[] init_array_1D_int(String filename, int size) {
+	int[] result = new int[size];
+	try {
+	    BufferedReader reader = new BufferedReader(new java.io.FileReader(filename));
+	    for (int i=0; i<size; i++) {
+		String line = reader.readLine();
+		if (line==null) {
+		    throw new RuntimeException("File \"" + filename + "\" contains fewer than " + size + " elements.");
+		}
+		result[i] = Integer.parseInt(line);
+	    }
+	    if (reader.readLine()!=null) {
+		System.err.println("WARNING: file \"" + filename + "\" contains more elements than were read (" + size + " were read)");
+	    }
+	    reader.close();
+	} catch (FileNotFoundException e) {
+	    System.err.println("Could not find file \"" + filename + "\", needed to initialize an array.");
+	    System.exit(1);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	return result;
+    }
+    
+    /**
+     * Returns float[size] array of elements read from <filename>,
+     * which should be text with one value per line.
+     */
+    public static float[] init_array_1D_float(String filename, int size) {
+	float[] result = new float[size];
+	try {
+	    BufferedReader reader = new BufferedReader(new java.io.FileReader(filename));
+	    for (int i=0; i<size; i++) {
+		String line = reader.readLine();
+		if (line==null) {
+		    throw new RuntimeException("File \"" + filename + "\" contains fewer than " + size + " elements.");
+		}
+		result[i] = Float.parseFloat(line);
+	    }
+	    if (reader.readLine()!=null) {
+		System.err.println("WARNING: file \"" + filename + "\" contains more elements than were read (" + size + " were read)");
+	    }
+	    reader.close();
+	} catch (FileNotFoundException e) {
+	    System.err.println("Could not find file \"" + filename + "\", needed to initialize an array.");
+	    System.exit(1);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	return result;
+    }
+    
+    /**
+     * END FILE READING FUNCTIONS -----------------------------------------------------------
+     */
 }

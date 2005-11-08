71c58fb1dbcb196c15bdddbe973d6376ad69dfe5
Allyn Dimock <dimock@cag.csail.mit.edu>
2005-11-08 15:42:44 +0000

    Start a SIRUtils class for little methods on SIR that would otherwise be spread all over the place.  Currently only has method isSpecialFilter which checks for SIRFileReader, SIRFileWriter, and SIRIdentity filters.

---
+++ streams/src/at/dms/kjc/sir/SIRUtils.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package at.dms.kjc.sir;
+
+/**
+ * This class contains the odd small utility method for use with SIR.
+ * 
+ * @author dimock
+ *
+ */
+public class SIRUtils {
+
+	/**
+	 * Determine whether a filter is one that needs special processing.
+	 * 
+	 * Returns true if a filter is one of the special pre-defined filters.
+	 * else returns false.
+	 * 
+	 * The predefined filters are currently: SIRFileReader, SIRFileWriter,
+	 * and SIRIdentity 
+	 *
+	 */
+	
+	static boolean isSpecialFilter (SIRFilter f) {
+		return (f instanceof SIRFileReader
+				|| f instanceof SIRFileReader
+				|| f instanceof SIRIdentity);
+	}
+}

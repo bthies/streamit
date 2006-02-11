b89ceebda4ed84931e727b9991880d094d4bf049
Bill Thies <bthies@gmail.com>
2006-02-11 03:39:29 +0000

    Forgot to add this interface.

---
+++ streams/src/streamit/library/iriter/IterFactory.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2003 by the Massachusetts Institute of Technology.
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+
+package streamit.library.iriter;
+
+import streamit.library.FeedbackLoop;
+import streamit.library.Filter;
+import streamit.library.Pipeline;
+import streamit.library.SplitJoin;
+import streamit.library.Stream;
+
+/**
+ * This represents an iterator factory that returns iterators for streams.
+ */
+public interface IterFactory {
+
+    /**
+     * Returns a new basic iterator for <filter>.
+     */
+    public streamit.scheduler2.iriter.FilterIter newFrom(Filter filter);
+
+    /**
+     * Returns a new iterator for <pipeline>.
+     */
+    public streamit.scheduler2.iriter.PipelineIter newFrom(Pipeline pipeline);
+    
+    /**
+     * Returns a new iterator for <sj>.
+     */
+    public streamit.scheduler2.iriter.SplitJoinIter newFrom(SplitJoin sj);
+    
+    /**
+     * Returns a new iterator for <fl>.
+     */
+    public streamit.scheduler2.iriter.FeedbackLoopIter newFrom(FeedbackLoop fl);
+
+}

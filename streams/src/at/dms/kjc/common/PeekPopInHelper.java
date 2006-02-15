2ef66f732e3cc8284068ba9bb78fd8930a1706bc
Michael Gordon <michael.ian.gordon@gmail.com>
2006-02-15 23:49:05 +0000

    These two classes determine if there are Peeks or Pops (PeekPopInHelper) or Peek or Pop or Push (PeekPopPush) in helper (non-work) methods.

---
+++ streams/src/at/dms/kjc/common/PeekPopInHelper.java
@@ -0,0 +1,41 @@
+package at.dms.kjc.common;
+
+import at.dms.kjc.*;
+import at.dms.kjc.sir.*;
+
+/**
+ * This class will search for input communication expressions outside of the work
+ * function of a filter and return true if they exist.
+ *
+ * @author mgordon
+ */
+public class PeekPopInHelper extends SLIREmptyVisitor {
+    private static boolean found;
+
+    /** returns true if we find communication statements/expressions
+     * outside of the work function (i.e. in a helper function 
+     * 
+     * @return true if we find a peek or pop in a helper function
+     */
+    public static boolean check(SIRFilter filter) {
+	for (int i = 0; i < filter.getMethods().length; i++) {
+	    if (!filter.getMethods()[i].equals(filter.getWork())) {
+		found = false;
+		filter.getMethods()[i]
+		    .accept(new PeekPopInHelper());
+		if (found)
+		    return true;
+	    }
+	}
+	return false;
+    }
+
+    public void visitPeekExpression(SIRPeekExpression self, CType tapeType,
+				    JExpression arg) {
+	found = true;
+    }
+
+    public void visitPopExpression(SIRPopExpression self, CType tapeType) {
+	found = true;
+    }
+}

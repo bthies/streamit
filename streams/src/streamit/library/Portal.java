0fd80b03e93bfaf542ffeb95782b27229d2b3eb2
Bill Thies <bthies@gmail.com>
2006-02-23 10:12:45 +0000

    Speeding up SDEP calculation in the library by calculating SDEP for many downstream receivers at once.
    Now can do about 10,000 downstream receivers in 15 secs.  Can probably
    be improved more.

--- streams/src/streamit/library/Portal.java
+++ streams/src/streamit/library/Portal.java
@@ -23,7 +23,10 @@ import streamit.library.iriter.Iterator;
 import streamit.library.iriter.SDEPIterFactory;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * A portal that messages can be sent to.  For the StreamIt compiler,
@@ -33,7 +36,7 @@ import java.util.HashMap;
  * defined; that class is the portal object.  Receiver objects should
  * also implement the interface.
  *
- * @version $Id: Portal.java,v 1.17 2006-02-20 22:32:17 thies Exp $
+ * @version $Id: Portal.java,v 1.18 2006-02-23 10:12:45 thies Exp $
  */
 public abstract class Portal
 {
@@ -48,11 +51,6 @@ public abstract class Portal
      * SDEP info for this portal.
      */
     private SDEPInfo sdepInfo;
-    /**
-     * A general scheduler to determine upstream/downstream relations
-     * between filters.
-     */
-    private static Scheduler upDownScheduler;
     
     public Portal() {
         this.receivers = new ArrayList();
@@ -138,77 +136,224 @@ public abstract class Portal
     }
 
     /**
-     * Returns SDEP data from sender to each receiver.
+     * Calculates which receivers are downstream of the sender.  In
+     * returned array, element of "true" means the i'th receiver is
+     * downstream.
+     */
+    public boolean[] calcDownstream(Stream sender) {
+        boolean[] downstream = new boolean[receivers.size()];
+        for (int i=0; i<receivers.size(); i++) {
+            Stream receiver = (Stream)receivers.get(i);
+            switch (Operator.compareStreamPosition(sender, receiver)) {
+            case -1: // receiver -> ... -> sender
+                downstream[i] = false;
+                break;
+            case 0:  // receiver | sender
+                // fail -- no path between sender and receiver
+                new RuntimeException("No path between message sender and receiver in stream graph.")
+                    .printStackTrace();
+                break;
+            case 1:  // sender -> ... -> receiver
+                downstream[i] = true;
+                break;
+            default:
+                assert false : "Unexpected comparison value.";
+            }
+        }
+        return downstream;
+    }
+
+    /**
+     * Returns list of receivers that are upstream of the sender.
+     * Requires that upstream/downstream info has been calculated.
+     */
+    private List getUpstreamReceivers(boolean[] downstream) {
+        List result = new LinkedList();
+        for (int i=0; i<receivers.size(); i++) {
+            if (!downstream[i]) {
+                result.add(receivers.get(i));
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Given whether or not each receiver is <downstream> of the
+     * sender, return mapping from filters to SDEP data for the
+     * upstream messages.
+     */
+    private HashMap getUpstreamSDEPs(Stream sender, boolean[] downstream) {
+        HashMap result = new HashMap();
+        // identify list of upstream receivers.
+        List upstreamReceivers = getUpstreamReceivers(downstream);
+        // for now have to do each one separately
+        for (java.util.Iterator i = upstreamReceivers.iterator(); i.hasNext(); ) {
+            Stream receiver = (Stream)i.next();
+
+            // make factory to represent this node as possibly phased
+            SDEPIterFactory factory = new SDEPIterFactory(sender, receiver, false);
+            Scheduler scheduler = Scheduler.createForSDEP(new Iterator(Stream.toplevel, factory));
+
+            // compute dependences upstream
+            SDEPData data = null;
+            try {
+                data = scheduler.computeSDEP(new Iterator(receiver, factory),
+                                             new Iterator(sender, factory));
+            } catch (NoPathException e) {
+                // should not happen because we checked that there is a path
+                e.printStackTrace();
+            }
+            result.put(receiver, data);
+        }
+        return result;
+    }
+
+    /**
+     * Returns list of HashSets of receivers that are downstream of
+     * the sender.  Each set is safe to calculate in parallel.
+     *
+     * Currently two SDEP receivers can be calculated
+     * together if:
+     *  1. they are both downstream of the sender
+     *  2. Either:
+     *      - the receiver closest to the sender is not phased
+     *     Or:
+     *      - they are in parallel in the stream graph
+     *
+     * Requires that upstream/downstream info has been calculated.
+     */
+    private List getDownstreamReceiverSets(boolean[] downstream) {
+        LinkedList result = new LinkedList();
+
+        // first build set of phased, non-phased downstream filters
+        HashSet notPhased = new HashSet();
+        HashSet phased = new HashSet();
+        for (int i=0; i<receivers.size(); i++) {
+            if (downstream[i]) {
+                if (((Stream)receivers.get(i)) instanceof PhasedFilter) {
+                    phased.add(receivers.get(i));
+                } else {
+                    notPhased.add(receivers.get(i));
+                }
+            }
+        }
+
+        // build list of sets for phased filters that can be computed in parallel
+        if (phased.size()>0) {
+            PhasedFilter str[] = (PhasedFilter[])phased.toArray(new PhasedFilter[0]);
+            result.add(new HashSet());
+
+            for (int i=0; i<str.length; i++) {
+                // find a set for str[i]
+                for (java.util.Iterator iter = result.iterator(); iter.hasNext(); ) {
+                    HashSet set = (HashSet)iter.next();
+                    // test to see if str[i] runs in parallel with all
+                    // elements of <set>
+                    boolean parallel = true;
+                    for (java.util.Iterator setIter = set.iterator(); setIter.hasNext(); ) {
+                        PhasedFilter other = (PhasedFilter)setIter.next();
+                        parallel = Operator.compareStreamPosition(str[i], other)==0;
+                        // if not in parallel, can't be in same set
+                        if (!parallel) break;
+                    }
+                    // if current stream runs in parallel with everyone in
+                    // current set, add stream to set
+                    if (parallel) {
+                        set.add(str[i]);
+                    } else if (!iter.hasNext()) {
+                        // if we reached the last set to try, make a new set
+                        HashSet newSet = new HashSet();
+                        newSet.add(str[i]);
+                        result.add(newSet);
+                    }
+                }
+            }
+        }
+
+        // prepend the list of non-phased filters to the list of sets
+        if (notPhased.size()>0) {
+            result.addFirst(notPhased);
+        }
+        
+        return result;
+    }
+
+    /**
+     * Given whether or not each receiver is <downstream> of the
+     * sender, return mapping from filters to SDEP data for the
+     * downstream messages.
+     */
+    private HashMap getDownstreamSDEPs(Stream sender, boolean[] downstream) {
+        HashMap result = new HashMap();
+        // calculate list of HashSets of downstream receivers that can
+        // be calcualted in parallel
+        List downstreamReceivers = getDownstreamReceiverSets(downstream);
+        // do one calculation per set
+        for (java.util.Iterator i = downstreamReceivers.iterator(); i.hasNext(); ) {
+            HashSet receiverSet = (HashSet)i.next();
+            assert receiverSet.size() > 0;
+
+            // make factory to represent receivers as possibly phased
+            SDEPIterFactory factory = new SDEPIterFactory(sender, receiverSet, true);
+            Scheduler scheduler = Scheduler.createForSDEP(new Iterator(Stream.toplevel, factory));
+
+            // map receiverSet (holds Streams) to receiverIters (holds Iterators)
+            HashSet receiverIters = new HashSet();
+            for (java.util.Iterator setIter = receiverSet.iterator(); setIter.hasNext(); ) {
+                receiverIters.add(new Iterator((Stream)setIter.next(), factory));
+            }
+
+            // compute dependences downstream
+            HashMap resultIters = null;
+            try {
+                resultIters = scheduler.computeSDEP(new Iterator(sender, factory),
+                                                    receiverIters);
+            } catch (NoPathException e) {
+                // should not happen because we checked that there is a path
+                e.printStackTrace();
+            }
+
+            // map resultIters (keyed on Iterators) to data (keyed on Streams)
+            for (java.util.Iterator resultIter = resultIters.keySet().iterator(); resultIter.hasNext(); ) {
+                Iterator iter = (Iterator)resultIter.next();
+                assert resultIters.get(iter)!=null : "Null data for " + iter.getObject();
+                result.put(iter.getObject(), resultIters.get(iter));
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * Toplevel procedure for getting SDEP data, from sender to each
+     * receiver.
      */
     private SDEPInfo getSDEP(Stream sender) {
         if (sdepInfo != null) {
             return sdepInfo;
         } else {
-            // initialize general scheduler if needed.  (can't do this
-            // in constructor because graph may not be setup yet.)
-            if (upDownScheduler == null) {
-                upDownScheduler = Scheduler.createForSDEP(new Iterator(Stream.toplevel));
-            }
-
             // for each receiver, calculate up/downstream info
-            boolean[] downstream = new boolean[receivers.size()];
-            for (int i=0; i<receivers.size(); i++) {
+            boolean[] downstream = calcDownstream(sender);
+
+            // get upstream and downstream sdep data
+            HashMap upstreamSDEPs = getUpstreamSDEPs(sender, downstream);
+            HashMap downstreamSDEPs = getDownstreamSDEPs(sender, downstream);
+
+            // combine data into single map (receiver Stream -> SDEP data)
+            HashMap data = new HashMap();
+            data.putAll(upstreamSDEPs);
+            data.putAll(downstreamSDEPs);
+
+            // dump hashmap of data into array of sdepInfo
+            SDEPData[] dataArray = new SDEPData[receivers.size()];
+            for (int i=0; i<dataArray.length; i++) {
+                // find data for receiver[i]
                 Stream receiver = (Stream)receivers.get(i);
-                switch (Operator.compareStreamPosition(sender, receiver)) {
-                case -1: // receiver -> ... -> sender
-                    downstream[i] = false;
-                    break;
-                case 0:  // receiver | sender
-                    // fail -- no path between sender and receiver
-                    new RuntimeException("No path between message sender and receiver in stream graph.")
-                        .printStackTrace();
-                    break;
-                case 1:  // sender -> ... -> receiver
-                    downstream[i] = true;
-                    break;
-                default:
-                    assert false : "Unexpected comparison value.";
-                }
+                assert data.containsKey(receiver) : "No data for " + receiver;
+                dataArray[i] = (SDEPData)data.get(receiver);
             }
+            sdepInfo = new SDEPInfo(dataArray, downstream);
 
-            // see if any receivers are phased.  If they are, will
-            // have to rebuild scheduler / SDEP graph for each
-            // receiver (see SDEPIterFactory for details)
-            boolean phasedReceiver = false;
-            for (int i=0; i<receivers.size(); i++) {
-                if (((Stream)receivers.get(i)) instanceof PhasedFilter) {
-                    phasedReceiver = true;
-                }
-            }
-
-            // for each receiver, calculate sdep data
-            SDEPData[] data = new SDEPData[receivers.size()];
-            SDEPIterFactory factory = null;
-            Scheduler scheduler = null;
-            for (int i=0; i<receivers.size(); i++) {
-                Stream receiver = (Stream)receivers.get(i);
-                // make sdep scheduler.  Need to redo for each receiver if phased.
-                if (i==0 || phasedReceiver) {
-                    factory = new SDEPIterFactory(sender, receiver, downstream[i]);
-                    scheduler = Scheduler.createForSDEP(new Iterator(Stream.toplevel, factory));
-                }
-                try {
-                    if (downstream[i]) {
-                        // if downstream path, compute dependences upstream
-                        data[i] = scheduler.computeSDEP(new Iterator(sender, factory),
-                                                        new Iterator(receiver, factory));
-                    } else {
-                        // if upstream path, compute dependences upstream
-                        data[i] = scheduler.computeSDEP(new Iterator(receiver, factory),
-                                                        new Iterator(sender, factory));
-                    }
-                } catch (NoPathException e) {
-                    // should not happen because we checked that there is a path
-                    e.printStackTrace();
-                }
-            }
-
-            sdepInfo = new SDEPInfo(data, downstream);
             return sdepInfo;
         }
     }

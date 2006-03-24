7b1c5924818319e3d8e165635270cfb295b1d883
Jasper Lin <jasperln@cag.csail.mit.edu>
2006-03-24 18:11:09 +0000

    Adding javadoc and inline comments.

--- streams/src/at/dms/kjc/sir/lowering/partition/GreedierPartitioner.java
+++ streams/src/at/dms/kjc/sir/lowering/partition/GreedierPartitioner.java
@@ -13,6 +13,15 @@ import at.dms.kjc.sir.lowering.fission.*;
 import at.dms.kjc.sir.lowering.partition.*;
 import at.dms.kjc.flatgraph.*;
 
+/**
+ * Greedier partitioner that can switch back and forth between fissing
+ * and fusing depending on what's more appropriate. Considers any
+ * adjacent fusable filters within same container for fusion. Cannot
+ * consider adjacent filters in graph but in different containers
+ * since graph needs to remain structured at this stage but it would
+ * be nice if this were supported at some point.
+ * @author jasperln
+ */
 public class GreedierPartitioner {
     /**
      * Whether or not joiners need tiles.
@@ -32,13 +41,21 @@ public class GreedierPartitioner {
     private final int numTiles;
     private TreeMap pairs; //Ordered fusable pairs (keys of type Pair)
     private TreeMap nodes; //Ordered filters (keys of type Node)
-    private static final int FUSION_OVERHEAD=0; //Adjust
+    private static final int FUSION_OVERHEAD=0; //Can adjust
 
+    /**
+     * Construct GreedierPartitioner.
+     * @param str The toplevel SIRStream.
+     * @param work WorkEstimate to drive fusion.
+     * @param numTiles Target number of tiles.
+     * @param joinersNeedTiles Indicates if joiners require a tile of thier own. (Needed on some backends.)
+     */
     public GreedierPartitioner(SIRStream str,WorkEstimate work,int numTiles,boolean joinersNeedTiles) {
         this.str=str;
         this.work=work;
         this.numTiles=numTiles;
         this.joinersNeedTiles=joinersNeedTiles;
+	//Setup pairs (of nodes) ordered by work
         pairs=new TreeMap(new Comparator() {
                 public int compare(Object o1,Object o2) {
                     if(o1==o2)
@@ -75,6 +92,7 @@ public class GreedierPartitioner {
                       return 1;*/
                 }
             });
+	//Setup full list of nodes ordered by work
         nodes=new TreeMap(new Comparator() {
                 public int compare(Object o1,Object o2) {
                     if(o1==o2)
@@ -95,7 +113,11 @@ public class GreedierPartitioner {
         buildNodesList();
     }
     
-    //Creates Nodes and populates pairs,nodes
+    /**
+     * Helper fucntion that creates Nodes for each filter and
+     * populates pairs and nodes. Pairs are generated for every pair
+     * of adjacent Nodes with same parent.
+     */
     private void buildNodesList() {
         // make sure that dynamic rates interpreted correctly for
         // isFusable test
@@ -112,16 +134,18 @@ public class GreedierPartitioner {
                         Node node=new Node(self,work.getWork(self));
                         nodes.put(node,null);
                         if(StatelessDuplicate.isFissable(self))
-                            node.fissable=true;
+                            node.fissable=true; //Set fissable
                         SIRContainer parent=self.getParent();
-                        if(prevNode!=null) {
+                        if(prevNode!=null) { //Check edge condition
+			    //Connect adjacent Nodes
                             prevNode.next=node;
                             node.prev=prevNode;
-                            if(parent.equals(prevParent)) {
+                            if(parent.equals(prevParent)) { //Make sure parents equal
                                 Pair pair=new Pair(prevNode,node);
                                 pairs.put(pair,null);
                             }
                         }
+			//Set state for next filter check
                         prevNode=node;
                         prevParent=parent;
                     }
@@ -143,47 +167,57 @@ public class GreedierPartitioner {
         do { //Iterate till can't (# fissable filters is always decreasing)
             cont=false;
             boolean fiss=shouldFiss(); //Try Fiss
+	    //Fiss while appropriate and under target num of tiles
             while(fiss&&count<numTiles) {
-                cont=true;
-                Node big=(Node)nodes.lastKey();
+                cont=true; //Some change so continue iterating
+                Node big=(Node)nodes.lastKey(); //Get biggest Node
                 System.out.println("  Fissing: "+big.filter);
-                fiss(big);
+                fiss(big); //Fiss
+		//Reeval number of tiles needed and whether should fiss
                 count=Partitioner.countTilesNeeded(str, joinersNeedTiles);
                 fiss=shouldFiss();
                 System.out.println("  GreedierPartitioner detects " + count + " tiles.");
             }
             while(count>numTiles) { //Try Fuse
-                cont=true;
-                //Pair smallest=(Pair)pairs.firstKey();
-                Pair smallest=findSmallest();
-                fuse(smallest);
+                cont=true; //Some change so continue iterating
+                Pair smallest=findSmallest();  //Get smallest Pair
+                fuse(smallest); //Fuse
+		//Reeval number of tiles needed
                 count=Partitioner.countTilesNeeded(str, joinersNeedTiles);
                 System.out.println("  GreedierPartitioner detects " + count + " tiles.");
             }
-        } while(cont);
+        } while(cont); //Iterate till convergence
         return str;
     }
-
+    
+    /**
+     * Helper function that returns smallest Pair.
+     */
     private Pair findSmallest() {
         // if there is only one key left, return it
         if (pairs.size()==1) {
             return (Pair)pairs.firstKey();
         }
 
-        Pair smallest=(Pair)pairs.firstKey();
+        Pair smallest=(Pair)pairs.firstKey(); //Get smallest pair
         int work=smallest.work;
         ArrayList temp=new ArrayList();
+	/* There may be several filters with smallest work though. If
+	 * there are prefer the ones near the middle of the
+	 * container. This seems to help keep the graph balanced. */
         boolean cont;
         do {
             cont=false;
-            temp.add(smallest);
-            pairs.remove(smallest);
+            temp.add(smallest); //Save smallest
+            pairs.remove(smallest); //Remove smallest so next smallest can be reached
             Pair newPair=(Pair)pairs.firstKey();
             int newWork=newPair.work;
-            if(newWork==work) {
+            if(newWork==work) { //There are several filters with smallest work
                 SIRContainer parent1=smallest.n1.filter.getParent();
                 SIRContainer parent2=newPair.n1.filter.getParent();
                 if(parent1==parent2) {
+		    //If there are several smallest in same parent
+		    //prefer the ones near the middle
                     double half=((double)(parent1.size()-1))/2;
                     double dist1=Math.abs(half-parent1.indexOf(smallest.n1.filter));
                     double dist2=Math.abs(half-parent1.indexOf(newPair.n1.filter));
@@ -195,12 +229,20 @@ public class GreedierPartitioner {
                 }
             }
         } while(cont);
+	//Restore the Pairs temporarily removed from pairs
         Object[] fix=temp.toArray();
         for(int i=0;i<fix.length;i++)
             pairs.put(fix[i],null);
         return smallest;
     }
 
+    /**
+     * Helper function to determine if it's beneficial to fiss. Checks
+     * to see if biggest filter is fissable and if fusing the 2nd
+     * smallest pair would create a filter larger than the
+     * bottleneck. This is because fissing will produce at least 1
+     * extra filter.
+     */
     private boolean shouldFiss() {
         Node big=(Node)nodes.lastKey();
         if(!big.fissable)
@@ -212,20 +254,24 @@ public class GreedierPartitioner {
         return testSmall.work+FUSION_OVERHEAD<big.work;
     }
 
+    /**
+     * Helper function to fuse Pairs.
+     */
     private void fuse(Pair p) {
+	//Remove nodes
         nodes.remove(p.n1);
         nodes.remove(p.n2);
         SIRContainer parent=p.n1.filter.getParent();
-        PartitionGroup part=findPartition(p,parent);
+        PartitionGroup part=findPartition(p,parent); //Create PartitionGroup
         //Fuse
         SIRFilter result=null;
         if(parent instanceof SIRPipeline) {
-            result=(SIRFilter)FusePipe.fuse((SIRPipeline)parent,part);
-            Lifter.eliminatePipe((SIRPipeline)parent);
+            result=(SIRFilter)FusePipe.fuse((SIRPipeline)parent,part); //Fuse
+            Lifter.eliminatePipe((SIRPipeline)parent); //Eliminate pipe potentially
         } else if(parent instanceof SIRSplitJoin) {
-            FuseSplit.fuse((SIRSplitJoin)parent,part);
+            FuseSplit.fuse((SIRSplitJoin)parent,part); //Fuse
             result=(SIRFilter)FuseSplit.getLastFused();
-            Lifter.eliminateSJ((SIRSplitJoin)parent);
+            Lifter.eliminateSJ((SIRSplitJoin)parent); //Eliminate splitjoin potentially
         } else
             Utils.fail("Trying to fuse FeedBackLoop");
         //Update Rep
@@ -233,7 +279,7 @@ public class GreedierPartitioner {
         Node next=p.n2.next;
         Node newNode=new Node(result,WorkEstimate.getWorkEstimate(result).getWork(result));
         nodes.put(newNode,null);
-        if(prev!=null) {
+        if(prev!=null) { //Fix connections
             prev.next=newNode;
             newNode.prev=prev;
             Pair inter=prev.p2;
@@ -247,7 +293,7 @@ public class GreedierPartitioner {
                 pairs.put(new Pair(prev,newNode),null);
             }
         }
-        if(next!=null) {
+        if(next!=null) { //Fix connections
             next.prev=newNode;
             newNode.next=next;
             Pair inter=next.p1;
@@ -267,6 +313,11 @@ public class GreedierPartitioner {
         p.n2=null;
     }
 
+    /**
+     * Helper function to fiss filter. shouldFiss() should have been
+     * called before this method so it assumes it's safe (and
+     * appropriate) to fiss largest filter.
+     */
     private void fiss(Node node) {
         nodes.remove(node);
         //Fiss
@@ -274,14 +325,14 @@ public class GreedierPartitioner {
         //Update Rep
         SIRFilter filt=(SIRFilter)split.get(0);
         Node n1=new Node(filt,WorkEstimate.getWorkEstimate(filt).getWork(filt));
-        nodes.put(n1,null);
+        nodes.put(n1,null); //Add new node for first filter
         filt=(SIRFilter)split.get(1);
         Node n2=new Node(filt,WorkEstimate.getWorkEstimate(filt).getWork(filt));
-        nodes.put(n2,null);
+        nodes.put(n2,null); //Add new node for second filter
         n1.next=n2;
         n2.prev=n1;
         Node prev=node.prev;
-        if(prev!=null) {
+        if(prev!=null) { //Fix connections
             prev.next=n1;
             n1.prev=prev;
             //Cleaning up
@@ -294,7 +345,7 @@ public class GreedierPartitioner {
             }
         }
         Node next=node.next;
-        if(next!=null) {
+        if(next!=null) { //Fix connections
             next.prev=n2;
             n2.next=next;
             //Cleaning up
@@ -308,7 +359,12 @@ public class GreedierPartitioner {
         }
     }
 
-    //Create PartitionGroup corresponding to Pair p in parent
+    /**
+     * Helper function to create PartitionGroup corresponding to Pair
+     * p in parent.
+     * @param p The pair we're interested in partitioning.
+     * @param parent The container to search within.
+     */
     private PartitionGroup findPartition(Pair p,SIRContainer parent) {
         SIRFilter f1=p.n1.filter;
         int[] part=new int[parent.size()-1];
@@ -318,27 +374,83 @@ public class GreedierPartitioner {
         return PartitionGroup.createFromArray(part);
     }
     
+    /**
+     * Class that packages SIRFilter together with its work estimate
+     * and maintains some connectivity information like prev/next
+     * Node, what Pairs it's contained in, and whether the filter is
+     * fissable.
+     */
     class Node {
+	/**
+	 * The SIRFilter represented by this Pair.
+	 */
         SIRFilter filter;
+	/**
+	 * The work estimate for this filter.
+	 */
         int work;
-        Node prev,next;
-        Pair p1,p2; // Pairs that contain this (order matters: p1==pair with prev)
-        boolean fissable; // Only filters fissable in the original graph have fissable==true
+	/**
+	 * Pointer to prev Node.
+	 */
+        Node prev;
+	/**
+	 * Pointer to next Node.
+	 */
+	Node next;
+	/**
+	 * Pair with previous filter.
+	 */
+        Pair p1;
+	/**
+	 * Pair with next filter.
+	 */
+	Pair p2;
+	/**
+	 * Set if filter is fissable.
+	 */
+        boolean fissable;
 
+	/**
+	 * Construct new node from filter with work estimate.
+	 * @param filter The filter this Node represents.
+	 * @param work The work estimate of this Node.
+	 */
         public Node(SIRFilter filter,int work) {
             this.filter=filter;
             this.work=work;
         }
 
+	/**
+	 * String representation. Returns filter's name.
+	 */
         public String toString() {
             return filter.getName();
         }
     }
 
+    /**
+     * Pair of Nodes with associated work estimate. Right now the work
+     * estimate is just the sum of the work estimates of the
+     * nodes. Order maters. The first node is the previous node.
+     */
     class Pair {
-        Node n1,n2; //Order matters (n1==first node)
+	/**
+	 * First node in Pair.
+	 */
+        Node n1;
+	/**
+	 * Second node in Pair.
+	 */	
+	Node n2;
+	/**
+	 * Work estimate of Pair (sum of work from Nodes)
+	 */
         int work;
-    
+	/**
+	 * Construct Pair.
+	 * @param n1 First node in Pair.
+	 * @param n2 Second node in Pair.
+	 */
         public Pair(Node n1,Node n2) {
             this.n1=n1;
             this.n2=n2;
@@ -347,10 +459,19 @@ public class GreedierPartitioner {
             n2.p1=this;
         }
 
+	/**
+	 * Calculates work of two Node. Would be static but inner
+	 * classes cannot have static declarations.
+	 * @param n1 First node.
+	 * @param n2 Second node.
+	 */
         public void work(Node n1,Node n2) {
             work=n1.work+n2.work;
         }
 
+	/**
+	 * String representation.
+	 */
         public String toString() {
             return "Pair(" + n1 + ", " + n2  + ")";
         }

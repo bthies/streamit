33a31eae1230bc77e18e41fe74449bd5654896e7
Allyn Dimock <dimock@cag.csail.mit.edu>
2006-05-25 21:52:18 +0000

    reinstated IA64 and ARM

--- streams/src/at/dms/kjc/cluster/GenerateMakefile.java
+++ streams/src/at/dms/kjc/cluster/GenerateMakefile.java
@@ -38,17 +38,18 @@ public class GenerateMakefile {
         p.print("LIB_CLUSTER = $(STREAMIT_HOME)/library/cluster\n");
 
         p.newLine();    
-        p.print("CC = gcc34 #gcc34\n"); // gcc34
-//        p.print("CC_IA64 = ecc\n");
-//        p.print("CC_ARM = /u/janiss/bin/arm343 #arm-linux-gcc\n");
-
-        p.newLine();
+        p.print("CC = gcc34\n"); // gcc34
         p.print("CCFLAGS = -O3\n");
-//        p.print("CCFLAGS_IA64 = -O3\n");
-//        p.print("CCFLAGS_ARM = -O3\n");
+
+        p.newline();
+        p.println("# Unsupported target machines");
+        p.print("CC_IA64 = ecc\n");
+        p.print("CC_ARM = /u/janiss/bin/arm343 #arm-linux-gcc\n");
+        p.print("CCFLAGS_IA64 = -O3\n");
+        p.print("CCFLAGS_ARM = -O3\n");
 
         p.newLine();
-        p.print("NAMES = ");
+        p.println("NAMES = \\");
     
         {
             int i;
@@ -62,10 +63,12 @@ public class GenerateMakefile {
 
         p.newLine();
 
-        p.print("SOURCES = \t$(NAMES:%=%.cpp)\n");
-        p.print("OBJS = \t$(NAMES:%=%.o)\n");
-//        p.print("OBJS_IA64 = \t$(NAMES:%=%_ia64.o)\n");
-//        p.print("OBJS_ARM = \t$(NAMES:%=%_arm.o)\n");
+        p.print("SOURCES = $(NAMES:%=%.cpp)\n");
+        p.print("OBJS = $(NAMES:%=%.o)\n");
+
+        p.newLine();    
+        p.print("OBJS_IA64 = $(NAMES:%=%_ia64.o)\n");
+        p.print("OBJS_ARM = $(NAMES:%=%_arm.o)\n");
 
         p.newLine();
 
@@ -73,28 +76,13 @@ public class GenerateMakefile {
         p.println("all: " + executablename);
         p.newLine();
 
-//        if (KjcOptions.standalone) {
-//            p.print("ia64: fusion_ia64\n");
-//        } else {
-//            p.print("ia64: run_cluster_ia64\n");
-//        }
-//
-//        p.newLine();
-//    
-//        p.print("arm: fusion_arm\n");
-//         
-//        p.newLine();
-//
+
         p.print("clean:\n");
         p.println("\trm -f master*.o fusion*.o thread*.o " + executablename);
         p.newLine();
         p.newLine();
 
-        // =============== run_cluster
-
         if (KjcOptions.standalone) {
-            // =============== fusion
-
             p.print(executablename + ": fusion.o ");
         } else {
             p.print(executablename + ": master.o global.o ");
@@ -116,43 +104,46 @@ public class GenerateMakefile {
 
         p.newLine();
 
-//        // =============== run_cluster_ia64
-//
-//        p.print("run_cluster_ia64: master_ia64.o $(OBJS_IA64)\n");
-//        p.print("\t$(CC_IA64) $(CCFLAGS_IA64) -o $@ $^ -L$(LIB_CLUSTER) -lpthread -lcluster_ia64\n");
-//        p.newLine();
-//
-//        // =============== fusion_ia64
-//
-//        p.print("fusion_ia64: fusion_ia64.o $(OBJS_IA64)\n");
-//        p.print("\t$(CC_IA64) $(CCFLAGS_IA64) -o $@ $^ -L$(LIB_CLUSTER) -lpthread -lcluster_ia64\n");
-//
-//        p.newLine();
-//
+        
+        p.println("# Unsupported target machines");
+        p.println("ia64: " + executablename + "_ia64");
+        p.newLine();
+        p.println("arm: " + executablename + "_arm");
+        p.newLine();
+
+         if (KjcOptions.standalone) {
+            p.print(executablename + "_ia64: fusion_ia64.o ");
+        } else {
+            p.print(executablename + "_ia64: master_ia64.o ");
+        }
+        p.println("$(OBJS_IA64)");
+        p.print("\t$(CC_IA64) $(CCFLAGS_IA64) -o $@ $^ -L$(LIB_CLUSTER) -lpthread -lcluster_ia64\n");
+        p.newLine();
+
 //        // =============== %_ia64.o : %.cpp
-//
-//        p.print("%_ia64.o: %.cpp fusion.h cluster.h\n");
-//        p.print("\t$(CC_IA64) $(CCFLAGS_IA64) -I$(LIB_CLUSTER) -c -o $@ $<\n");
-//        p.newLine();
-//
-//
-//
-//
-//        // =============== fusion_arm
-//
-//        p.print("fusion_arm: fusion_arm.o $(OBJS_ARM)\n");
-//        p.print("\tar r objects_arm.a $^\n");
-//        p.print("\tranlib objects_arm.a\n");
-//        p.print("\t$(CC_ARM) $(CCFLAGS_ARM) -o $@ objects_arm.a -L$(LIB_CLUSTER) -lstdc++ -lm -lcluster_arm #-lpthread\n");
-//
-//        p.newLine();
-//
-//        // =============== %_arm.o : %.cpp
-//
-//        p.print("%_arm.o: %.cpp fusion.h cluster.h\n");
-//        p.print("\t$(CC_ARM) $(CCFLAGS_ARM) -I$(LIB_CLUSTER) -c -o $@ $<\n");
-//        p.newLine();
-//
+
+        p.print("%_ia64.o: %.cpp fusion.h cluster.h\n");
+        p.print("\t$(CC_IA64) $(CCFLAGS_IA64) -I$(LIB_CLUSTER) -c -o $@ $<\n");
+        p.newLine();
+
+
+
+   
+        // =============== fusion_arm
+
+        p.print(executablename + "_arm: fusion_arm.o $(OBJS_ARM)\n");
+        p.print("\tar r objects_arm.a $^\n");
+        p.print("\tranlib objects_arm.a\n");
+        p.print("\t$(CC_ARM) $(CCFLAGS_ARM) -o $@ objects_arm.a -L$(LIB_CLUSTER) -lstdc++ -lm -lcluster_arm #-lpthread\n");
+
+        p.newLine();
+
+        // =============== %_arm.o : %.cpp
+
+        p.print("%_arm.o: %.cpp fusion.h cluster.h\n");
+        p.print("\t$(CC_ARM) $(CCFLAGS_ARM) -I$(LIB_CLUSTER) -c -o $@ $<\n");
+        p.newLine();
+
 
 
         try {

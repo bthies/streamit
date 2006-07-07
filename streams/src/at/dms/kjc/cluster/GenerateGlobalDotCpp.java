0183e06c0aea15868acd8c7c91cd2bea0f002758
Allyn Dimock <dimock@cag.csail.mit.edu>
2006-07-07 17:40:01 +0000

    Break up since global.cpp not used in cluster -standalone

--- streams/src/at/dms/kjc/cluster/GenerateGlobalDotCpp.java
+++ streams/src/at/dms/kjc/cluster/GenerateGlobalDotCpp.java
@@ -1,3 +1,4 @@
+// $Header: /afs/csail.mit.edu/group/commit/reps/projects/streamit/cvsroot/streams/src/at/dms/kjc/cluster/GenerateGlobalDotCpp.java,v 1.1 2006-07-07 17:40:01 dimock Exp $
 package at.dms.kjc.cluster;
 
 import java.io.FileWriter;
@@ -14,12 +15,12 @@ import at.dms.kjc.sir.SIRHelper;
 
 
 /**
- * Generate "global.h" and "global.cpp" for cluster back end.
+ * Generate "global.cpp" for cluster back end.
  * 
  * @author Janis
  *
  */
-public class GenerateGlobal {
+public class GenerateGlobalDotCpp {
 
     /**
      * Generate "global.h" and "global.cpp" for cluster back end.
@@ -29,7 +30,7 @@ public class GenerateGlobal {
      * @param global  A static section if any
      * @param helpers Helper functions
      */
-    public static void generateGlobal(SIRGlobal global, SIRHelper[] helpers) {
+    public static void generateGlobalDotCpp(SIRGlobal global, SIRHelper[] helpers) {
 
         String str = new String();
         JFieldDeclaration fields[];
@@ -41,58 +42,9 @@ public class GenerateGlobal {
         }
 
         // ================================
-        // Writing global.h
-        // ================================
-
-        str += "#include <math.h>\n";
-        str += "#include \"structs.h\"\n";
-        str += "#include <StreamItVectorLib.h>\n";
-        str += "\n";
-
-        str += "#define max(A,B) (((A)>(B))?(A):(B))\n";
-        str += "#define min(A,B) (((A)<(B))?(A):(B))\n";
-
-        str += "\n";
-    
-        for (int i = 0; i < helpers.length; i++) {
-            JMethodDeclaration[] m = helpers[i].getMethods();
-            for (int j = 0; j < m.length; j++) {
-                FlatIRToCluster f2c = new FlatIRToCluster();
-                f2c.helper_package = helpers[i].getIdent();
-                f2c.setDeclOnly(true);
-                m[j].accept(f2c);
-                str += "extern "+f2c.getPrinter().getString()+"\n";
-            }
-        }
-
-        str += "extern void __global__init();\n";
-        str += "\n";
-
-        for (int f = 0; f < fields.length; f++) {
-            CType type = fields[f].getType();
-
-            if (type.toString().endsWith("Portal")) continue;
-
-            String ident = fields[f].getVariable().getIdent();
-            str += "extern " + ClusterUtils.declToString(type, "__global__" + ident) + ";\n";
-        }
-
-        try {
-            FileWriter fw = new FileWriter("global.h");
-            fw.write(str.toString());
-            fw.close();
-        }
-        catch (Exception e) {
-            System.err.println("Unable to write <global.h>");
-        }
-
-
-
-        // ================================
         // Writing global.cpp
         // ================================
 
-        str = ""; // reset string
         str += "#include <stdlib.h>\n";
         str += "#include <unistd.h>\n";
         str += "#include <math.h>\n";
---
+++ streams/src/at/dms/kjc/cluster/GenerateGlobalDotH.java
@@ -0,0 +1,90 @@
+// $Header: /afs/csail.mit.edu/group/commit/reps/projects/streamit/cvsroot/streams/src/at/dms/kjc/cluster/GenerateGlobalDotH.java,v 1.1 2006-07-07 17:40:01 dimock Exp $
+package at.dms.kjc.cluster;
+
+import java.io.FileWriter;
+
+import at.dms.kjc.CType;
+import at.dms.kjc.JExpression;
+import at.dms.kjc.JFieldDeclaration;
+import at.dms.kjc.JFloatLiteral;
+import at.dms.kjc.JIntLiteral;
+import at.dms.kjc.JMethodDeclaration;
+import at.dms.kjc.sir.SIRGlobal;
+import at.dms.kjc.sir.SIRHelper;
+
+
+
+/**
+ * Generate "global.h" for cluster back end.
+ * 
+ * @author Janis
+ *
+ */
+public class GenerateGlobalDotH {
+
+    /**
+     * Generate "global.h" and "global.cpp" for cluster back end.
+     * 
+     * <p>Forward references for globals, and code for helper functions</p>
+     * 
+     * @param global  A static section if any
+     * @param helpers Helper functions
+     */
+    public static void generateGlobalDotH(SIRGlobal global, SIRHelper[] helpers) {
+
+        String str = new String();
+        JFieldDeclaration fields[];
+
+        if (global == null) {
+            fields = new JFieldDeclaration[0];
+        } else {
+            fields = global.getFields();
+        }
+
+        // ================================
+        // Writing global.h
+        // ================================
+
+        str += "#include <math.h>\n";
+        str += "#include \"structs.h\"\n";
+        str += "#include <StreamItVectorLib.h>\n";
+        str += "\n";
+
+        str += "#define max(A,B) (((A)>(B))?(A):(B))\n";
+        str += "#define min(A,B) (((A)<(B))?(A):(B))\n";
+
+        str += "\n";
+    
+        for (int i = 0; i < helpers.length; i++) {
+            JMethodDeclaration[] m = helpers[i].getMethods();
+            for (int j = 0; j < m.length; j++) {
+                FlatIRToCluster f2c = new FlatIRToCluster();
+                f2c.helper_package = helpers[i].getIdent();
+                f2c.setDeclOnly(true);
+                m[j].accept(f2c);
+                str += "extern "+f2c.getPrinter().getString()+"\n";
+            }
+        }
+
+        str += "extern void __global__init();\n";
+        str += "\n";
+
+        for (int f = 0; f < fields.length; f++) {
+            CType type = fields[f].getType();
+
+            if (type.toString().endsWith("Portal")) continue;
+
+            String ident = fields[f].getVariable().getIdent();
+            str += "extern " + ClusterUtils.declToString(type, "__global__" + ident) + ";\n";
+        }
+
+        try {
+            FileWriter fw = new FileWriter("global.h");
+            fw.write(str.toString());
+            fw.close();
+        }
+        catch (Exception e) {
+            System.err.println("Unable to write <global.h>");
+        }
+    }
+}

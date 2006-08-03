be2bdfc2bf6d3421ea405f6f6523c1971968f634
Allyn Dimock <dimock@cag.csail.mit.edu>
2006-08-03 21:10:13 +0000

    changed perl location to one that should work on all cag systems

--- streams/apps/applications/802.11a/streamit/importFFT.pl
+++ streams/apps/applications/802.11a/streamit/importFFT.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/uns/bin/perl
 
 # Writes out all of FFT6 except for the void->void stream.  We need to
 # do this since strc expects only one void->void stream in the input

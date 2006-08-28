1dd0ae2e4ca23f6d794311a9e8def920e1a48e9f
Bill Thies <bthies@gmail.com>
2006-08-28 21:32:24 +0000

    Only print the warning about "WARNING: non-Array input to dumpAssign" once per call to ArrayCopy, to avoid pages and pages of warnings on MPEG.
    Not that I have the faintest idea what this warning means.

--- streams/src/at/dms/kjc/common/ArrayCopy.java
+++ streams/src/at/dms/kjc/common/ArrayCopy.java
@@ -17,11 +17,12 @@ public class ArrayCopy {
                 temp=((Object[])temp)[0];
             }
             if(dims.size()>0) {
+                printWarning = false;
                 dumpAssign(val,body,new JLocalVariableExpression(null,params[i]));
             }
         }
     }
-    
+
     private static void dumpAssign(Object array,JBlock body,JExpression prefix) {
         if(array instanceof JExpression) {
             if(((JExpression)array).isConstant())
@@ -30,7 +31,14 @@ public class ArrayCopy {
             for(int i=((Object[])array).length-1;i>=0;i--)
                 dumpAssign(((Object[])array)[i],body,new JArrayAccessExpression(null,prefix,new JIntLiteral(i)));
         } else {
-            System.err.println("WARNING: Non Array input to dumpAssign"+array);
+            if (!printWarning) {
+                System.err.println("WARNING: Non-array input in ArrayCopy (" + array + ")");
+                printWarning = true;
+            }
         }
     }
+    // whether or not we have already printed a warning -- to avoid
+    // hundreds of pages of warnings
+    private static boolean printWarning;
+ 
 }

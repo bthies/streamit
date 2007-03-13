c884d74a81c719d8eacca2dbe5cc2243ab0a6354
Allyn Dimock <dimock@cag.csail.mit.edu>
2007-03-13 20:19:27 +0000

    added moved files to CVS: eclipse only removes source from CVS on move, does not add new location

---
+++ streams/src/at/dms/kjc/backendSupport/BasicGreedyLayout.java
@@ -0,0 +1,154 @@
+/**
+ * 
+ */
+package at.dms.kjc.backendSupport;
+
+import java.util.*;
+
+import at.dms.kjc.slicegraph.DataFlowOrder;
+import at.dms.kjc.slicegraph.FilterSliceNode;
+import at.dms.kjc.slicegraph.Slice;
+import at.dms.kjc.spacetime.CompareSliceBNWork;
+import at.dms.kjc.spacetime.SpaceTimeBackend;
+
+
+/**
+ * Use greedy bin packing to allocate slices to compute nodes.
+ * @author mgordon / dimock
+ *
+ */
+public class BasicGreedyLayout<T extends ComputeNode> implements Layout<T> {
+    private HashMap<FilterSliceNode, T> assignment;
+    private SpaceTimeScheduleAndPartitioner spaceTime;
+    private int numBins;
+    private LinkedList<FilterSliceNode>[] bins;
+    private int[] binWeight;
+    //private int[] searchOrder;
+    private int totalWork;
+    private T[] nodes;
+    
+    /**
+     * Constructor
+     * @param spaceTime
+     * @param nodes
+     */
+    public BasicGreedyLayout(SpaceTimeScheduleAndPartitioner spaceTime, T[] nodes) {
+        this.spaceTime = spaceTime;
+        this.nodes = nodes;
+        this.numBins = nodes.length;
+     
+        bins = new LinkedList[numBins];
+        binWeight = new int[numBins];
+        for (int i = 0; i < numBins; i++) {
+            bins[i] = new LinkedList<FilterSliceNode>();
+            binWeight[i] = 0;
+        }
+    }
+    
+    public HashMap<FilterSliceNode, T> getAssignment() {
+        return assignment;
+    }
+    
+    
+    public T getComputeNode(FilterSliceNode node) {
+        return assignment.get(node);
+    }
+   
+    public void setComputeNode(FilterSliceNode node, T tile) {
+        assignment.put(node, tile);
+    }
+    public void run() {
+        assignment = new HashMap<FilterSliceNode, T>();
+        pack();
+
+        System.out.println("IdealWork = " + totalWork / numBins);
+        System.out.println("Greedy max tile Work Cost = " + maxBinWeight());
+    }
+    
+    private void pack() {
+        //now sort the filters by work
+        LinkedList<FilterSliceNode> sortedList = new LinkedList<FilterSliceNode>();
+        LinkedList<Slice> scheduleOrder;
+        
+        //get the schedule order of the graph!
+        if (SpaceTimeBackend.NO_SWPIPELINE) {
+            //if we are not software pipelining then use then respect
+            //dataflow dependencies
+            scheduleOrder = DataFlowOrder.getTraversal(spaceTime.getPartitioner().getSliceGraph());
+        } else {
+            //if we are software pipelining then sort the traces by work
+            Slice[] tempArray = (Slice[]) spaceTime.getPartitioner().getSliceGraph().clone();
+            Arrays.sort(tempArray, new CompareSliceBNWork(spaceTime.getPartitioner()));
+            scheduleOrder = new LinkedList<Slice>(Arrays.asList(tempArray));
+            //reverse the list, we want the list in descending order!
+            Collections.reverse(scheduleOrder);
+        }
+
+        
+        for (int i = 0; i < scheduleOrder.size(); i++) {
+            Slice slice = scheduleOrder.get(i);
+            assert slice.getNumFilters() == 1 : "The greedy partitioner only works for Time!";
+            sortedList.add(slice.getHead().getNextFilter());
+        }
+        
+        Iterator<FilterSliceNode> sorted = sortedList.iterator();
+        
+        //perform the packing
+        while (sorted.hasNext()) {
+            FilterSliceNode fnode = sorted.next();
+            int bin = findMinBin();
+            
+            bins[bin].add(fnode);
+            assignment.put(fnode, nodes[bin]);
+            binWeight[bin] += spaceTime.getPartitioner().getFilterWorkSteadyMult(fnode);
+            totalWork += spaceTime.getPartitioner().getFilterWorkSteadyMult(fnode);
+            System.out.println(" Placing: " + fnode + " work = " + 
+                    spaceTime.getPartitioner().getFilterWorkSteadyMult(fnode) + 
+                            " on bin " + bin + ", bin work = " + binWeight[bin]);
+
+        }
+    }
+    
+    private int findMinBin() {
+        int minWeight = Integer.MAX_VALUE;
+        int minBin = -1;
+        for (int i = 0; i < numBins; i++) {
+            //int index = searchOrder[i]; 
+            if (binWeight[/*index*/ i] < minWeight) {
+                minBin = /*index*/ i;
+                minWeight = binWeight[/*index*/ i];
+            }
+        }
+        return minBin;
+    }
+    
+    /**
+     * get the bin weights (Estimated max computation at each node).
+     * @return
+     */
+    public int[] getBinWeights() {
+        return binWeight;
+    }
+    
+    private boolean gotMaxBinWeight = false;
+    private int maxBinWeight;
+    
+    /**
+     * get maximum bin weight (Estimated max computation at any node).
+     * @return
+     */
+    public int maxBinWeight() {
+        if (!gotMaxBinWeight) {
+            maxBinWeight = -1;
+            // find max bin
+            for (int i = 0; i < numBins; i++) {
+                if (binWeight[i] > maxBinWeight) {
+                    maxBinWeight = binWeight[i];
+                }
+            }
+            gotMaxBinWeight = true;
+        }
+
+        return maxBinWeight;
+    }
+}
---
+++ streams/src/at/dms/kjc/backendSupport/SpaceTimeScheduleAndPartitioner.java
@@ -0,0 +1,83 @@
+//$Id: SpaceTimeScheduleAndPartitioner.java,v 1.1 2007-03-13 20:19:27 dimock Exp $
+/**
+ * Extracts the "schedule" part of Mike's SpaceTimeSchedule.
+ * 
+ */
+package at.dms.kjc.backendSupport;
+
+import java.util.Vector;
+
+import at.dms.kjc.slicegraph.FilterInfo;
+import at.dms.kjc.slicegraph.FilterSliceNode;
+import at.dms.kjc.slicegraph.Partitioner;
+import at.dms.kjc.slicegraph.Slice;
+import at.dms.kjc.spacetime.BasicSpaceTimeSchedule;
+
+/**
+ * Extend BasicSpaceTimeSchedule by storing a partitioner.
+ * 
+ * BasicSpaceTimeSchedule collects initialization schedule, prime-pump schedule
+ * and steady-state schedule in one place.
+ * This is purely a data structure: it is operated on by 
+ * other classes to generate these schedules.
+ * 
+ * It is convenient for classes performing layout to keep the partitioner with
+ * the schedule.  The partitioner supplies the initial slice graph and includes
+ * a map from filters to the amount of work that they perform, which is needed
+ * to partition.
+ * 
+ * @author mgordon (refactored dimock)
+ */
+public class SpaceTimeScheduleAndPartitioner extends BasicSpaceTimeSchedule {
+    /** Partitioner stored with schedule. */
+    private Partitioner partitioner;
+    
+    public SpaceTimeScheduleAndPartitioner(Partitioner partitioner) {
+        super();
+        this.partitioner = partitioner;
+    }
+    
+    
+    /**
+     * Return the number of outputs that are written to file writers during the 
+     * steady-state.
+     *  
+     * @return the number of outputs that are written to file writers during the 
+     * steady-state.
+     */
+    public int outputsPerSteady() {
+        int outputs = 0;
+        
+        //get all the file writers
+        Vector<Slice> fileWriters = new Vector<Slice>();
+        for (int i = 0; i < getPartitioner().io.length; i++) 
+            if (getPartitioner().io[i].getHead().isFileOutput())
+                fileWriters.add(getPartitioner().io[i]);
+        
+        for (int i = 0; i < fileWriters.size(); i++) {
+            FilterSliceNode node = (FilterSliceNode)fileWriters.get(i).getHead().getNext();
+            FilterInfo fi = FilterInfo.getFilterInfo(node);
+            assert node.getFilter().getInputType().isNumeric() :
+                "non-numeric type for input to filewriter";
+        
+            outputs += fi.totalItemsReceived(SchedulingPhase.STEADY);
+        }
+        return outputs;
+    }
+    
+
+    
+    /** 
+     * @param partitioner
+     */
+    public void setPartitioner(Partitioner partitioner) {
+        this.partitioner = partitioner;
+    }
+
+    /**
+     * @return the partitioner associated with this schedule.
+     */
+    public Partitioner getPartitioner() {
+        return partitioner;
+    }
+}

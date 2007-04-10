d356de96b349c76b57346a97f72aba8700a35efa
Allyn Dimock <dimock@cag.csail.mit.edu>
2007-04-10 20:30:23 +0000

    Add some comments.  Deprecate variables used only in deprecated code.

--- streams/src/at/dms/kjc/spacetime/GeneratedVariables.java
+++ streams/src/at/dms/kjc/spacetime/GeneratedVariables.java
@@ -2,21 +2,35 @@ package at.dms.kjc.spacetime;
 
 import at.dms.kjc.*;
 
-/*** Class to hold the variables we generate during raw execution code
+/** Class to hold the variables we generate during raw execution code
  * generated, it is passed around to various classes 
  */
 public class GeneratedVariables
 {
+    /** Variable is peek buffer */
     JVariableDefinition recvBuffer;
+    /** Variable seems to be obsolete: eclipse shows single write to this */
     JVariableDefinition recvBufferSize;
+    /** Variable is mask for modulo access to peek buffer */
     JVariableDefinition recvBufferBits;
+    /** Variable is index into peek buffer */
     JVariableDefinition recvBufferIndex;
+    /** Variable is index of end of peek buffer */
     JVariableDefinition recvIndex;
+    /** Variable is loop index variable */
     JVariableDefinition exeIndex;
+    /** Variable is another loop index variable */
     JVariableDefinition exeIndex1;
+    /** Variable is indices for copying (multidimensional) arrays */
     JVariableDefinition[] ARRAY_INDEX;
+    /** Variable is unused? */
     JVariableDefinition[] ARRAY_COPY;
+    /** Variable is index in peek buffer for ConvertCommunicationSimple */
     JVariableDefinition simpleIndex;
+    /** obsolete?? */
+    @Deprecated
     JVariableDefinition sendBufferIndex;
+    /** obsolete?? */
+    @Deprecated
     JVariableDefinition sendBuffer;
 }

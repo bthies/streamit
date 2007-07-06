260e7faba2b521af171da6ebd10f4a2a70a291e2
Bill Thies <bthies@gmail.com>
2007-07-06 15:14:16 +0000

    Updating the manual partitioning examples to work with the current compiler.

--- streams/apps/benchmarks/beamformer/streamit/MyPartition1.java
+++ streams/apps/benchmarks/beamformer/streamit/MyPartition1.java
@@ -1,3 +1,4 @@
+import at.dms.kjc.*;
 import at.dms.kjc.sir.*;
 import at.dms.kjc.sir.lowering.partition.*;
 
@@ -8,14 +9,18 @@ public class MyPartition1 {
 
     public static SIRStream manualPartition(SIRStream str) {
 
+        // uncomment this and run to discover which streams have which numbers
+        // NumberDot.printGraph(str, "numbered.dot");
+        // return str;
+
 	// fuse the top pipelines
-	for (int i=41; i<=85; i+=4) {
+	for (int i=135; i<=146; i++) {
 	    SIRStream s = ManualPartition.getStream(str, i);
 	    ManualPartition.fuse(s);
 	}
 
 	// fuse bottom pipelines
-	for (int i=89; i<=104; i+=5) {
+	for (int i=148; i<=151; i++) {
 	    SIRStream s = ManualPartition.getStream(str, i);
 	    ManualPartition.fuse(s);
 	}
@@ -23,8 +28,10 @@ public class MyPartition1 {
 	// fuse the bottom splitjoin to have 2 children instead of 4
 	// children
 	PartitionGroup partitions = PartitionGroup.createFromArray(new int[] {2, 2});
-	SIRSplitJoin bottomSJ = (SIRSplitJoin)ManualPartition.getStream(str, 40);
+	SIRSplitJoin bottomSJ = (SIRSplitJoin)ManualPartition.getStream(str, 147);
 	ManualPartition.fuse(bottomSJ, partitions);
+
+        // you can view the result of the manual partitioning in "after-partition.dot"
 	
 	return str;
     }
--- streams/apps/benchmarks/beamformer/streamit/MyPartition2.java
+++ streams/apps/benchmarks/beamformer/streamit/MyPartition2.java
@@ -8,11 +8,15 @@ public class MyPartition2 {
 
     public static SIRStream manualPartition(SIRStream str) {
 
+        // uncomment this and run to discover which streams have which numbers
+        // NumberDot.printGraph(str, "numbered.dot");
+        // return str;
+
 	// pull all the InputGenerate's into their own splitjoin, so
 	// that we can fuse them all together
 	PartitionGroup partitions = PartitionGroup.createFromArray(new int[] {1, 2});
 	// get top-most splitjoin
-	SIRSplitJoin sj = (SIRSplitJoin)ManualPartition.getStream(str, 34);
+	SIRSplitJoin sj = (SIRSplitJoin)ManualPartition.getStream(str, 134);
 	// add sync point after input generate
 	ManualPartition.addSyncPoints(sj, partitions);
 
@@ -20,23 +24,23 @@ public class MyPartition2 {
 	// on top
 	ManualPartition.printGraph(str, "numbered2.dot");
 
-	// the new SJ is #114 -- collapse it into a single filter
-	SIRSplitJoin top = (SIRSplitJoin)ManualPartition.getStream(str, 114);
+	// the new SJ is #153 -- collapse it into a single filter
+	SIRSplitJoin top = (SIRSplitJoin)ManualPartition.getStream(str, 153);
 	ManualPartition.fuse(top);
 
-	// ok, now decrease the rest of the splitjoin to a 6-way
-	// splitjoin.  We will fuse groups of 4 filters into a single
-	// child.
+	// ok, now decrease the rest of the 12-way splitjoin to a
+	// 6-way splitjoin.  We will fuse groups of 4 filters (2
+	// pipelines of 2 filters each) into a single child.
 	partitions = PartitionGroup.createFromArray(new int[] {2,2,2,2,2,2});
-	// get the second splitjoin -- #115.  It is now the top one
-	top = (SIRSplitJoin)ManualPartition.getStream(str, 115);
+	// get the second splitjoin -- #154.  It is now the top one
+	top = (SIRSplitJoin)ManualPartition.getStream(str, 154);
 	ManualPartition.addHierarchicalChildren(top, partitions);
 
-	// print to graph to make sure we got it
+	// print to graph to get new numbers
 	ManualPartition.printGraph(str, "numbered3.dot");
 
 	// fuse each of these 6 children
-	for (int i=136; i<=141; i++) {
+	for (int i=173; i<=178; i++) {
 	    ManualPartition.fuse(ManualPartition.getStream(str, i));
 	}
 
@@ -44,7 +48,7 @@ public class MyPartition2 {
 	ManualPartition.printGraph(str, "numbered4.dot");
 
 	// fuse bottom pipelines
-	for (int i=89; i<=104; i+=5) {
+	for (int i=148; i<=151; i++) {
 	    SIRStream s = ManualPartition.getStream(str, i);
 	    ManualPartition.fuse(s);
 	}

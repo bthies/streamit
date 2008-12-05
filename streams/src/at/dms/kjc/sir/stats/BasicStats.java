16916d5138d12c3430c3164cbba1594d0c490ca2
Bill Thies <bthies@gmail.com>
2008-12-05 20:28:11 +0000

    Latest statistics gathering, including mode multiplicity and other reports.

--- streams/src/at/dms/kjc/sir/stats/BasicStats.java
+++ streams/src/at/dms/kjc/sir/stats/BasicStats.java
@@ -43,6 +43,10 @@ public class BasicStats {
     int multiplicityOneFilters = 0;
     // names of stateful filters
     List stateful = new ArrayList();
+    // names of peeking filters
+    List peeking = new ArrayList();
+    // dynamic rate filters
+    List<SIRFilter> dynamicRateFilters = new ArrayList<SIRFilter>();
     // total work in the program
     long totalWork = 0;
     // total stateful work
@@ -55,6 +59,8 @@ public class BasicStats {
     long totalFeedbackWork = 0;
     // list of multiplicities of filters (in the schedule)
     List multiplicities = new LinkedList();
+    // unique multiplicities of filters (in the schedule)
+    HashSet<Integer> uniqueMultiplicities = new HashSet<Integer>();
 
     /**
      * Gather statistics for <str>.
@@ -117,6 +123,7 @@ public class BasicStats {
                     long myWork = work.getWork(self);
                     totalWork += myWork;
                     multiplicities.add(new Integer(work.getReps(self)));
+                    uniqueMultiplicities.add(new Integer(work.getReps(self)));
                     if (work.getReps(self)==1) {
                         multiplicityOneFilters++;
                     }
@@ -134,14 +141,15 @@ public class BasicStats {
                         numIdentityFilters++;
                     } else if (self.getPeek().isDynamic() || self.getPop().isDynamic() || self.getPush().isDynamic()) {
                         numDynamicRates++;
+                        dynamicRateFilters.add(self);
                     }  else if (self.getPeekInt()==1 && self.getPushInt()==1) {
                             numOneToOneFilters++;
                     }
                     if (!self.getPeek().isDynamic() && !self.getPop().isDynamic()) {
                         // don't worry about checking peeking for dynamic-rate filters
                         if (self.getPeekInt()>self.getPopInt()) {
-                            System.out.println("Peeking filter:" + self.getCleanIdent());
                             numPeekingFilters++;
+                            peeking.add(self.getCleanIdent());
                             if (myWork > maxPeekingWork) {
                                 maxPeekingWork = myWork;
                             }
@@ -180,6 +188,31 @@ public class BasicStats {
                           (mults[(mults.length+1)/2].intValue()));
         int maxMult = mults[mults.length-1].intValue();
 
+        // calculate the mode multiplicity and the number of filters having that multiplicity
+        int modeFreq = 0;
+        int modeMult = 0;
+        int lastMult = -1;
+        int curFreq = 0;
+        for (int i=0; i<mults.length; i++) {
+            if (mults[i] == lastMult) {
+                // continue a trend
+                curFreq++;
+            } else {
+                // start a new trend
+                if (curFreq > modeFreq) {
+                    modeFreq = curFreq;
+                    modeMult = lastMult;
+                }
+                curFreq = 1;
+                lastMult = mults[i];
+            }
+        }
+        // check if last run was best
+        if (curFreq > modeFreq) {
+            modeFreq = curFreq;
+            modeMult = lastMult;
+        }
+        
         // print out results
         double fractionStatefulTotal = (double)totalStatefulWork/(double)totalWork;
         double fractionStatefulMax = (double)maxStatefulWork/(double)totalWork;
@@ -201,7 +234,10 @@ public class BasicStats {
         System.out.println("Number of pop 1, peek 1, push 1 filters: " + numOneToOneFilters);
         System.out.println("Number of stateful filters: " + numStatefulFilters);
         System.out.println("Min / median / max multiplicity: " + minMult + " / " + medianMult + " / " + maxMult);
+        System.out.println("Mode multiplicity: " + modeMult);
+        System.out.println("Number of filters with mode multiplicity: " + modeFreq);
         System.out.println("Number of multiplicity-one filters: " + multiplicityOneFilters);
+        System.out.println("Number of distinct multiplicities: " + uniqueMultiplicities.size());
         if (stateful.size()>0) {
             System.out.println("Total stateful work in graph (ignoring sources/sinks): " + fractionStatefulTotal + "%");
             System.out.println("Max stateful filter in single filter (ignoring sources/sinks): " + fractionStatefulMax + "%");
@@ -216,6 +252,26 @@ public class BasicStats {
             }
         } else {
             System.out.println("No stateful filters (ignoring sources/sinks)");
+        }
+        if (peeking.size()>0) {
+            System.out.println("Peeking filters:");
+            Collections.sort(peeking); // output in alphabetical order
+            for (int i=0; i<peeking.size(); i++) {
+                // don't print duplicates
+                if (i==0 || (i>0 && !peeking.get(i).equals(peeking.get(i-1)))) {
+                    System.out.println(" - " + peeking.get(i));
+                }
+            }
+        }
+        if (numDynamicRates>0) {
+            System.out.println("Dynamic rate filters:");
+            for (int i=0; i<dynamicRateFilters.size(); i++) {
+                SIRFilter filter = dynamicRateFilters.get(i);
+                System.out.println(" - " + filter.getCleanIdent() + 
+                                   " push " +  (filter.getPush().isDynamic() ? ""+filter.getPush() : ""+filter.getPushInt()) +
+                                   " pop " + (filter.getPop().isDynamic() ? ""+filter.getPop() : ""+filter.getPopInt()) +
+                                   " peek " + (filter.getPeek().isDynamic() ? ""+filter.getPeek() : ""+filter.getPeekInt()));
+            }
         }
 
         System.out.println();
@@ -240,7 +296,10 @@ public class BasicStats {
         System.out.print("Min mult"  + ",");
         System.out.print("Median mult"  + ",");
         System.out.print("Max mult"  + ",");
+        System.out.print("Mode multiplicity: " + ",");
+        System.out.print("Filters with mode multiplicity: " + ",");
         System.out.print("Mult-one filters"  + ",");
+        System.out.print("Number of distinct multiplicities" + ",");
         System.out.println();
 
         System.out.print(Kopi2SIR.numFilters + ",");
@@ -263,7 +322,10 @@ public class BasicStats {
         System.out.print(minMult + ",");
         System.out.print(medianMult + ",");
         System.out.print(maxMult + ",");
+        System.out.print(modeMult + ",");
+        System.out.print(modeFreq + ",");
         System.out.print(multiplicityOneFilters + ",");
+        System.out.print(uniqueMultiplicities.size() + ",");
         System.out.println();
 
         // we're done

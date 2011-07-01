e1fa285e3022658121d3bdd6271d3ab2850c1055
no_author <no_author@no_author>
2011-07-01 15:41:49 +0000

    Used eclipse to automatically infer type arguments to generic code.

--- streams/src/at/dms/kjc/spacedynamic/SwitchCode.java
+++ streams/src/at/dms/kjc/spacedynamic/SwitchCode.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.io.*;
 
-import sun.java2d.pipe.LoopPipe;
+//import sun.java2d.pipe.LoopPipe;
 
 /**
  * This class generates the switch code for each tile and writes it to a file
